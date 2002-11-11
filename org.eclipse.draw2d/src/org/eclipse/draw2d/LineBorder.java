634bafd6b551dfca3ac30e3c62d82f2330c22687
ebordeau <ebordeau>
2002-11-11 19:46:47 +0000

    CheckStyle fixes and updated javadoc. Also added getters and setters for width and color.

--- org.eclipse.draw2d/src/org/eclipse/draw2d/LineBorder.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/LineBorder.java
@@ -16,80 +16,109 @@ public class LineBorder
 	extends AbstractBorder
 {
 
-protected int width = 1;
-protected Color color;
+private int width = 1;
+private Color color;
 
 /**
- * Constructs a LineBorder with the specified color, and
- * of the specified width.
+ * Constructs a LineBorder with the specified color and of the specified width.
  *
- * @param c  Color of the border.
- * @param width  Width in pixels of the border.
+ * @param color The color of the border.
+ * @param width The width of the border in pixels.
  * @since 2.0
  */
-public LineBorder(Color c, int width){
-	color = c;
-	this.width = width;
+public LineBorder(Color color, int width) {
+	setColor(color);
+	setWidth(width);
 }
 
 /**
- * Constructs a LineBorder with the specified color, and
- * a width of 1 pixel.
+ * Constructs a LineBorder with the specified color and a width of 1 pixel.
  *
- * @param c  Color of the border.
+ * @param color The color of the border.
  * @since 2.0
  */
-public LineBorder(Color c){
-	this(c, 1);
+public LineBorder(Color color) {
+	this(color, 1);
 }
 
 /**
  * Constructs a black LineBorder with the specified width.
  *
- * @param width width of the LineBorder in pixels.
+ * @param width The width of the border in pixels.
  * @since 2.0
  */
-public LineBorder(int width){
+public LineBorder(int width) {
 	this(null, width);
 }
 
 /**
- * Constructs a default black LineBorder with a width of
- * one pixel.
+ * Constructs a default black LineBorder with a width of one pixel.
  * 
  * @since 2.0
  */
-public LineBorder(){}
+public LineBorder() { }
 
-/*
- * Returns the space used by the border for the 
- * figure provided as input. In this border all 
- * sides always have equal width.
- */
-public Insets getInsets(IFigure figure){
-	return new Insets(width);
+/**
+ * Returns the line color of this border.
+ * @return The line color of this border */
+public Color getColor() {
+	return color;
 }
 
-/*
- * Returns true to indicate whether this border is opaque or not.
- * Being opaque it is responsible to fill in the area within
- * its boundaries.
+/**
+ * Returns the space used by the border for the figure provided as input. In this border
+ * all sides always have equal width.
+ * @param figure The figure this border belongs to
+ * @return This border's insets
  */
-public boolean isOpaque(){
+public Insets getInsets(IFigure figure) {
+	return new Insets(getWidth());
+}
+
+/**
+ * Returns the line width of this border.
+ * @return The line width of this border */
+public int getWidth() {
+	return width;
+}
+
+/**
+ * Returns <code>true</code> since this border is opaque. Being opaque it is responsible 
+ * to fill in the area within its boundaries.
+ * @return <code>true</code> since this border is opaque
+ */
+public boolean isOpaque() {
 	return true;
 }
 
-public void paint(IFigure figure, Graphics graphics, Insets insets){
+/**
+ * @see org.eclipse.draw2d.Border#paint(IFigure, Graphics, Insets)
+ */
+public void paint(IFigure figure, Graphics graphics, Insets insets) {
 	tempRect.setBounds(getPaintRectangle(figure, insets));
-	if (width%2 == 1){
+	if (getWidth() % 2 == 1) {
 		tempRect.width--;
 		tempRect.height--;
 	}
-	tempRect.shrink(width/2,width/2);
-	graphics.setLineWidth(width);
-	if (color != null)
-		graphics.setForegroundColor(color);
+	tempRect.shrink(getWidth() / 2, getWidth() / 2);
+	graphics.setLineWidth(getWidth());
+	if (getColor() != null)
+		graphics.setForegroundColor(getColor());
 	graphics.drawRectangle(tempRect);
+}
+
+/**
+ * Sets the line color for this border.
+ * @param color The line color */
+public void setColor(Color color) {
+	this.color = color;
+}
+
+/**
+ * Sets the line width for this border.
+ * @param width The line width */
+public void setWidth(int width) {
+	this.width = width;
 }
 
 }
