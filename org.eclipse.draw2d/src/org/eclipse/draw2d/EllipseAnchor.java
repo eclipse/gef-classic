78c35a1f3abc8f2bef76dc6f5d4d5661c3dd943f
ebordeau <ebordeau>
2002-11-13 16:58:13 +0000

    *** empty log message ***

---
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/EllipseAnchor.java
@@ -0,0 +1,55 @@
+package org.eclipse.draw2d;
+
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+
+/**
+ * Similar to a {@link org.eclipse.draw2d.ChopboxAnchor}, except this anchor is located on
+ * the ellipse defined by the owners bounding box.
+ * @author Alex Selkov
+ * Created 31.08.2002 23:11:43
+ */
+public class EllipseAnchor extends AbstractConnectionAnchor {
+
+/**
+ * @see org.eclipse.draw2d.AbstractConnectionAnchor#AbstractConnectionAnchor()
+ */
+public EllipseAnchor() { }
+
+/**
+ * @see org.eclipse.draw2d.AbstractConnectionAnchor#AbstractConnectionAnchor(IFigure)
+ */
+public EllipseAnchor(IFigure owner) {
+	super(owner);
+}
+
+/**
+ * Returns a point on the ellipse (defined by the owner's bounding box) where the
+ * connection should be anchored.
+ * @see org.eclipse.draw2d.ConnectionAnchor#getLocation(Point) */
+public Point getLocation(Point reference) {
+	Rectangle r = Rectangle.SINGLETON;
+	r.setBounds(getOwner().getBounds());
+	r.translate(-1, -1);
+	r.resize(1, 1);
+	getOwner().translateToAbsolute(r);
+	
+	Point ref = r.getCenter().negate().translate(reference);
+	
+	if (ref.x == 0)
+		return new Point(reference.x, (ref.y > 0) ? r.bottom() : r.y);
+	if (ref.y == 0)
+		return new Point((ref.x > 0) ? r.right() : r.x, reference.y);
+	
+	float dx = (ref.x > 0) ? 0.5f : -0.5f;
+	float dy = (ref.y > 0) ? 0.5f : -0.5f;
+	  
+	// ref.x, ref.y, r.width, r.height != 0 => safe to proceed
+	  
+	float k = (float)(ref.y * r.width) / (ref.x * r.height);
+	k = k * k;
+	  
+	return r.getCenter().translate((int)(r.width * dx / Math.sqrt(1 + k)),
+	                                (int)(r.height * dy / Math.sqrt(1 + 1 / k)));
+}
+}
