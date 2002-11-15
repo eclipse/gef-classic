0fd1b9ae138f4dac99ec0507e51cc74291c6568a
ebordeau <ebordeau>
2002-11-15 01:37:09 +0000

    *** empty log message ***

--- org.eclipse.gef/src/org/eclipse/gef/dnd/LocalTransferDragListener.java
+++ org.eclipse.gef/src/org/eclipse/gef/dnd/LocalTransferDragListener.java
@@ -1,10 +1,19 @@
 package org.eclipse.gef.dnd;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPartViewer;
 import org.eclipse.swt.dnd.DragSourceEvent;
 import org.eclipse.swt.dnd.Transfer;
 
+import org.eclipse.jface.viewers.*;
+import org.eclipse.jface.viewers.StructuredSelection;
+
 public class LocalTransferDragListener extends AbstractTransferDragSourceListener {
+
+private List modelSelection;
 
 public LocalTransferDragListener(EditPartViewer viewer) {
 	super(viewer, LocalTransfer.getInstance());
@@ -22,12 +31,32 @@ public void dragSetData(DragSourceEvent event) {
 
 public void dragStart(DragSourceEvent event) {
 	LocalTransfer.getInstance().setViewer(getViewer());
-	LocalTransfer.getInstance().setObject(getViewer().getSelectedEditParts());
+	List selection = getViewer().getSelectedEditParts();
+	LocalTransfer.getInstance().setObject(selection);
+	saveModelSelection(selection);
 }
 
 public void dragFinished(DragSourceEvent event) {
 	LocalTransfer.getInstance().setObject(null);
 	LocalTransfer.getInstance().setViewer(null);
+	revertModelSelection();
+}
+
+protected void revertModelSelection() {
+	List list = new ArrayList();
+	for (int i = 0; i < modelSelection.size(); i++) {
+		list.add(getViewer().getEditPartRegistry().get(modelSelection.get(i)));
+	}
+	IStructuredSelection selection = new StructuredSelection(list);
+	getViewer().setSelection(selection);
+}
+
+protected void saveModelSelection(List editPartSelection) {
+	modelSelection = new ArrayList();
+	for (int i = 0; i < editPartSelection.size(); i++) {
+		EditPart editpart = (EditPart)editPartSelection.get(i);
+		modelSelection.add(editpart.getModel());
+	}
 }
 
 }
