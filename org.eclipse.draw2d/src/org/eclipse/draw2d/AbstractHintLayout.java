13690aaf760b5e175c49b94559040447a503faac
rhudson <rhudson>
2002-12-03 15:06:08 +0000

    getMinimumSize(hints) and horz/vert sensitivity

--- org.eclipse.draw2d/src/org/eclipse/draw2d/AbstractHintLayout.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/AbstractHintLayout.java
@@ -3,6 +3,14 @@ package org.eclipse.draw2d;
 import org.eclipse.draw2d.geometry.Dimension;
 
 /**
+ * The foundation for layout managers which are sensitive to width and/or height hints.
+ * This class will cache preferred and minimum sizes for a given set of hints. If the
+ * hints change in a meaningful way, the cached size is thrown out and redetermined.
+ * <P>
+ * Subclasses may be sensitive to one or both hints. By default, this class assumes both
+ * hints are important. Subclasses may override this behavior in {@link
+ * #isSensitiveHorizontally(IFigure)} and {@link #isSensitiveVertically(IFigure)}. At
+ * least one of these method should return <code>true</code>.
  * 
  * Created on :Sep 30, 2002
  * @author hudsonr
@@ -15,30 +23,75 @@ public abstract class AbstractHintLayout
 private Dimension minimumSize = null;
 private Dimension cachedHint = new Dimension(-1, -1);
 
-protected final Dimension calculatePreferredSize(IFigure container) {
-	throw new RuntimeException("Unreachable code"); //$NON-NLS-1$
+/**
+ * Calculates the minimum size using the given width and height hints. This method is
+ * called from {@link #getMinimumSize(IFigure, int, int)} whenever the cached minimum size
+ * has been flushed.
+ * <P>
+ * By default, this method just calls {@link #getPreferredSize(IFigure, int, int)},
+ * meaning minimum and preferres sizes will be the same unless this method is overridden.
+ * @param container the Figure on which this layout is installed * @param wHint the width hint * @param hHint the height hint * @return the layout's minimum size */
+protected Dimension calculateMinimumSize(IFigure container, int wHint, int hHint) {
+	return getPreferredSize(container, wHint, hHint);
 }
 
-protected abstract Dimension calculateMinimumSize(IFigure container);
-
-public final Dimension getMinimumSize(IFigure container) {
-	if (minimumSize == null)
-		minimumSize = calculateMinimumSize(container);
-	return minimumSize;
-}
-
-public final Dimension getPreferredSize(IFigure figure, int w, int h) { 
-	if (cachedHint.width != w || cachedHint.height != h) {
-		invalidate();
+/**
+ *  * @see org.eclipse.draw2d.LayoutManager#getMinimumSize(IFigure, int, int) */
+public final Dimension getMinimumSize(IFigure container, int w, int h) {
+	boolean flush = cachedHint.width != w
+		&& isSensitiveHorizontally(container);
+	flush |= cachedHint.height != h
+		&& isSensitiveVertically(container);
+	if (flush) {
+		minimumSize = null;
 		cachedHint.width = w;
 		cachedHint.height = h;
 	}
-	return super.getPreferredSize(figure, w, h);
+	if (minimumSize == null)
+		minimumSize = calculateMinimumSize(container, w, h);
+	return minimumSize;
 }
 
+/** * @see org.eclipse.draw2d.LayoutManager#getPreferredSize(IFigure, int, int) */
+public final Dimension getPreferredSize(IFigure container, int w, int h) {
+	boolean flush = cachedHint.width != w
+		&& isSensitiveHorizontally(container);
+	flush |= cachedHint.height != h
+		&& isSensitiveVertically(container);
+	if (flush) {
+		preferredSize = null;
+		cachedHint.width = w;
+		cachedHint.height = h;
+	}
+	return super.getPreferredSize(container, w, h);
+}
+
+/**
+ * Extends the superclass implementation to flush the cached minimum size.
+ * @see org.eclipse.draw2d.LayoutManager#invalidate() */
 public void invalidate() {
 	minimumSize = null;
 	super.invalidate();
+}
+
+/**
+ * Returns whether this layout manager is sensitive to changes in the horizontal hint. By
+ * default, this method returns <code>true</code>.
+ * @param container the layout's container
+ * @return <code>true</code> if this layout is sensite to horizontal hint changes
+ */
+protected boolean isSensitiveHorizontally(IFigure container) {
+	return true;
+}
+
+/**
+ * Returns whether this layout manager is sensitive to changes in the vertical hint. By
+ * default, this method returns <code>true</code>.
+ * @param container the layout's container
+ * @return <code>true</code> if this layout is sensite to vertical hint changes
+ */
+protected boolean isSensitiveVertically(IFigure container) {
+	return true;
 }
 
 }
