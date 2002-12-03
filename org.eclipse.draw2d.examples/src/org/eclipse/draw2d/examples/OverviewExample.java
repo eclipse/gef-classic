bd9c163cd0dae013b12cabaaef23ee4ef432f4f5
ebordeau <ebordeau>
2002-12-03 20:01:34 +0000

    *** empty log message ***

--- org.eclipse.draw2d.examples/src/org/eclipse/draw2d/examples/OverviewExample.java
+++ org.eclipse.draw2d.examples/src/org/eclipse/draw2d/examples/OverviewExample.java
@@ -3,82 +3,104 @@ package org.eclipse.draw2d.examples;
 import org.eclipse.draw2d.*;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Rectangle;
-import org.eclipse.draw2d.parts.Dock;
-import org.eclipse.draw2d.parts.DockLocator;
-import org.eclipse.draw2d.parts.ScrollableThumbnail;
 import org.eclipse.draw2d.parts.Thumbnail;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public class OverviewExample {
 
-private static Figure leftContents;
+private static Figure contents;
+private static Shell mainShell, overviewShell;
 private static Dimension offset = new Dimension();
-private static Thumbnail thumbnail;
 
 public static void main(String[] args) {
 	Display display = new Display();
-	Shell shell = new Shell(display);
-	shell.setLayout(new GridLayout(2, true));
-	FigureCanvas leftCanvas = new FigureCanvas(shell);
-	leftCanvas.setLayoutData(new GridData(GridData.FILL_BOTH));
-	leftCanvas.setContents(getLeftContents());
-	FigureCanvas rightCanvas = new FigureCanvas(shell);
-	rightCanvas.setLayoutData(new GridData(GridData.FILL_BOTH));
-	rightCanvas.setContents(getRightContents());
-	shell.setSize(600, 300);
-	shell.open();
-	while (!shell.isDisposed())
+	
+	mainShell = new Shell(display);
+	mainShell.setLayout(new FillLayout());
+	FigureCanvas mainCanvas = new FigureCanvas(mainShell);
+	mainCanvas.setContents(getContents());
+	
+	overviewShell = new Shell(display);
+	overviewShell.setLayout(new FillLayout());
+	FigureCanvas overviewCanvas = new FigureCanvas(overviewShell);
+	overviewCanvas.setContents(createThumbnail(getContents()));
+
+	mainShell.setSize(600, 600);
+	mainShell.open();
+	overviewShell.setSize(200, 200);
+	overviewShell.open();
+
+	while (!mainShell.isDisposed())
 		if (!display.readAndDispatch())
 			display.sleep();
+	mainShell.dispose();
+	overviewShell.dispose();
 }
 
 
-protected static Figure getLeftContents() {
-	leftContents = new Figure();
-	leftContents.setBorder(new LineBorder());
-	leftContents.setLayoutManager(new XYLayout());
-	Figure figure1 = new RectangleFigure();
+protected static Figure getContents() {
+	if (contents == null)
+		contents = createContents();
+	return contents;
+}
+
+private static Figure createContents() {	
+	Figure contents = new Figure();
+	contents.setBorder(new LineBorder());
+	contents.setLayoutManager(new XYLayout());
+	final Figure figure1 = new RectangleFigure();
 	figure1.setBackgroundColor(ColorConstants.green);
 	figure1.setBounds(new Rectangle(50, 50, 200, 200));
-	leftContents.add(figure1);
-	Figure figure2 = new RectangleFigure();
+	figure1.addMouseListener(new MouseListener.Stub() {
+		public void mousePressed(MouseEvent event) {
+			offset.width = event.x - figure1.getLocation().x;
+			offset.height = event.y - figure1.getLocation().y;
+		}
+		public void mouseReleased(MouseEvent event) {
+			offset.width = 0;
+			offset.height = 0;
+		}
+	});
+	figure1.addMouseMotionListener(new MouseMotionListener.Stub() {
+		public void mouseDragged(MouseEvent event) {
+			Rectangle rect = figure1.getBounds().getCopy();
+			rect.x = event.x - offset.width;
+			rect.y = event.y - offset.height;
+			figure1.setBounds(rect);
+		}
+	});
+	contents.add(figure1);
+	final Figure figure2 = new RectangleFigure();
 	figure2.setBackgroundColor(ColorConstants.blue);
 	figure2.setBounds(new Rectangle(350, 350, 150, 200));
-	leftContents.add(figure2);
-	return leftContents;
+	figure2.addMouseListener(new MouseListener.Stub() {
+		public void mousePressed(MouseEvent event) {
+			offset.width = event.x - figure2.getLocation().x;
+			offset.height = event.y - figure2.getLocation().y;
+		}
+		public void mouseReleased(MouseEvent event) {
+			offset.width = 0;
+			offset.height = 0;
+		}
+	});
+	figure2.addMouseMotionListener(new MouseMotionListener.Stub() {
+		public void mouseDragged(MouseEvent event) {
+			Rectangle rect = figure2.getBounds().getCopy();
+			rect.x = event.x - offset.width;
+			rect.y = event.y - offset.height;
+			figure2.setBounds(rect);
+		}
+	});
+	contents.add(figure2);
+	return contents;
 }
 
-protected static Figure getRightContents() {
-	LayeredPane topLayers = new LayeredPane();
-
-	Layer scrollPaneLayer = new Layer();
-	LayeredPane innerLayers = new LayeredPane();
-
-	Layer layer = new Layer();
-	layer.setLayoutManager(new StackLayout());
-	innerLayers.add(layer, "primary layer");
-
-	ScrollPane pane = new ScrollPane();
-	pane.setViewport(new Viewport(true));
-	pane.setView(innerLayers);
-
-	scrollPaneLayer.setLayoutManager(new StackLayout());
-	scrollPaneLayer.add(pane);
-	topLayers.add(scrollPaneLayer);
-
-	Layer dockLayer = new Layer();
-	dockLayer.setLayoutManager(new DelegatingLayout());
-	thumbnail = new ScrollableThumbnail(pane.getViewport());
-	thumbnail.setSource(leftContents);
-	Dock dock = new Dock(pane.getViewport(), thumbnail);
-	dock.setTitle("Overview");
-	dockLayer.add(dock, new DockLocator());
-	topLayers.add(dockLayer);
-
-	return topLayers;
+protected static Figure createThumbnail(Figure source) {
+	Thumbnail thumbnail = new Thumbnail();
+	thumbnail.setSource(source);
+	return thumbnail;
 }
 
 }
