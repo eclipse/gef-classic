f6ffffdd4dd89b5de25424c4587f131537715f33
ebordeau <ebordeau>
2002-12-09 21:09:30 +0000

    Delete all connections, as well as item requesting deletion.

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/model/DeleteCommand.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/model/DeleteCommand.java
@@ -6,6 +6,9 @@ package org.eclipse.gef.examples.logicdesigner.model;
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.gef.commands.AbstractCommand;
 import org.eclipse.gef.examples.logicdesigner.LogicMessages;
 
@@ -16,9 +19,31 @@ public class DeleteCommand
 private LogicSubpart child;
 private LogicDiagram parent;
 private int index = -1;
+private List sourceConnections = new ArrayList();
+private List targetConnections = new ArrayList();
 
 public DeleteCommand() {
 	super(LogicMessages.DeleteCommand_Label);
+}
+
+private void deleteConnections(LogicSubpart part) {
+	if (part instanceof LogicDiagram) {
+		List children = ((LogicDiagram)part).getChildren();
+		for (int i = 0; i < children.size(); i++)
+			deleteConnections((LogicSubpart)children.get(i));
+	}
+	sourceConnections.addAll(part.getSourceConnections());
+	for (int i = 0; i < sourceConnections.size(); i++) {
+		Wire wire = (Wire)sourceConnections.get(i);
+		wire.detachSource();
+		wire.detachTarget();
+	}
+	targetConnections.addAll(part.getTargetConnections());
+	for (int i = 0; i < targetConnections.size(); i++) {
+		Wire wire = (Wire)targetConnections.get(i);
+		wire.detachSource();
+		wire.detachTarget();
+	}
 }
 
 public void execute() {
@@ -26,12 +51,28 @@ public void execute() {
 }
 
 protected void primExecute() {
-	index=parent.getChildren().indexOf(child);
+	deleteConnections(child);
+	index = parent.getChildren().indexOf(child);
 	parent.removeChild(child);
 }
 
 public void redo() {
 	primExecute();
+}
+
+private void restoreConnections() {
+	for (int i = 0; i < sourceConnections.size(); i++) {
+		Wire wire = (Wire)sourceConnections.get(i);
+		wire.attachSource();
+		wire.attachTarget();
+	}
+	sourceConnections.clear();
+	for (int i = 0; i < targetConnections.size(); i++) {
+		Wire wire = (Wire)targetConnections.get(i);
+		wire.attachSource();
+		wire.attachTarget();
+	}
+	targetConnections.clear();
 }
 
 public void setChild (LogicSubpart c) {
@@ -44,6 +85,7 @@ public void setParent(LogicDiagram p) {
 
 public void undo() {
 	parent.addChild(child, index);
+	restoreConnections();
 }
 
 }
