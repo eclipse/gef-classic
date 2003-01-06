cb3f46d13c82d614b6fbc4f391b0bac33f081af6
dlee <dlee>
2003-01-06 19:20:04 +0000

    Action to print in GEF

---
+++ org.eclipse.gef/src/org/eclipse/gef/ui/actions/PrintAction.java
@@ -0,0 +1,63 @@
+package org.eclipse.gef.ui.actions;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.printing.PrintDialog;
+import org.eclipse.swt.printing.Printer;
+import org.eclipse.swt.printing.PrinterData;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchActionConstants;
+
+import org.eclipse.gef.GraphicalViewer;
+import org.eclipse.gef.LayerConstants;
+import org.eclipse.gef.PrintGraphicalViewerOperation;
+import org.eclipse.gef.editparts.LayerManager;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.PrintFigureOperation;
+
+/**
+ * @author hudsonr
+ * @since 2.1
+ */
+public class PrintAction extends EditorPartAction {
+
+/**
+ * Constructor for PrintAction.
+ * @param editor The EditorPart associated with this PrintAction
+ */
+public PrintAction(IEditorPart editor) {
+	super(editor);
+}
+
+/**
+ * @see org.eclipse.gef.ui.actions.EditorPartAction#calculateEnabled()
+ */
+protected boolean calculateEnabled() {
+	return true;
+}
+
+/**
+ * @see org.eclipse.jface.action.Action#getId()
+ */
+public String getId() {
+	return IWorkbenchActionConstants.PRINT;
+}
+
+/**
+ * @see org.eclipse.jface.action.Action#run()
+ */
+public void run() {
+	GraphicalViewer viewer;
+	viewer = (GraphicalViewer)getEditorPart().getAdapter(GraphicalViewer.class);
+	
+	PrintDialog dialog = new PrintDialog(viewer.getControl().getShell(), SWT.NULL);
+	PrinterData data = dialog.open();
+	
+	if (data != null) {
+		PrintGraphicalViewerOperation op = 
+					new PrintGraphicalViewerOperation(new Printer(data), viewer);
+		op.run(getEditorPart().getTitle());
+	}	
+}
+
+}
