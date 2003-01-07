862e94ddd1924bf3405f89d9b83a3b2f8773e316
rhudson <rhudson>
2003-01-07 16:49:15 +0000

    *** empty log message ***

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/LogicColorConstants.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/LogicColorConstants.java
@@ -11,13 +11,18 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.draw2d.FigureUtilities;
 
 public interface LogicColorConstants
-	extends ColorConstants{
-	public final static Color 
-		logicGreen = new Color(null, 123,174,148),
-		logicHighlight = new Color(null,66,166,115),
-		connectorGreen = new Color(null,0,69,40),
-		logicPrimarySelectedColor = new Color(null, 10,36,106),
-		logicSecondarySelectedColor = FigureUtilities.mixColors(logicPrimarySelectedColor, button),
-		logicBackgroundBlue = new Color(null, 200, 200, 240);
+	extends ColorConstants
+{
+
+public final static Color andGate = new Color(null, 220,70,80);
+public final static Color orGate = new Color (null, 0, 134, 255);
+public final static Color xorGate = new Color (null, 240,240,40);
+public final static Color logicGreen = new Color(null, 123,174,148);
+public final static Color logicHighlight = new Color(null,66,166,115);
+public final static Color connectorGreen = new Color(null,0,69,40);
+public final static Color logicPrimarySelectedColor = new Color(null, 10,36,106);
+public final static Color logicSecondarySelectedColor =
+	FigureUtilities.mixColors(logicPrimarySelectedColor, button);
+public final static Color logicBackgroundBlue = new Color(null, 200, 200, 240);
 
 }
