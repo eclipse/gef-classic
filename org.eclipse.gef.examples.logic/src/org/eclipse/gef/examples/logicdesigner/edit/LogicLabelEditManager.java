3cafc9353118f73d82ab46e0556dab02fcb4a6d4
rhudson <rhudson>
2003-01-07 17:39:31 +0000

    Zoom change

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/edit/LogicLabelEditManager.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/edit/LogicLabelEditManager.java
@@ -6,15 +6,22 @@ package org.eclipse.gef.examples.logicdesigner.edit;
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Text;
 
+import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
+import org.eclipse.draw2d.geometry.*;
+
 import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.tools.CellEditorLocator;
 import org.eclipse.gef.tools.DirectEditManager;
 
 public class LogicLabelEditManager 
 	extends DirectEditManager {
+
+Font scaledFont;
 
 public LogicLabelEditManager(
 		GraphicalEditPart source,
@@ -24,11 +31,32 @@ public LogicLabelEditManager(
 	super(source, editorType, locator);
 }
 
+/**
+ * @see org.eclipse.gef.tools.DirectEditManager#bringDown()
+ */
+protected void bringDown() {
+	//This method might be re-entered when super.bringDown() is called.
+	Font disposeFont = scaledFont;
+	scaledFont = null;
+	super.bringDown();
+	if (disposeFont != null)
+		disposeFont.dispose();	
+}
+
 protected void initCellEditor() {
-	String initialLabelText = ((Label)((GraphicalEditPart)getEditPart()).getFigure()).getText();
+	Label label = (Label)((GraphicalEditPart)getEditPart()).getFigure();
+	String initialLabelText = label.getText();
 	getCellEditor().setValue(initialLabelText);
 	Text text = (Text)getCellEditor().getControl();
-	text.setFont(((GraphicalEditPart)getEditPart()).getFigure().getFont());
+	IFigure figure = ((GraphicalEditPart)getEditPart()).getFigure();
+	scaledFont = figure.getFont();
+	FontData data = scaledFont.getFontData()[0];
+	Dimension fontSize = new Dimension(0, data.height);
+	label.translateToAbsolute(fontSize);
+	data.height = fontSize.height;
+	scaledFont = new Font(null, data);
+	
+	text.setFont(scaledFont);
 	text.selectAll();
 }
 
