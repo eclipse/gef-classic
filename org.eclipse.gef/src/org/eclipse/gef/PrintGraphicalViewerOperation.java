fe094f328b11050925bacdd1aefd044305e2d304
dlee <dlee>
2003-01-08 14:20:24 +0000

    Removed unnecessary constructor, now inherits from PrintFigureOperation

--- org.eclipse.gef/src/org/eclipse/gef/PrintGraphicalViewerOperation.java
+++ org.eclipse.gef/src/org/eclipse/gef/PrintGraphicalViewerOperation.java
@@ -3,37 +3,19 @@ package org.eclipse.gef;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.PrintOperation;
+import org.eclipse.draw2d.PrintFigureOperation;
 import org.eclipse.gef.editparts.LayerManager;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.printing.Printer;
-import org.eclipse.swt.widgets.Display;
 
 /**
  * @author danlee
  */
-public class PrintGraphicalViewerOperation extends PrintOperation {
+public class PrintGraphicalViewerOperation extends PrintFigureOperation {
 
 private GraphicalViewer viewer;
 private List selectedEditParts;
-
-/**
- * Constructor for PrintGraphicalViewerOperation.
- */
-public PrintGraphicalViewerOperation() {
-	super();
-}
-
-/**
- * Constructor for PrintGraphicalViewerOperation.
- * 
- * @param p The Printer to print to
- */
-public PrintGraphicalViewerOperation(Printer p) {
-	super(p);
-}
 
 /**
  * Constructor for PrintGraphicalViewerOperation
@@ -42,10 +24,14 @@ public PrintGraphicalViewerOperation(Printer p) {
 public PrintGraphicalViewerOperation(Printer p, GraphicalViewer g) {
 	super(p);
 	viewer = g;
+	LayerManager lm = (LayerManager)viewer.getEditPartRegistry().get(LayerManager.ID);
+	IFigure f = lm.getLayer(LayerConstants.PRINTABLE_LAYERS);
+	setPrintSource(f);
 }	
 	
 /**
  * Returns the viewer.
+ * 
  * @return GraphicalViewer
  */
 public GraphicalViewer getViewer() {
@@ -62,21 +48,6 @@ protected void preparePrintSource() {
 }
 
 /**
- * @see org.eclipse.draw2d.PrintOperation#printPages()
- */
-protected void printPages() {
-	getPrinter().startPage();
-
-	Graphics g = getFreshPrinterGraphics();
-	
-	LayerManager lm = (LayerManager)viewer.getEditPartRegistry().get(LayerManager.ID);
-	IFigure f = lm.getLayer(LayerConstants.PRINTABLE_LAYERS);
-	setupPrinterGraphicsFor(g, f);
-	f.paint(g);
-	getPrinter().endPage();
-}
-
-/**
  * @see org.eclipse.draw2d.PrintOperation#restorePrintSource()
  */
 protected void restorePrintSource() {
@@ -85,23 +56,8 @@ protected void restorePrintSource() {
 }
 
 /**
- * Sets up Graphics object g for IFigure f.
- * 
- * @param g The Graphics to setup
- * @param f The IFigure used to setup g
- */
-protected void setupPrinterGraphicsFor(Graphics g, IFigure f) {
-	g.setForegroundColor(f.getForegroundColor());
-	g.setBackgroundColor(f.getBackgroundColor());
-	g.setFont(f.getFont());
-
-	g.scale((double)getPrinter().getDPI().x / Display.getDefault().getDPI().x);
-	g.translate(f.getBounds().getCopy().getLocation().negate());
-	g.clipRect(f.getBounds());
-}
-
-/**
  * Sets the viewer.
+ * 
  * @param viewer The viewer to set
  */
 public void setViewer(GraphicalViewer viewer) {
