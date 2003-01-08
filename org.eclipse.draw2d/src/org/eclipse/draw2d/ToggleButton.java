8569e022eee60841c8106c9dba1b95a028193b6b
pshah <pshah>
2003-01-08 19:32:46 +0000

    pressed state background

--- org.eclipse.draw2d/src/org/eclipse/draw2d/ToggleButton.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/ToggleButton.java
@@ -6,17 +6,22 @@ package org.eclipse.draw2d;
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
+
+import org.eclipse.draw2d.geometry.Insets;
+import org.eclipse.draw2d.geometry.Rectangle;
 
 /**
  * A Toggle that appears like a 3-dimensional button.
  */
 public class ToggleButton
-	extends Toggle{
+	extends Toggle
+{
 
 protected Label label = null;
 
-public ToggleButton(){
+public ToggleButton() {
 }
 
 /**
@@ -25,7 +30,7 @@ public ToggleButton(){
  * 
  * @since 2.0
  */
-public ToggleButton(IFigure contents){
+public ToggleButton(IFigure contents) {
 	super(contents);
 }
 
@@ -35,7 +40,7 @@ public ToggleButton(IFigure contents){
  * 
  * @since 2.0
  */
-public ToggleButton(String text){
+public ToggleButton(String text) {
 	this(text, null);
 }
 
@@ -45,7 +50,7 @@ public ToggleButton(String text){
  * 
  * @since 2.0
  */
-public ToggleButton(String text, Image normalIcon){
+public ToggleButton(String text, Image normalIcon) {
 	super(text, normalIcon);
 }
 
@@ -55,9 +60,40 @@ public ToggleButton(String text, Image normalIcon){
  * 
  * @since 2.0
  */
-protected void init (){
+protected void init() {
 	setStyle(STYLE_BUTTON);
 	super.init();
+}
+
+/**
+ * @see org.eclipse.draw2d.Figure#paintFigure(Graphics)
+ */
+protected void paintFigure(Graphics graphics) {
+	if (isSelected() && isOpaque()) {
+		fillCheckeredRectangle(graphics);
+	} else {
+		super.paintFigure(graphics);
+	}
+}
+
+/**
+ * Helper method that can fill a rectangle of given dimensions with a checkered pattern
+ * using the foreground and background colors of the given Graphics object.
+ *  * @param graphics	The Graphics object used to paint * @param x		The x location starting from where the pattern has to be drawn * @param y		The y location starting from where the pattern has to be drawn * @param width	The width of the pattern * @param height	The height of the pattern */
+protected void fillCheckeredRectangle(Graphics graphics) {
+
+	graphics.setBackgroundColor(ColorConstants.button);
+	graphics.setForegroundColor(ColorConstants.buttonLightest);
+	Rectangle rect = getClientArea(Rectangle.SINGLETON).crop(new Insets(1, 1, 0, 0));
+	graphics.fillRectangle(rect.x, rect.y, rect.width, rect.height);
+	
+	graphics.clipRect(rect);
+	graphics.translate(rect.x, rect.y);
+	int n = rect.width + rect.height;
+	for (int i = 1; i < n; i += 2) {
+		graphics.drawLine(0, i, i, 0);
+	}
+	graphics.restoreState();
 }
 
 }
