af1a00f89b6bf8d2f7aefb053d24ea3097b6cebd
rhudson <rhudson>
2003-01-29 18:37:15 +0000

    PolylineConnection was fixed, rolling back

--- org.eclipse.gef/src/org/eclipse/gef/editparts/AbstractConnectionEditPart.java
+++ org.eclipse.gef/src/org/eclipse/gef/editparts/AbstractConnectionEditPart.java
@@ -24,22 +24,10 @@ public abstract class AbstractConnectionEditPart
 	implements ConnectionEditPart, LayerConstants
 {
 
-/* This fixes a memory leak where a PolylineConnection being deleted would add itself to
- * these anchors as an AnchorListener even though it was being deleted.  This happens
- * after removeNotify() gets called on the PolylineConnection, so unhooking the anchor
- * there does no good.  As far as I know, these are only here to allow developers to see
- * that something went wrong, so nothing should be listening to them anyway.
- */
 private static final ConnectionAnchor DEFAULT_SOURCE_ANCHOR =
-	new XYAnchor(new Point(10, 10)) {
-		public void addAnchorListener(AnchorListener listener) {}
-		public void removeAnchorListener(AnchorListener listener) {}
-	};
+	new XYAnchor(new Point(10, 10));
 private static final ConnectionAnchor DEFAULT_TARGET_ANCHOR =
-	new XYAnchor(new Point(100, 100)) {
-		public void addAnchorListener(AnchorListener listener) {}
-		public void removeAnchorListener(AnchorListener listener) {}
-	};
+	new XYAnchor(new Point(100, 100));
 
 /**
  * Provides accessibility support for when connections are also themselves nodes. If a
