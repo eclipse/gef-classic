f4e1058d9a12a9ee8996f2a0d611e610a2a3bb2e
pshah <pshah>
2003-02-05 19:16:45 +0000

    removed unnecessary casts

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/palette/LogicEntryPage.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/palette/LogicEntryPage.java
@@ -1,6 +1,5 @@
 package org.eclipse.gef.examples.logicdesigner.palette;
 
-import org.eclipse.gef.palette.PaletteEntry;
 import org.eclipse.gef.ui.palette.customize.DefaultEntryPage;
 
 /**
@@ -14,14 +13,14 @@ protected static final String ERROR_MESSAGE = "Name contains invalid character: 
  * @see org.eclipse.gef.ui.palette.customize.DefaultEntryPage#handleDescriptionChanged(String)
  */
 protected void handleDescriptionChanged(String text) {
-	((PaletteEntry)getEntry()).setDescription(text);
+	getEntry().setDescription(text);
 }
 
 /**
  * @see org.eclipse.gef.ui.palette.customize.DefaultEntryPage#handleHiddenSelected(boolean)
  */
 protected void handleHiddenSelected(boolean b) {
-	((PaletteEntry)getEntry()).setVisible(!b);
+	getEntry().setVisible(!b);
 }
 
 /**
@@ -31,7 +30,7 @@ protected void handleNameChanged(String text) {
 	if( text.indexOf('*') >= 0 ){
 		getPageContainer().showProblem(ERROR_MESSAGE);
 	} else {
-		((PaletteEntry)getEntry()).setLabel(text);
+		getEntry().setLabel(text);
 		getPageContainer().clearProblem();
 	}
 }
