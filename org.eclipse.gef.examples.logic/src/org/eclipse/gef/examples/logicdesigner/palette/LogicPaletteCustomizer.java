a459a60bd0c48aefdaf90bb900b548c080a0196d
pshah <pshah>
2003-02-05 19:16:59 +0000

    PaletteCustomizer changes

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/palette/LogicPaletteCustomizer.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/palette/LogicPaletteCustomizer.java
@@ -1,7 +1,6 @@
 package org.eclipse.gef.examples.logicdesigner.palette;
 
-import java.util.List;
-
+import org.eclipse.gef.palette.PaletteCategory;
 import org.eclipse.gef.palette.PaletteContainer;
 import org.eclipse.gef.palette.PaletteGroup;
 import org.eclipse.gef.palette.PaletteContainer;
@@ -9,8 +8,16 @@ import org.eclipse.gef.palette.PaletteEntry;
 import org.eclipse.gef.palette.PaletteSeparator;
 import org.eclipse.gef.palette.PaletteToolEntry;
 import org.eclipse.gef.ui.palette.PaletteCustomizer;
+import org.eclipse.gef.ui.palette.PaletteMessages;
 import org.eclipse.gef.ui.palette.customize.EntryPage;
 import org.eclipse.gef.ui.palette.customize.ReadOnlyEntryPage;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 
 /**
  * @author Pratik Shah
@@ -18,7 +25,7 @@ import org.eclipse.gef.ui.palette.customize.ReadOnlyEntryPage;
 public class LogicPaletteCustomizer 
 	extends PaletteCustomizer 
 {
-
+	
 /**
  * Tools cannot be deleted
  * 
@@ -45,6 +52,7 @@ public boolean canMoveDown(PaletteEntry entry) {
 			return false;
 		}
 	}
+	
 	return super.canMoveDown(entry);
 }
 
@@ -78,44 +86,10 @@ public EntryPage getPropertiesPage(PaletteEntry entry) {
 	if( entry instanceof PaletteSeparator || entry instanceof PaletteToolEntry || 
 	    entry instanceof PaletteGroup ){
 		return new ReadOnlyEntryPage();
+	} else if (entry instanceof PaletteCategory) {
+		return new CategoryEntryPage();
 	} else {
 		return new LogicEntryPage();
-	}
-}
-
-/**
- * Optimized version
- * 
- * @see org.eclipse.gef.ui.palette.PaletteCustomizer#performMoveDown(PaletteEntry)
- */
-public void performMoveDown(PaletteEntry entry){
-	PaletteContainer parent = (PaletteContainer)entry.getParent();
-	if( !parent.moveDown(entry) ){
-		// This is the case of a PaletteEntry that is its parent's last child
-		parent.remove(entry);
-		PaletteContainer grandparent = (PaletteContainer)parent.getParent();
-		List parents = grandparent.getChildren();
-		int index = parents.indexOf( parent );
-		PaletteContainer sibling = (PaletteContainer)parents.get(index+1);
-		sibling.add(0, entry);
-	}
-}
-
-/**
- * Optimized version
- * 
- * @see org.eclipse.gef.ui.palette.PaletteCustomizer#performMoveUp(PaletteEntry)
- */
-public void performMoveUp(PaletteEntry entry){
-	PaletteContainer parent = (PaletteContainer)entry.getParent();
-	if( !parent.moveUp(entry) ){
-		//This is the case of a PaletteEntry that is its parent's first child
-		parent.remove(entry);
-		PaletteContainer grandparent = (PaletteContainer)parent.getParent();
-		List parents = grandparent.getChildren();
-		int index = parents.indexOf( parent );
-		PaletteContainer sibling = (PaletteContainer)parents.get(index-1);
-		sibling.add(entry);
 	}
 }
 
@@ -130,6 +104,58 @@ public void revertToSaved() {
  * @see org.eclipse.gef.ui.palette.PaletteCustomizer#dialogClosed(PaletteEntry)
  */
 public void save() {
+}
+
+private class CategoryEntryPage extends LogicEntryPage {
+	private Button b, pinOption;
+	
+	public void createControl(Composite parent, PaletteEntry entry) {
+		super.createControl(parent, entry);
+		
+		b = new Button(panel, SWT.CHECK);
+		b.setFont(panel.getFont());
+		b.setText(PaletteMessages.EXPAND_LABEL);
+		b.setSelection(getCategory().isInitiallyOpen());
+		b.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleExpandSelected(((Button)e.getSource()).getSelection());
+			}
+		});
+		
+		pinOption = new Button(panel, SWT.CHECK);
+		pinOption.setFont(panel.getFont());
+		pinOption.setText(PaletteMessages.CATEGORY_PIN);
+		GridData data = new GridData();
+		data.horizontalIndent = 15;
+		pinOption.setLayoutData(data);
+		pinOption.setEnabled(b.getSelection());
+		pinOption.setSelection(getCategory().isInitiallyPinned());
+		pinOption.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handlePinSelected(((Button)e.getSource()).getSelection());
+			}
+		});
+	}
+	
+	private PaletteCategory getCategory(){
+		return (PaletteCategory)entry;
+	}
+	
+	private void handleExpandSelected(boolean selection) {
+		int status = selection ? PaletteCategory.INITIAL_STATUS_EXPANDED
+		                       : PaletteCategory.INITIAL_STATUS_COLLAPSED;
+		getCategory().setInitialStatus(status);
+		pinOption.setEnabled(selection);
+		if (!selection) {
+			pinOption.setSelection(false);
+		}
+	}
+	
+	private void handlePinSelected(boolean selection) {
+		int status = selection ? PaletteCategory.INITIAL_STATUS_PINNED_OPEN
+		                       : PaletteCategory.INITIAL_STATUS_EXPANDED;
+		getCategory().setInitialStatus(status);
+	}
 }
 
 }
