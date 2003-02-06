5f9204822cdfe33c4c6e07f744e0416fbc5d873e
rhudson <rhudson>
2003-02-06 16:10:50 +0000

    *** empty log message ***

--- org.eclipse.gef/src/org/eclipse/gef/internal/SharedImages.java
+++ org.eclipse.gef/src/org/eclipse/gef/internal/SharedImages.java
@@ -5,12 +5,6 @@ package org.eclipse.gef.internal;
  * US Government Users Restricted Rights - Use, duplication or disclosure
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  */
-/*
- * Licensed Material - Property of IBM 
- * (C) Copyright IBM Corp. 2001 - All Rights Reserved.
- * US Government Users Restricted Rights - Use, duplication or disclosure 
- * restricted by GSA ADP Schedule Contract with IBM Corp. 
- */
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Image;
@@ -19,7 +13,6 @@ public class SharedImages {
 
 public static final ImageDescriptor DESC_ZOOM_IN;
 public static final ImageDescriptor DESC_ZOOM_OUT;
-
 
 public static final ImageDescriptor DESC_HORZ_ALIGN_CENTER;
 public static final ImageDescriptor DESC_HORZ_ALIGN_LEFT;
@@ -31,14 +24,6 @@ public static final ImageDescriptor DESC_VERT_ALIGN_BOTTOM;
 
 public static final ImageDescriptor ICON_MATCH_WIDTH;
 public static final ImageDescriptor ICON_MATCH_HEIGHT;
-
-public static final Image ICON_DOWN;
-public static final Image ICON_DOWN_GRAYED;
-public static final Image ICON_DOWN_PRESSED;
-
-public static final Image ICON_UP;
-public static final Image ICON_UP_GRAYED;
-public static final Image ICON_UP_PRESSED;
 
 static {
 	DESC_ZOOM_IN = createDescriptor("icons/zoomplus.gif"); //$NON-NLS-1$
@@ -53,14 +38,6 @@ static {
 
 	ICON_MATCH_WIDTH = createDescriptor("icons/sizehz.gif"); //$NON-NLS-1$
 	ICON_MATCH_HEIGHT = createDescriptor("icons/sizevt.gif"); //$NON-NLS-1$
-
-	ICON_DOWN = create("icons/down.gif"); //$NON-NLS-1$
-	ICON_DOWN_GRAYED = create("icons/downgray.gif"); //$NON-NLS-1$
-	ICON_DOWN_PRESSED = create("icons/downpressed.gif"); //$NON-NLS-1$
-
-	ICON_UP = create("icons/up.gif"); //$NON-NLS-1$
-	ICON_UP_GRAYED = create("icons/upgray.gif"); //$NON-NLS-1$
-	ICON_UP_PRESSED = create("icons/uppressed.gif"); //$NON-NLS-1$
 }
 
 static private Image create(String filename){
