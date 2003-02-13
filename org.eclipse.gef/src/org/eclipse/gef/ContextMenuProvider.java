f6e20f39257c558b2fe678d556f402a5751d31fe
ebordeau <ebordeau>
2003-02-13 21:57:24 +0000

    Removed references to ActionRegistry.  Also removed groups in menuAboutToShow().  Subclasses can add groups if they need them.

--- org.eclipse.gef/src/org/eclipse/gef/ContextMenuProvider.java
+++ org.eclipse.gef/src/org/eclipse/gef/ContextMenuProvider.java
@@ -6,24 +6,23 @@ package org.eclipse.gef;
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
-import org.eclipse.gef.ui.actions.ActionRegistry;
-import org.eclipse.gef.ui.actions.GEFActionConstants;
-import org.eclipse.jface.action.*;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.widgets.Menu;
+
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
 
 public abstract class ContextMenuProvider 
 	implements IMenuListener, DisposeListener
 {
 
-private ActionRegistry registry;
 private EditPartViewer viewer;
 private MenuManager menuManager;
 
-public ContextMenuProvider(EditPartViewer viewer, ActionRegistry registry) {
+public ContextMenuProvider(EditPartViewer viewer) {
 	setViewer(viewer);
-	setActionRegistry(registry);
 	createContextMenu();
 }
 
@@ -42,10 +41,6 @@ public Menu createContextMenu() {
 
 public void dispose() {}
 
-protected ActionRegistry getActionRegistry() {
-	return registry;
-}
-
 public MenuManager getMenuManager() {
 	return menuManager;
 }
@@ -57,21 +52,7 @@ protected EditPartViewer getViewer() {
 public void menuAboutToShow(IMenuManager menu) {
 	//Any async paints won't be dispatched while the menu is visible.
 	getViewer().flush();
-	// As long as setRemoveAllWhenShown is set to true, we must re-add these.
-	menu.add(new Separator(GEFActionConstants.GROUP_UNDO));
-	menu.add(new Separator(GEFActionConstants.GROUP_COPY));
-	menu.add(new Separator(GEFActionConstants.GROUP_PRINT));
-	menu.add(new Separator(GEFActionConstants.GROUP_EDIT));
-	menu.add(new Separator(GEFActionConstants.GROUP_FIND));
-	menu.add(new Separator(GEFActionConstants.GROUP_ADD));
-	menu.add(new Separator(GEFActionConstants.GROUP_REST));
-	menu.add(new Separator(GEFActionConstants.MB_ADDITIONS));
-	menu.add(new Separator(GEFActionConstants.GROUP_SAVE));
 	buildContextMenu(menu);
-}
-
-protected void setActionRegistry(ActionRegistry registry) {
-	this.registry = registry;
 }
 
 protected void setViewer(EditPartViewer viewer) {
