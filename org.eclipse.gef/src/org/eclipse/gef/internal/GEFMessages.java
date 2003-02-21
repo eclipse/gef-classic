2b7d0187611841a578bad69b36ff9f2b7a276940
rhudson <rhudson>
2003-02-21 05:30:47 +0000

    shortened translation keynames

--- org.eclipse.gef/src/org/eclipse/gef/internal/GEFMessages.java
+++ org.eclipse.gef/src/org/eclipse/gef/internal/GEFMessages.java
@@ -6,14 +6,17 @@ package org.eclipse.gef.internal;
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  */
 
-import java.util.*;
-import org.eclipse.core.runtime.*;
+import java.util.MissingResourceException;
+
+import org.eclipse.core.runtime.IPluginDescriptor;
+import org.eclipse.core.runtime.Platform;
 
 /**
  * Internal Messages
  * @author hudsonr
- * @since 2.0 */
-public interface GEFMessages {
+ * @since 2.0
+ */
+public class GEFMessages {
 
 /** */
 static class Helper {
@@ -29,73 +32,62 @@ static class Helper {
 		}
 	}
 }
+public static String AlignBottomAction_Label = Helper.getString("%AlignBottomAction.Label");//$NON-NLS-1$
+public static String AlignBottomAction_Tooltip = Helper.getString("%AlignBottomAction.Tooltip");//$NON-NLS-1$
+public static String AlignCenterAction_Label = Helper.getString("%AlignCenterAction.Label");//$NON-NLS-1$
+public static String AlignCenterAction_Tooltip = Helper.getString("%AlignCenterAction.Tooltip");//$NON-NLS-1$
+public static String AlignLeftAction_Label = Helper.getString("%AlignLeftAction.Label");//$NON-NLS-1$
+public static String AlignLeftAction_Tooltip = Helper.getString("%AlignLeftAction.Tooltip");//$NON-NLS-1$
+public static String AlignMiddleAction_Label = Helper.getString("%AlignMiddleAction.Label");//$NON-NLS-1$
+public static String AlignMiddleAction_Tooltip = Helper.getString("%AlignMiddleAction.Tooltip");//$NON-NLS-1$
+public static String AlignRightAction_Label = Helper.getString("%AlignRightAction.Label");//$NON-NLS-1$
+public static String AlignRightAction_Tooltip = Helper.getString("%AlignRightAction.Tooltip");//$NON-NLS-1$
+public static String AlignTopAction_Label = Helper.getString("%AlignTopAction.Label");//$NON-NLS-1$
+public static String AlignTopAction_Tooltip = Helper.getString("%AlignTopAction.Tooltip");//$NON-NLS-1$
 
-public String ZoomAction_ZoomIn_ActionToolTipText = Helper.getString("%ZoomAction.ZoomIn.ActionToolTipText_UI_");//$NON-NLS-1$
-public String ZoomAction_ZoomIn_ActionLabelText=Helper.getString("%ZoomAction.ZoomIn.ActionLabelText_UI_");			//$NON-NLS-1$
 
-public String ZoomAction_ZoomOut_ActionToolTipText = Helper.getString("%ZoomAction.ZoomOut.ActionToolTipText_UI_");//$NON-NLS-1$
-public String ZoomAction_ZoomOut_ActionLabelText=Helper.getString("%ZoomAction.ZoomOut.ActionLabelText_UI_");			//$NON-NLS-1$
+public static String CopyAction_ActionDeleteCommandName = Helper.getString("%CopyAction.ActionDeleteCommandName_UI_");//$NON-NLS-1$
+public static String CopyAction_Label = Helper.getString("%CopyAction.Label");//$NON-NLS-1$
+public static String CopyAction_Tooltip = Helper.getString("%CopyAction.Tooltip");//$NON-NLS-1$
 
-public String AlignLeftAction_ActionToolTipText = Helper.getString("%AlignLeftAction.ActionToolTipText_UI_");//$NON-NLS-1$
-public String AlignLeftAction_ActionLabelText=Helper.getString("%AlignLeftAction.ActionLabelText_UI_");			//$NON-NLS-1$
+public static String DeleteAction_ActionDeleteCommandName = Helper.getString("%DeleteAction.ActionDeleteCommandName_UI_");//$NON-NLS-1$
+public static String DeleteAction_Label = Helper.getString("%DeleteAction.Label");//$NON-NLS-1$
+public static String DeleteAction_Tooltip = Helper.getString("%DeleteAction.Tooltip");//$NON-NLS-1$
 
-public String AlignRightAction_ActionToolTipText=Helper.getString("%AlignRightAction.ActionToolTipText_UI_");		//$NON-NLS-1$
-public String AlignRightAction_ActionLabelText=Helper.getString("%AlignRightAction.ActionLabelText_UI_");			//$NON-NLS-1$
+public static String ERR_Assert_IsNotNull_Exception_AssertionFailed = Helper.getString("%Assert.IsNotNull.Exception.AssertionFailed_EXC_");//$NON-NLS-1$
+public static String ERR_Assert_IsTrue_Exception_AssertionFailed = Helper.getString("%Assert.IsTrue.Exception.AssertionFailed_EXC_");//$NON-NLS-1$
 
-public String AlignTopAction_ActionToolTipText=Helper.getString("%AlignTopAction.ActionToolTipText_UI_");		//$NON-NLS-1$
-public String AlignTopAction_ActionLabelText=Helper.getString("%AlignTopAction.ActionLabelText_UI_");			//$NON-NLS-1$
+public static String GraphicalEditor_CLOSE_BUTTON_UI = Helper.getString("%GraphicalEditor.CLOSE_BUTTON_UI_");//$NON-NLS-1$
+public static String GraphicalEditor_FILE_DELETED_TITLE_UI = Helper.getString("%GraphicalEditor.FILE_DELETED_TITLE_UI_");//$NON-NLS-1$
+public static String GraphicalEditor_FILE_DELETED_WITHOUT_SAVE_INFO = Helper.getString("%GraphicalEditor.FILE_DELETED_WITHOUT_SAVE_INFO_");//$NON-NLS-1$
+public static String GraphicalEditor_SAVE_BUTTON_UI = Helper.getString("%GraphicalEditor.SAVE_BUTTON_UI_");//$NON-NLS-1$
+public static String GraphicalRootEditPart_Overview_Title = Helper.getString("%GraphicalRootEditPart.Overview.Title_UI_");//$NON-NLS-1$
 
-public String AlignBottomAction_ActionToolTipText=Helper.getString("%AlignBottomAction.ActionToolTipText_UI_");		//$NON-NLS-1$
-public String AlignBottomAction_ActionLabelText=Helper.getString("%AlignBottomAction.ActionLabelText_UI_");			//$NON-NLS-1$
+//public static String PasteAction_ActionDeleteCommandName = Helper.getString("%PasteAction.ActionDeleteCommandName_UI_");//$NON-NLS-1$
+public static String PasteAction_Label = Helper.getString("%PasteAction.Label");//$NON-NLS-1$
+public static String PasteAction_Tooltip = Helper.getString("%PasteAction.Tooltip");//$NON-NLS-1$
 
-public String AlignCenterAction_ActionToolTipText=Helper.getString("%AlignCenterAction.ActionToolTipText_UI_");		//$NON-NLS-1$
-public String AlignCenterAction_ActionLabelText=Helper.getString("%AlignCenterAction.ActionLabelText_UI_");			//$NON-NLS-1$
+public static String PrintAction_ActionDeleteCommandName = Helper.getString("%PrintAction.ActionDeleteCommandName_UI_");//$NON-NLS-1$
+public static String PrintAction_Label = Helper.getString("%PrintAction.Label");//$NON-NLS-1$
+public static String PrintAction_Tooltip = Helper.getString("%PrintAction.Tooltip");//$NON-NLS-1$
 
-public String AlignMiddleAction_ActionToolTipText=Helper.getString("%AlignMiddleAction.ActionToolTipText_UI_");		//$NON-NLS-1$
-public String AlignMiddleAction_ActionLabelText=Helper.getString("%AlignMiddleAction.ActionLabelText_UI_");			//$NON-NLS-1$
+public static String RedoAction_Label = Helper.getString("%RedoAction.Label");//$NON-NLS-1$
+public static String RedoAction_Tooltip = Helper.getString("%RedoAction.Tooltip");//$NON-NLS-1$
+public static String RenameAction_Label = Helper.getString("%RenameAction.Label");//$NON-NLS-1$
+public static String RenameAction_Tooltip = Helper.getString("%RenameAction.Tooltip");//$NON-NLS-1$
+public static String SaveAction_Label = Helper.getString("%SaveAction.Label");//$NON-NLS-1$
+public static String SaveAction_Tooltip = Helper.getString("%SaveAction.Tooltip");//$NON-NLS-1$
+public static String SetPropertyValueCommand_Label = Helper.getString("%SetPropertyValueCommand.Label_UI_");//$NON-NLS-1$
 
-public String UndoAction_ActionLabelText=Helper.getString("%UndoAction.ActionLabelText_UI_");			//$NON-NLS-1$
-public String UndoAction_ActionShortcutText=Helper.getString("%UndoAction.ActionShortcutText_UI_");		//$NON-NLS-1$
-public String UndoAction_ToolTipText=Helper.getString("%UndoAction.ActionToolTipText_UI_");				//$NON-NLS-1$
+public static String SelectionTool_Label = Helper.getString("%SelectionTool.Label");//$NON-NLS-1$
+public static String MarqueeTool_Label = Helper.getString("%MarqueeTool.Label");//$NON-NLS-1$
+public static String UndoAction_Label = Helper.getString("%UndoAction.Label");//$NON-NLS-1$
+public static String UndoAction_Tooltip = Helper.getString("%UndoAction.Tooltip");//$NON-NLS-1$
 
-public String DeleteAction_ActionToolTipText=Helper.getString("%DeleteAction.ActionToolTipText_UI_");	//$NON-NLS-1$
-public String DeleteAction_ActionLabelText=Helper.getString("%DeleteAction.ActionLabelText_UI_");		//$NON-NLS-1$
-public String DeleteAction_ActionDeleteCommandName=Helper.getString("%DeleteAction.ActionDeleteCommandName_UI_");//$NON-NLS-1$
+public static String ZoomIn_Label = Helper.getString("%ZoomIn.Label");//$NON-NLS-1$
+public static String ZoomIn_Tooltip = Helper.getString("%ZoomIn.Tooltip");//$NON-NLS-1$
 
-public String PrintAction_ActionToolTipText=Helper.getString("%PrintAction.ActionToolTipText_UI_");	//$NON-NLS-1$
-public String PrintAction_ActionLabelText=Helper.getString("%PrintAction.ActionLabelText_UI_");		//$NON-NLS-1$
-public String PrintAction_ActionDeleteCommandName=Helper.getString("%PrintAction.ActionDeleteCommandName_UI_");//$NON-NLS-1$
-
-public String CopyAction_ActionToolTipText=Helper.getString("%CopyAction.ActionToolTipText_UI_");	//$NON-NLS-1$
-public String CopyAction_ActionLabelText=Helper.getString("%CopyAction.ActionLabelText_UI_");		//$NON-NLS-1$
-public String CopyAction_ActionDeleteCommandName=Helper.getString("%CopyAction.ActionDeleteCommandName_UI_");//$NON-NLS-1$
-
-public String PasteAction_ActionToolTipText=Helper.getString("%PasteAction.ActionToolTipText_UI_");	//$NON-NLS-1$
-public String PasteAction_ActionLabelText=Helper.getString("%PasteAction.ActionLabelText_UI_");		//$NON-NLS-1$
-public String PasteAction_ActionDeleteCommandName=Helper.getString("%PasteAction.ActionDeleteCommandName_UI_");//$NON-NLS-1$
-
-public String RedoAction_ActionToolTipText=Helper.getString("%RedoAction.ActionToolTipText_UI_");		//$NON-NLS-1$
-public String RedoAction_ActionLabelText=Helper.getString("%RedoAction.ActionLabelText_UI_");			//$NON-NLS-1$
-public String RedoAction_ActionShortcutText=Helper.getString("%RedoAction.ActionShortcutText_UI_");		//$NON-NLS-1$
-
-public String SaveAction_ActionToolTipText=Helper.getString("%SaveAction.ActionToolTipText_UI_");		//$NON-NLS-1$
-public String SaveAction_ActionLabelText=Helper.getString("%SaveAction.ActionLabelText_UI_");			//$NON-NLS-1$
-public String SaveAction_ActionShortcutText=Helper.getString("%SaveAction.ActionShortcutText_UI_");		//$NON-NLS-1$
-
-public String RenameAction_ActionToolTipText=Helper.getString("%RenameAction.ActionToolTipText_UI_");		//$NON-NLS-1$
-public String RenameAction_ActionLabelText=Helper.getString("%RenameAction.ActionLabelText_UI_");			//$NON-NLS-1$
-public String RenameAction_ActionShortcutText=Helper.getString("%RenameAction.ActionShortcutText_UI_");		//$NON-NLS-1$
-
-public String ERR_Assert_IsNotNull_Exception_AssertionFailed=Helper.getString("%Assert.IsNotNull.Exception.AssertionFailed_EXC_");	//$NON-NLS-1$
-public String ERR_Assert_IsTrue_Exception_AssertionFailed=Helper.getString("%Assert.IsTrue.Exception.AssertionFailed_EXC_");	//$NON-NLS-1$
-
-public String GraphicalEditor_FILE_DELETED_TITLE_UI=Helper.getString("%GraphicalEditor.FILE_DELETED_TITLE_UI_"); //$NON-NLS-1$
-public String GraphicalEditor_FILE_DELETED_WITHOUT_SAVE_INFO=Helper.getString("%GraphicalEditor.FILE_DELETED_WITHOUT_SAVE_INFO_");//$NON-NLS-1$
-public String GraphicalEditor_SAVE_BUTTON_UI=Helper.getString("%GraphicalEditor.SAVE_BUTTON_UI_");	//$NON-NLS-1$
-public String GraphicalEditor_CLOSE_BUTTON_UI=Helper.getString("%GraphicalEditor.CLOSE_BUTTON_UI_");	//$NON-NLS-1$
-
-public String GraphicalRootEditPart_Overview_Title=Helper.getString("%GraphicalRootEditPart.Overview.Title_UI_"); //$NON-NLS-1$
-
-public String SetPropertyValueCommand_Label=Helper.getString("%SetPropertyValueCommand.Label_UI_");		//$NON-NLS-1$
+public static String ZoomOut_Label = Helper.getString("%ZoomOut.Label");//$NON-NLS-1$
+public static String ZoomOut_Tooltip = Helper.getString("%ZoomOut.Tooltip");//$NON-NLS-1$
 
 }
