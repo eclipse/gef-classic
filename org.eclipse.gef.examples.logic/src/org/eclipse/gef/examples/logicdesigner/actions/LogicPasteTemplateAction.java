ba063699061031f02a482f6d3b35ca09c0198c1c
ebordeau <ebordeau>
2003-02-26 22:59:06 +0000

    Implemented getPasteLocation()

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/actions/LogicPasteTemplateAction.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/actions/LogicPasteTemplateAction.java
@@ -1,10 +1,12 @@
 package org.eclipse.gef.examples.logicdesigner.actions;
 
+import org.eclipse.ui.IEditorPart;
+
+import org.eclipse.draw2d.geometry.Point;
+
 import org.eclipse.gef.requests.CreationFactory;
 
 import org.eclipse.gef.examples.logicdesigner.model.LogicElementFactory;
-
-import org.eclipse.ui.IEditorPart;
 
 /**
  * @author Eric Bordeau
@@ -24,6 +26,14 @@ public LogicPasteTemplateAction(IEditorPart editor) {
  */
 protected CreationFactory getFactory(Object template) {
 	return new LogicElementFactory((String)template);
+}
+
+/**
+ * 
+ * @see org.eclipse.gef.examples.logicdesigner.actions.PasteTemplateAction#getPasteLocation()
+ */
+protected Point getPasteLocation() {
+	return new Point(10, 10);
 }
 
 }
