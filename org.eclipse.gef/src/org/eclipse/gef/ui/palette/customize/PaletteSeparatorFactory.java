4da42f40221077a87457f254553e6d95dffaf04f
pshah <pshah>
2003-02-27 19:52:37 +0000

    Separators can be created inside groups again.

--- org.eclipse.gef/src/org/eclipse/gef/ui/palette/customize/PaletteSeparatorFactory.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/palette/customize/PaletteSeparatorFactory.java
@@ -3,7 +3,6 @@ package org.eclipse.gef.ui.palette.customize;
 import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.gef.palette.PaletteEntry;
-import org.eclipse.gef.palette.PaletteGroup;
 import org.eclipse.gef.palette.PaletteSeparator;
 import org.eclipse.gef.ui.palette.PaletteMessages;
 
@@ -21,16 +20,6 @@ public class PaletteSeparatorFactory
  */
 public PaletteSeparatorFactory() {
 	setLabel(PaletteMessages.MODEL_TYPE_SEPARATOR);
-}
-
-/**
- * Separators cannot be inside groups.
- * 
- * @see org.eclipse.gef.ui.palette.customize.PaletteEntryFactory#canCreate(PaletteEntry)
- */
-public boolean canCreate(PaletteEntry selected) {
-	return super.canCreate(selected) && !(selected instanceof PaletteGroup) &&
-			!(selected.getParent() instanceof PaletteGroup);
 }
 
 /**
