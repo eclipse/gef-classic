8f5094df6f0f2bdee874bd218d48b7bbf3b0b723
rhudson <rhudson>
2003-03-06 15:10:29 +0000

    removed autogen comment

--- org.eclipse.gef/src/org/eclipse/gef/internal/ui/palette/editparts/GroupFigure.java
+++ org.eclipse.gef/src/org/eclipse/gef/internal/ui/palette/editparts/GroupFigure.java
@@ -1,9 +1,3 @@
-/*
- * Created on Feb 14, 2003
- *
- * To change this generated comment go to 
- * Window>Preferences>Java>Code Generation>Code Template
- */
 package org.eclipse.gef.internal.ui.palette.editparts;
 
 import org.eclipse.draw2d.Border;
@@ -18,7 +12,6 @@ private static final Border BORDER = new RaisedBorder(3, 3, 3, 3);
 
 {
 	setBorder(BORDER);
-//	setOpaque(true);
 }
 
 }
