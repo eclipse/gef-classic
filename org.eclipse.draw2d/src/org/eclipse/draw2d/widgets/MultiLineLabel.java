e2d40adbd05cdc648ef577a100eef98707584e40
rhudson <rhudson>
2003-03-06 15:36:19 +0000

    *** empty log message ***

--- org.eclipse.draw2d/src/org/eclipse/draw2d/widgets/MultiLineLabel.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/widgets/MultiLineLabel.java
@@ -1,10 +1,14 @@
 package org.eclipse.draw2d.widgets;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.accessibility.*;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Composite;
 
 import org.eclipse.draw2d.*;
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.text.FlowPage;
 import org.eclipse.draw2d.text.TextFlow;
 
@@ -60,6 +64,10 @@ public MultiLineLabel(Composite parent) {
 	page.add(textFlow);
 	setContents(page);
 	getViewport().setContentsTracksWidth(true);
+	addAccessibility();
+}
+
+private void addAccessibility() {
 	getAccessible().addAccessibleControlListener(new AccessibleControlAdapter() {
 		public void getRole(AccessibleControlEvent e) {
 			e.detail = ACC.ROLE_LABEL;
@@ -68,6 +76,35 @@ public MultiLineLabel(Composite parent) {
 	getAccessible().addAccessibleListener(new AccessibleAdapter() {
 		public void getName(AccessibleEvent e) {
 			e.result = getText();
+		}
+	});
+	addKeyListener(new KeyAdapter() {
+		public void keyPressed(KeyEvent e) {
+			Point p = getViewport().getViewLocation();
+			int dy = getFont().getFontData()[0].getHeight();
+			int dx = dy * 3 / 2;
+			if (e.keyCode == SWT.ARROW_DOWN) {
+				scrollToY(p.y + dy / 2);
+				scrollToY(p.y + dy);
+				scrollToY(p.y + dy * 3 / 2);
+				scrollToY(p.y + dy * 2);
+			}
+			if (e.keyCode == SWT.ARROW_UP) {
+				scrollToY(p.y - dy / 2);
+				scrollToY(p.y - dy);
+				scrollToY(p.y - dy * 3 / 2);
+				scrollToY(p.y - dy * 2);
+			}
+			if (e.keyCode == SWT.ARROW_RIGHT) {
+				scrollToX(p.x + dx);
+				scrollToX(p.x + dx * 2);
+				scrollToX(p.x + dx * 3);
+			}
+			if (e.keyCode == SWT.ARROW_LEFT) {
+				scrollToX(p.x - dx);
+				scrollToX(p.x - dx * 2);
+				scrollToX(p.x - dx * 3);
+			}
 		}
 	});
 }
