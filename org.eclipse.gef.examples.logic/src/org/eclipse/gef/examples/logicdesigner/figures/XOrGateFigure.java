42e70c45caf50094cb9815726a2402a2312106fd
rhudson <rhudson>
2003-03-06 15:02:15 +0000

    removed autogen comment

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/figures/XOrGateFigure.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/figures/XOrGateFigure.java
@@ -9,11 +9,6 @@ import org.eclipse.draw2d.geometry.Rectangle;
 
 /**
  * @author danlee
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
  */
 public class XOrGateFigure extends GateFigure {
 
