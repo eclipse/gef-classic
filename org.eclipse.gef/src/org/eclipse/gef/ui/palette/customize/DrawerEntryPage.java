1604ffe3c47deafc91188be4539b06b37181c88e
pshah <pshah>
2003-03-07 03:01:02 +0000

    Bug-fix where hide-only permission was not being respected; broke down in several methods so that it's easier to sub-class; JavaDoc

--- org.eclipse.gef/src/org/eclipse/gef/ui/palette/customize/DrawerEntryPage.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/palette/customize/DrawerEntryPage.java
@@ -12,62 +12,103 @@ import org.eclipse.gef.palette.PaletteEntry;
 import org.eclipse.gef.ui.palette.PaletteMessages;
 
 /**
+ * The default entry page for drawers.
+ * 
  * @author Pratik Shah
  */
 public class DrawerEntryPage 
 	extends DefaultEntryPage 
 {
 	
-private Button b, pinOption;
+private Button openDrawerOption, pinDrawerOption;
 
+/** * @see org.eclipse.gef.ui.palette.customize.EntryPage#createControl(Composite, PaletteEntry) */
 public void createControl(Composite parent, PaletteEntry entry) {
 	super.createControl(parent, entry);
 	
-	b = new Button(panel, SWT.CHECK);
+	openDrawerOption = createOpenDrawerInitiallyOption((Composite)getControl());
+	pinDrawerOption = createPinDrawerInitiallyOption((Composite)getControl());
+}
+
+/**
+ * Creates the button that provides the option to pin a drawer open at start-up.
+ * 
+ * @param panel	The parent Composite
+ * @return The button for the new option
+ */
+protected Button createOpenDrawerInitiallyOption(Composite panel) {
+	Button b = new Button(panel, SWT.CHECK);
 	b.setFont(panel.getFont());
 	b.setText(PaletteMessages.EXPAND_DRAWER_AT_STARTUP_LABEL);
 	b.setSelection(getDrawer().isInitiallyOpen());
-	if (!isReadOnly()) {
+	if (getPermission() >= getEntry().PERMISSION_LIMITED_MODIFICATION) {
 		b.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
-				handleExpandSelected(((Button)e.getSource()).getSelection());
+				handleOpenSelected(((Button)e.getSource()).getSelection());
 			}
 		});
 	} else {
 		b.setEnabled(false);
 	}
-	
-	pinOption = new Button(panel, SWT.CHECK);
+
+	return b;
+}
+
+/**
+ * Creates the button that provides the option to have a drawer open at start-up.
+ * 
+ * @param panel	The parent Composite
+ * @return The button for the new option
+ */
+protected Button createPinDrawerInitiallyOption(Composite panel) {
+	Button pinOption = new Button(panel, SWT.CHECK);
 	pinOption.setFont(panel.getFont());
 	pinOption.setText(PaletteMessages.DRAWER_PIN_AT_STARTUP);
 	GridData data = new GridData();
 	data.horizontalIndent = 15;
 	pinOption.setLayoutData(data);
-	pinOption.setEnabled(b.getSelection() && b.isEnabled());
+	pinOption.setEnabled(openDrawerOption.getSelection() && openDrawerOption.isEnabled());
 	pinOption.setSelection(getDrawer().isInitiallyPinned());
-	if (!isReadOnly()) {
+	if (getPermission() >= getEntry().PERMISSION_LIMITED_MODIFICATION) {
 		pinOption.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
 				handlePinSelected(((Button)e.getSource()).getSelection());
 			}
 		});
 	}
+	return pinOption;
 }
 
+/**
+ * Convenience method that provides access to the PaletteDrawer.
+ *  * @return the entry as a PaletteDrawer */
 protected PaletteDrawer getDrawer() {
-	return (PaletteDrawer)entry;
+	return (PaletteDrawer)getEntry();
 }
 
-protected void handleExpandSelected(boolean selection) {
+/**
+ * This method is invoked when the selection state of the option to open drawer at
+ * start-up is toggled.
+ * <p>
+ * It sets the initial state of the drawer accordingly.
+ *  * @param selection	<code>true</code> if that option is now selected */
+protected void handleOpenSelected(boolean selection) {
 	int status = selection ? PaletteDrawer.INITIAL_STATE_OPEN
 	                       : PaletteDrawer.INITIAL_STATE_CLOSED;
 	getDrawer().setInitialState(status);
-	pinOption.setEnabled(selection);
+	pinDrawerOption.setEnabled(selection);
 	if (!selection) {
-		pinOption.setSelection(false);
+		pinDrawerOption.setSelection(false);
 	}
 }
 
+/**
+ * This method is invoked when the selection state of the option to pin a drawer open at
+ * start-up is toggled.
+ * <p>
+ * It sets the initial state of the drawer accordingly.
+ *  * @param selection	<code>true</code> if that option is now selected
+ */
 protected void handlePinSelected(boolean selection) {
 	int status = selection ? PaletteDrawer.INITIAL_STATUS_PINNED_OPEN
 	                       : PaletteDrawer.INITIAL_STATE_OPEN;
