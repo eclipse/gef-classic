03ff1fa7ae1b33677476ee32b29133aa29c4c89f
rhudson <rhudson>
2003-03-10 03:28:01 +0000

    added search helper

--- org.eclipse.gef/src/org/eclipse/gef/AutoexposeHelper.java
+++ org.eclipse.gef/src/org/eclipse/gef/AutoexposeHelper.java
@@ -11,4 +11,27 @@ boolean detect(Point where);
 
 boolean step(Point where);
 
+/**
+ * Used with EditPartViewers to find the AutoexposeHelper at a Point. Clients can
+ * instantiate the search, call {@link EditPartViewer#findObjectAtExcluding(Point,
+ * Collection, Conditional)}, and then check the {@link #result} field.
+ */
+class Search implements EditPartViewer.Conditional {
+	public Search(Point pt){
+		where = pt;
+	}
+	/**
+	 * the result of the search.
+	 */
+	private Point where;
+	public AutoexposeHelper result;
+	public boolean evaluate(EditPart editpart) {
+		result = (AutoexposeHelper)editpart.getAdapter(AutoexposeHelper.class);
+		if (result != null && result.detect(where))
+			return true;
+		result = null;
+		return false;
+	}
+}
+
 }
