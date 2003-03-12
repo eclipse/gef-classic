582e9c9b6e51f63c4931fec109b0e5b189abdfb0
ebordeau <ebordeau>
2003-03-12 22:23:24 +0000

    *** empty log message ***

---
+++ org.eclipse.draw2d.examples/src/swt/experimentation/FocusTest.java
@@ -0,0 +1,44 @@
+package swt.experimentation;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusAdapter;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class FocusTest {
+
+public static void main(String[] args) {
+	Display display = Display.getDefault();
+	Shell shell = new Shell(display);
+	shell.setLayout(new FillLayout());
+	
+	final Canvas canvas = new Canvas(shell, SWT.H_SCROLL | SWT.V_SCROLL);
+	canvas.getVerticalBar().setVisible(false);
+	canvas.getHorizontalBar().setVisible(false);
+	Text text = new Text(canvas, SWT.SINGLE);
+	text.setSize(100, 20);
+	text.addVerifyListener(new VerifyListener() {
+		public void verifyText(VerifyEvent e) {
+			canvas.getVerticalBar().setVisible(false);
+		}
+	});
+	text.addFocusListener(new FocusAdapter() {
+		public void focusLost(FocusEvent e) {
+			System.out.println("Focus Lost");
+		}
+	});
+	
+	shell.setSize(600, 500);
+	shell.open();
+	while (!shell.isDisposed())
+		if (!display.readAndDispatch())
+			display.sleep();
+}
+
+}
