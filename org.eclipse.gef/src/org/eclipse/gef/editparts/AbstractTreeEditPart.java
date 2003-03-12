1fbd33b97bc30cf79af978be2e550540490e029a
ebordeau <ebordeau>
2003-03-12 16:40:05 +0000

    Updated copyright statements.

--- org.eclipse.gef/src/org/eclipse/gef/editparts/AbstractTreeEditPart.java
+++ org.eclipse.gef/src/org/eclipse/gef/editparts/AbstractTreeEditPart.java
@@ -1,10 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.gef.editparts;
-/*
- * Licensed Material - Property of IBM
- * (C) Copyright IBM Corp. 2001, 2002 - All Rights Reserved.
- * US Government Users Restricted Rights - Use, duplication or disclosure
- * restricted by GSA ADP Schedule Contract with IBM Corp.
- */
 
 import java.util.Iterator;
 import java.util.List;
