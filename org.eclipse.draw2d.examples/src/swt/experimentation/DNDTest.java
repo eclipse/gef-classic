430a8d6cf02ac26a6f538e68ba38df5aac1cf359
ebordeau <ebordeau>
2003-03-24 17:15:01 +0000

    *** empty log message ***

--- org.eclipse.draw2d.examples/src/swt/experimentation/DNDTest.java
+++ org.eclipse.draw2d.examples/src/swt/experimentation/DNDTest.java
@@ -4,6 +4,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.*;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
@@ -14,8 +15,20 @@ public class DNDTest {
 public static void main(String[] args) {
 	Shell shell = new Shell();
 	shell.setLayout(new FillLayout());
-	Canvas canvas = new Canvas(shell, SWT.NONE);
-	DragSource ds = new DragSource(canvas, DND.DROP_MOVE | DND.DROP_COPY | DND.DROP_LINK);
+
+	Canvas dragCanvas = new Canvas(shell, SWT.NONE);
+	dragCanvas.setBackground(new Color(null, 255, 0, 0));
+	dragCanvas.addMouseListener(new MouseListener() {
+		public void mouseDoubleClick(MouseEvent event) {}
+		public void mouseDown(MouseEvent event) {
+			System.out.println("Drag Canvas: Mouse Down");
+		}
+		public void mouseUp(MouseEvent event) {
+			System.out.println("Drag Canvas: Mouse Up");
+		}
+	});
+	DragSource ds = new DragSource(dragCanvas, DND.DROP_MOVE | DND.DROP_COPY | DND.DROP_LINK);
+	ds.setTransfer(new Transfer[] {TextTransfer.getInstance()});
 	ds.addDragListener(new DragSourceListener() {
 		public void dragStart(DragSourceEvent event) {
 			System.out.println("Drag Start");
@@ -27,15 +40,31 @@ public static void main(String[] args) {
 			System.out.println("Drag Finished");
 		}
 	});
-	canvas.addMouseListener(new MouseListener() {
+
+	Canvas dropCanvas = new Canvas(shell, SWT.NONE);
+	dropCanvas.setBackground(new Color(null, 0, 255, 0));
+	dropCanvas.addMouseListener(new MouseListener() {
 		public void mouseDoubleClick(MouseEvent event) {}
 		public void mouseDown(MouseEvent event) {
-			System.out.println("Mouse Down");
+			System.out.println("Drop Canvas: Mouse Down");
 		}
 		public void mouseUp(MouseEvent event) {
-			System.out.println("Mouse Up");
+			System.out.println("Drop Canvas: Mouse Up");
 		}
 	});
+	DropTarget dt = new DropTarget(dropCanvas, DND.DROP_MOVE | DND.DROP_COPY | DND.DROP_LINK);
+	dt.setTransfer(new Transfer[] {TextTransfer.getInstance()});
+	dt.addDropListener(new DropTargetListener() {
+		public void dragEnter(DropTargetEvent event) {}
+		public void dragLeave(DropTargetEvent event) {}
+		public void dragOperationChanged(DropTargetEvent event) {}
+		public void dragOver(DropTargetEvent event) {}
+		public void drop(DropTargetEvent event) {
+			System.out.println("Drop");
+		}
+		public void dropAccept(DropTargetEvent event) {}
+	});
+	
 	shell.open();
 	Display display = Display.getDefault();
 	while (!shell.isDisposed())
