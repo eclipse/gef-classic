788aa7e0005003f70eaafea991d0d66c1d91901f
rhudson <rhudson>
2003-05-05 19:38:01 +0000

    JavaDoc

--- org.eclipse.gef/src/org/eclipse/gef/EditPart.java
+++ org.eclipse.gef/src/org/eclipse/gef/EditPart.java
@@ -100,14 +100,13 @@ void addNotify();
  * case of the {@link RootEditPart}. This method may be called multiple times.
  * <P>During deactivation the receiver should:
  * <UL>
- * 	<LI>remove all listeners that were added in {@link #activate}
- * 	<LI>activate all of its EditPolicies.  EditPolicies may also observe
- * 		the model, although this is rare.  But it is common for EditPolicies to contribute
- * 		additional visuals, such as selection handles or feedback during interactions.
- * 		Therefore it is necessary to tell the EditPolicies when to start doing this, and
- * 		when to stop.
- * 	<LI>call activate() on the EditParts it manages.  This includes
- * 		its children, and for GraphicalEditParts, its <i>source connections</i>.
+ * 	 <LI>remove all listeners that were added in {@link #activate}
+ * 	 <LI>deactivate all of its EditPolicies. EditPolicies may be contributing
+ * 	 additional visuals, such as selection handles or feedback during interactions.
+ * 	 Therefore it is necessary to tell the EditPolicies when to start doing this, and
+ * 	 when to stop.
+ * 	 <LI>call deactivate() on the EditParts it manages.  This includes its children,
+ * 	 and for <code>GraphicalEditParts</code>, its <i>source connections</i>.
  * </UL>
  */
 void deactivate();
