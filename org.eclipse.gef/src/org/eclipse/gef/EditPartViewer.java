42189a379a941e572ed978c6024d523f018e9f6c
rhudson <rhudson>
2003-05-28 20:26:34 +0000

    Missing removeDragSourceListener Missing removeDropTargetListener

--- org.eclipse.gef/src/org/eclipse/gef/EditPartViewer.java
+++ org.eclipse.gef/src/org/eclipse/gef/EditPartViewer.java
@@ -24,60 +24,78 @@ import org.eclipse.gef.dnd.TransferDragSourceListener;
 import org.eclipse.gef.dnd.TransferDropTargetListener;
 
 /**
- * An adapter on an SWT {@link org.eclipse.swt.widgets.Control} that manages EditParts,
- * the conceptual objects with which the User interacts. The Viewer is responsible
- * for:
- * <UL>
- *   <LI>Managing the EditParts in it
- *   <LI>Supporting some form of <i>visuals</i>, such as Figures or TreeItems
- *   <LI>Managing the <i>selection</i>
- *   <LI>Managing a <i>focus</i> EditPart
- *   <LI>Routing mouse input and keystrokes to the {@link EditDomain}
- * </UL>
+ * An adapter on an SWT {@link org.eclipse.swt.widgets.Control} that manages the
+ * {@link org.eclipse.gef.EditPart EditParts}. The viewer is responsible for the editpart
+ * lifecycle. Editparts have <i>visuals</i>, such as <code>TreeItems</code> or
+ * <code>Figures</code>, which are hosted by the viewer and its control. The viewer
+ * provides targeting of editparts via their visuals.
  * 
- * <P>When the Viewer is realized, meaning it has an SWT <code>Control</code>, it should
- * activate its <code>RootEditPart</code>.  When that Control is being disposed, it should
- * {@link EditPart#deactivate()} the root.
+ * <P>
+ * A viewer is a {@link org.eclipse.jface.viewers.ISelectionProvider}. It maintains a list
+ * of selected editparts. The last member of this list is the <i>primary</i> member of the
+ * selection. The list should never be empty; when no editparts are selected, the viewer's
+ * contents editpart is used.
  * 
- * <P><I>Visuals</I> is the term used to describe how EditParts are manifested to the
- * User.  {@link org.eclipse.draw2d.IFigure Figures} and {@link
- * org.eclipse.swt.widgets.TreeItem TreeItems} are examples of visuals support by the
- * EditPartViewers included with GEF.  The Viewer creates the necessary infrastructure for
- * hosting its EditParts' visuals.
+ * <P>
+ * A viewer is populated by setting its <i>contents</i>. This can be done by passing the
+ * model corresponding to the contents. The viewer's {@link
+ * org.eclipse.gef.EditPartFactory EditPartFactory} is then used to create the contents
+ * editpart, and add it to the <i>root</i> editpart. Alternatively, the contents editpart
+ * itself can be provided. Once the contents editpart is parented, it will populate the
+ * rest of the viewer by calling its {@link EditPart#refresh()} method.
  * 
- * <P>The Viewer maintains a list of selected EditParts.  The last EditPart in this list
- * is said to have <i>primary</i> selection.  <I>Focus</I> is also maintained by the
- * viewer.  Focus can be changed without affecting the current selection.  This is used
- * for keyboard accessibility.  The Viewer is responsible for reflecting the current
- * focus and selection on its EditParts by calling {@link EditPart#setFocus(boolean)} and
- * {@link EditPart#setSelected(int)}.
+ * <P>
+ * The Root editpart does not correspond to anything in the model, it is used to
+ * bootstrap the viewer, and to parent the contents. Depending on the type of viewer being
+ * used, it may be common to replace the root editpart. See implementations of {@link
+ * org.eclipse.gef.RootEditPart}.
+ * 
+ * <P>
+ * An editpart's lifecycle is managed by the viewer. When the Viewer is realized, meaning
+ * it has an SWT <code>Control</code>, it activates its root, which in turn activates all
+ * editparts. Editparts are deactivated when they are removed from the viewer. When the
+ * viewer's control is disposed, all editparts are similarly deactivated by decativating
+ * the root.
+ * 
  */
 public interface EditPartViewer
 	extends org.eclipse.jface.viewers.ISelectionProvider
 {
 
+/**
+ * An object which evaluates an EditPart for an arbitrary property. Conditionals are used
+ * when querying a viewer for an editpart.
+ * @author hudsonr
+ */
 interface Conditional {
+	/**
+	 * Returns <code>true</code> if the editpart meets this condition.
+	 * @param editpart the editpart being evaluated
+	 * @return <code>true</code> if the editpart meets the condition
+	 */
 	boolean evaluate(EditPart editpart);
 }
 
 /**
  * Adds a <code>TransferDragSourceListener</code> to this viewer. This has the side-effect
- * of ensuring that a {@link org.eclipse.swt.dnd.DragSource} exists for the viewer's
- * Control.
+ * of creating a {@link org.eclipse.swt.dnd.DragSource} on the viewer's Control. A Control
+ * can only have a single DragSource. Clients must not create their own DragSource when
+ * using this method.
  * @param listener the listener
  */
 void addDragSourceListener(TransferDragSourceListener listener);
 
 /**
  * Adds a <code>TransferDropTargetListener</code> to this viewer. This has the side-effect
- * of ensuring that a {@link org.eclipse.swt.dnd.DropTarget} exists for the viewer's
- * Control.
+ * of creating a {@link org.eclipse.swt.dnd.DropTarget} on the viewer's Control. A Control
+ * can only have a single DropTarget. Clients must not create their own DropTarget when
+ * using this method.
  * @param listener the listener
  */
 void addDropTargetListener(TransferDropTargetListener listener);
 
 /**
- * Appends the specified <code>EditPart</code> to the Viewer's <i>selection</i>. The
+ * Appends the specified <code>EditPart</code> to the viewer's <i>selection</i>. The
  * EditPart becomes the new primary selection. Fires selection changed to all{@link
  * org.eclipse.jface.viewers.ISelectionChangedListener}s.
  * @param editpart the EditPart to append
@@ -144,7 +162,8 @@ EditPart findObjectAtExcluding(Point location, Collection exclusionSet);
  * @param conditional the Conditional used to evaluate a potential hit
  * @return <code>null</code> or an EditPart
  */
-EditPart findObjectAtExcluding(Point location, Collection exclusionSet, Conditional conditional);
+EditPart findObjectAtExcluding(Point location, Collection exclusionSet,
+	Conditional conditional);
 
 /**
  * Flushes all pending updates to the Viewer.
@@ -163,37 +182,45 @@ void flush();
 EditPart getContents();
 
 /**
- * Returns the <code>MenuManager</code> for this viewer or <code>null</code> if  no
- * provider exists.
- * @return the MenuManager
+ * Returns <code>null</code> or the <code>MenuManager</code> for this viewer. The menu
+ * manager is set using {@link #setContextMenu(MenuManager)}.
+ * @return <code>null</code> or a MenuManager
  */
 MenuManager getContextMenu();
 
 /**
- * Returns the SWT <code>Control</code> for this viewer.
+ * Returns the SWT <code>Control</code> for this viewer. This method may return null if
+ * the control has not yet been provided or created.
+ * @see #setControl(Control)
+ * @see #createControl(Composite)
  * @return the SWT <code>Control</code>
  */
 Control getControl();
 
 /**
- * Returns the {@link EditDomain EditDomain} to which this viewer belongs
- * @return the EditDomain
+ * Returns the {@link EditDomain EditDomain} to which this viewer belongs.
+ * @return the viewer's EditDomain
  */
 EditDomain getEditDomain();
 
 /**
  * Returns the <code>EditPartFactory</code> for this viewer.  The EditPartFactory is used
  * to create the <i>contents</i> EditPart when {@link #setContents(Object)} is called. It
- * is then used by all EditParts that need to create additional EditParts, such as
- * children.
+ * is made available so that other EditParts can use it to create their children or
+ * connection editparts.
  * @return EditPartFactory */
 EditPartFactory getEditPartFactory();
 
 /**
  * Returns the {@link Map} for registering <code>EditParts</code> by <i>Keys</i>. 
- * EditParts may register themselves using any method, and may register themselved
- * multiple times. The purpose of such registration is to allow an EditPart to be found by
- * other EditParts.
+ * EditParts may register themselves using any method, and may register themselves
+ * with multiple keys. The purpose of such registration is to allow an EditPart to be
+ * found by other EditParts, or by listeners of domain notifiers. By default, EditParts
+ * are registered by their model.
+ * <P>
+ * Some models use a "domain" notification system, in which all changes are dispatched to
+ * a single listener. Such a listener might use this map to lookup editparts for a given
+ * model, and then ask the editpart to update.
  * @return the registry map
  */
 Map getEditPartRegistry();
@@ -201,29 +228,32 @@ Map getEditPartRegistry();
 /**
  * Returns the <i>focus</i> <code>EditPart</code>. Focus refers to keyboard focus.  This
  * is the same concept as focus in a native Tree or Table.  The User can change focus
- * using the keyboard without affecting the currently selected objects.
+ * using the keyboard without affecting the currently selected objects. Never returns
+ * <code>null</code>.
  * @return the <i>focus</i> <code>EditPart</code> */
 EditPart getFocusEditPart();
 
 /**
- * Returns the <code>KeyHandler</code> for this viewer.  The KeyHandler is forwarded keys
- * by the active <code>Tool</code>.  This is important, because only the current tool
- * knows if it is in a state in which keys should be ignored, such as during a drag.  By
- * default, only the {@link org.eclipse.gef.tools.SelectionTool} forwards keysrokes.  It
- * does not do so during a drag.
- * @return KeyHandler */
+ * Returns the <code>KeyHandler</code> for this viewer.  The KeyHandler is sent KeyEvents
+ * by the currently active <code>Tool</code>. This is important, because only the current
+ * tool knows if it is in a state in which keys should be ignored, such as during a drag. 
+ * By default, only the {@link org.eclipse.gef.tools.SelectionTool} forwards keysrokes. 
+ * It does not do so during a drag.
+ * @return <code>null</code> or a KeyHandler */
 KeyHandler getKeyHandler();
 
 /**
- * Returns the <code>RootEditPart</code>.  The RootEditPart is a special EditPart
- * that bridges the connection to the viewer.  The <i>root</i> is never selected. 
- * The root does not correspond to anything in the model.  The User does not interact with
- * the root.
+ * Returns the <code>RootEditPart</code>.  The RootEditPart is a special EditPart that
+ * serves as the parent to the contents editpart. The <i>root</i> is never selected. The
+ * root does not correspond to anything in the model.  The User does not interact with the
+ * root.
  * <P>
  * The RootEditPart has a single child: the {@link #getContents() contents}.
  * <P>
  * By defining the concept of "root", GEF allows the application's "real" EditParts to be
- * more homogeneous.  For example, all non-root EditParts have a parent.
+ * more homogeneous.  For example, all non-root EditParts have a parent.  Also, it allows
+ * applications to change the type of root being used without affecting their own editpart
+ * implementation hierarchy.
  * @see #getContents()
  * @see #setRootEditPart(RootEditPart)
  * @return the RootEditPart
@@ -231,8 +261,10 @@ KeyHandler getKeyHandler();
 RootEditPart getRootEditPart();
 
 /**
- * Returns an unmodifiable <code>List</code> containing the selected editparts.
- * @return the selected parts
+ * Returns an unmodifiable <code>List</code> containing zero or more selected editparts.
+ * This list may be empty. This list can be modified indirectly by calling other
+ * methods on the viewer.
+ * @return a list containing zero or more editparts
  */
 List getSelectedEditParts();
 
@@ -241,8 +273,8 @@ List getSelectedEditParts();
  * <code>EditParts</code>. This map is used for hit-testing.  Hit testing is performed by
  * first determining which visual part is hit, and then mapping that part to an
  * <code>EditPart</code>.  What consistutes a <i>visual part</i> is viewer-specific. 
- * Examples include Figures and TreeItems.
- * @return the visual part <code>Map</code>
+ * Examples include <code>Figures</code> and <code>TreeItems</code>.
+ * @return the visual part map
  */
 Map getVisualPartMap();
 
@@ -252,39 +284,54 @@ Map getVisualPartMap();
 void registerAccessibleEditPart(AccessibleEditPart acc);
 
 /**
- * Shows the given EditPart if it is not visible.  
+ * Removes the specified drag source listener.  If all listeners are removed, the
+ * DragSource that was created will be disposed.
+ * @see #addDragSourceListener(TransferDragSourceListener)
+ * @param listener the listener
+ */
+void removeDragSourceListener(TransferDragSourceListener listener);
+
+/**
+ * Removes the specified drop target listener. If all listeners are removed, the
+ * DropTarget that was created will be disposed.
+ * @see #addDropTargetListener(TransferDropTargetListener)
+ * @param listener
+ */
+void removeDropTargetListener(TransferDropTargetListener listener);
+
+/**
+ * Reveals the given EditPart if it is not visible.  
  * @param editpart the EditPart to reveal
  */
 void reveal(EditPart editpart);
 
 /**
  * Replaces the current selection with the specified <code>EditPart</code>. That part
- * becomes the primary selection.
- * <P>Fires selection changed to
- * {@link org.eclipse.jface.viewers.ISelectionChangedListener}s.
+ * becomes the primary selection.  Fires selection changed to {@link
+ * org.eclipse.jface.viewers.ISelectionChangedListener}s.
  * @param editpart the new selection
  */
 void select(EditPart editpart);
 
 /**
- * Sets the  for this Viewer.
+ * Sets the contents for this Viewer. The contents can also be set using {@link
+ * #setContents(Object)}.
  * @see #getRootEditPart()
  */
 void setContents(EditPart editpart);
 
 /**
- * Creates an <code>EditPart</code> for the specified contents using this viewer's
+ * Creates an <code>EditPart</code> for the provided model object using the
  * <code>EditPartFactory</code>.  That EditPart is then added to the {@link
- * #getRootEditPart() RootEditPart}.
- * @param contents the Object representing the viewer's contents
- * @see #setEditPartFactory(EditPartFactory)
+ * #getRootEditPart() RootEditPart}, and becomes the viewer's contents.
+ * @param contents the contents model object
  */
 void setContents(Object contents);
 
 /**
- * Sets the <code>MenuManager</code> for this viewer's context menu. This MenuManager
- * creates the SWT Menu for this viewer's control.
- * @see #getControl()
+ * Sets the context <code>MenuManager</code> for this viewer. The MenuManager will be
+ * asked to create a Menu, which will be used as the context menu for this viewer's
+ * Control.
  * @param contextMenu the <code>ContextMenuProvider</code>
  */
 void setContextMenu(MenuManager contextMenu);
@@ -350,7 +397,7 @@ void setRootEditPart(RootEditPart root);
 void setRouteEventsToEditDomain(boolean value);
 
 /**
- * Used for accessibility purposes
+ * Used for accessibility purposes.
  * @param acc the accessible part */
 void unregisterAccessibleEditPart(AccessibleEditPart acc);
 
