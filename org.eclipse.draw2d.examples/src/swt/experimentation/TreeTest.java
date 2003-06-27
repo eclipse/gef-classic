50d16ad44d459cdba7c41ca2befe0e8ad4d29f8d
ebordeau <ebordeau>
2003-06-27 18:55:21 +0000

    *** empty log message ***

---
+++ org.eclipse.draw2d.examples/src/swt/experimentation/TreeTest.java
@@ -0,0 +1,33 @@
+package swt.experimentation;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.widgets.*;
+
+public class TreeTest {
+
+public static void main(String[] args) {
+	Shell shell = new Shell();
+
+	Tree tree = new Tree(shell, SWT.NONE);
+	final TreeItem item1 = new TreeItem(tree, SWT.NONE);
+	item1.setText("Item 1");
+	item1.addDisposeListener(new DisposeListener() {
+		public void widgetDisposed(DisposeEvent e) {
+			boolean expanded = item1.getExpanded();
+		}
+	});
+
+	shell.open();
+
+	tree.removeAll();
+
+	Display display = Display.getDefault();
+	while (!shell.isDisposed())
+		if (!display.readAndDispatch())
+			display.sleep();
+	display.dispose();
+}
+
+}
