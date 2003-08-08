afe1243e0d9f12dadca29fa08f4640d7416b5b9b
ebordeau <ebordeau>
2003-08-08 18:46:15 +0000

    *** empty log message ***

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/actions/LogicPrintAction.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/actions/LogicPrintAction.java
@@ -58,11 +58,15 @@ public void run(IAction action) {
 	viewer.setContents(getContents());
 	viewer.flush();
 	
+	int printMode = new PrintModeDialog(shell).open();
+	if (printMode == -1)
+		return;
 	PrintDialog dialog = new PrintDialog(shell, SWT.NULL);
 	PrinterData data = dialog.open();
 	if (data != null) {
 		PrintGraphicalViewerOperation op = 
 					new PrintGraphicalViewerOperation(new Printer(data), viewer);
+		op.setPrintMode(printMode);
 		op.run(selectedFile.getName());
 	}
 }
---
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/actions/PrintModeDialog.java
@@ -0,0 +1,63 @@
+package org.eclipse.gef.examples.logicdesigner.actions;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.*;
+
+import org.eclipse.jface.dialogs.Dialog;
+
+import org.eclipse.draw2d.PrintOperation;
+
+/**
+ * @author Eric Bordeau
+ */
+public class PrintModeDialog extends Dialog {
+
+private Button tile, fitPage, fitWidth, fitHeight;
+
+public PrintModeDialog(Shell shell) {
+	super(shell);
+}
+
+protected void cancelPressed() {
+	setReturnCode(-1);
+	close();
+}
+
+protected void configureShell(Shell newShell) {
+	newShell.setText("Select Print Mode");
+	super.configureShell(newShell);
+}
+
+protected Control createDialogArea(Composite parent) {
+	Composite composite = (Composite)super.createDialogArea(parent);
+	
+	tile = new Button(composite, SWT.RADIO);
+	tile.setText("Tile");
+	tile.setSelection(true);
+	
+	fitPage = new Button(composite, SWT.RADIO);
+	fitPage.setText("Fit Page");
+
+	fitWidth = new Button(composite, SWT.RADIO);
+	fitWidth.setText("Fit Width");
+
+	fitHeight = new Button(composite, SWT.RADIO);
+	fitHeight.setText("Fit Height");
+
+	return composite;
+}
+
+protected void okPressed() {
+	int returnCode = -1;
+	if (tile.getSelection())
+		returnCode = PrintOperation.TILE;
+	else if (fitPage.getSelection())
+		returnCode = PrintOperation.FIT_PAGE;
+	else if (fitHeight.getSelection())
+		returnCode = PrintOperation.FIT_HEIGHT;
+	else if (fitWidth.getSelection())
+		returnCode = PrintOperation.FIT_WIDTH;
+	setReturnCode(returnCode);
+	close();
+}
+}
