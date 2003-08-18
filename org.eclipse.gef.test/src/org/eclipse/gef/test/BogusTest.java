16c9be672cafd00465a4dbf8b684b1e0fbe6738a
ebordeau <ebordeau>
2003-08-18 18:03:39 +0000

    Initial JUnit test plugin

---
+++ org.eclipse.gef.test/src/org/eclipse/gef/test/BogusTest.java
@@ -0,0 +1,22 @@
+package org.eclipse.gef.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Eric Bordeau
+ */
+public class BogusTest extends TestCase {
+
+public BogusTest() {
+	super();
+}
+
+public BogusTest(String name) {
+	super(name);
+}
+
+public void testTest() {
+	assertTrue(true);
+}
+
+}
---
+++ org.eclipse.gef.test/src/org/eclipse/gef/test/GEFTestPlugin.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gef.test;
+
+import org.eclipse.core.runtime.IPluginDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class GEFTestPlugin extends AbstractUIPlugin {
+
+public GEFTestPlugin(IPluginDescriptor descriptor) {
+	super(descriptor);
+}
+
+}
