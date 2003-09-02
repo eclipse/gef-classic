dfc2d1b51fa4cc67559cb680e97fbaeb6255b26f
rhudson <rhudson>
2003-09-02 18:29:51 +0000

    isHorizontal did not check contents pane

--- org.eclipse.gef/src/org/eclipse/gef/editpolicies/FlowLayoutEditPolicy.java
+++ org.eclipse.gef/src/org/eclipse/gef/editpolicies/FlowLayoutEditPolicy.java
@@ -12,8 +12,7 @@ package org.eclipse.gef.editpolicies;
 
 import java.util.List;
 
-import org.eclipse.draw2d.FlowLayout;
-import org.eclipse.draw2d.Polyline;
+import org.eclipse.draw2d.*;
 import org.eclipse.draw2d.geometry.*;
 
 import org.eclipse.gef.*;
@@ -151,7 +150,8 @@ private Point getLocationFromRequest(Request request) {
  * @return <code>true</code> if the host's LayoutManager is in a horizontal orientation
  */
 protected boolean isHorizontal() {
-	return ((FlowLayout) getHostFigure().getLayoutManager()).isHorizontal();
+	IFigure figure = ((GraphicalEditPart)getHost()).getContentPane();
+	return ((FlowLayout)figure.getLayoutManager()).isHorizontal();
 }
 
 /**
