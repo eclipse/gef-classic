b86e13be0c6b67d29b7c8087b24fe2082bd9e6a0
ebordeau <ebordeau>
2003-09-12 20:12:10 +0000

    Updated javadoc

--- org.eclipse.draw2d/src/org/eclipse/draw2d/PositionConstants.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/PositionConstants.java
@@ -11,9 +11,32 @@
 package org.eclipse.draw2d;
 
 /**
- * Constants representing cardinal directions and relative positions.
+ * Constants representing cardinal directions and relative positions. Some of these 
+ * constants can be grouped as follows:
+ * <TABLE border="1" cellpadding="5" cellspacing="0">
+ * 	<TBODY>
+ * 		<TR>
+ * 			<TD>LEFT, CENTER, RIGHT</TD>
+ * 			<TD>Used to describe horizontal position.</TD>
+ * 		</TR>
+ * 		<TR>
+ * 			<TD>TOP, MIDDLE, BOTTOM</TD>
+ * 			<TD>Used to describe vertical position.</TD>
+ * 		</TR>
+ * 		<TR>
+ * 			<TD>NORTH, SOUTH, EAST, WEST</TD>
+ * 			<TD>Used to describe the four positions relative to an object's center point. 
+ * 			May also be used when describing which direction an object is facing.<BR>
+ * 			NOTE: If you have a use for all four of these possibilities, do not use TOP, 
+ * 			BOTTOM, RIGHT, LEFT in place of NORTH, SOUTH, EAST, WEST.</TD>
+ * 		</TR>
+ * 	</TBODY>
+ * </TABLE> 
  */
 public interface PositionConstants {
+
+/** None */
+int NONE  =  0;
 
 /** Left */
 int LEFT   =  1;
@@ -23,6 +46,7 @@ int CENTER =  2;
 int RIGHT  =  4;
 /** Bit-wise OR of LEFT, CENTER, and RIGHT */
 int LEFT_CENTER_RIGHT = LEFT | CENTER | RIGHT;
+
 /** Top */
 int TOP    =  8;
 /** Middle (Vertical) */
@@ -31,8 +55,7 @@ int MIDDLE = 16;
 int BOTTOM = 32;
 /** Bit-wise OR of TOP, MIDDLE, and BOTTOM */
 int TOP_MIDDLE_BOTTOM = TOP | MIDDLE | BOTTOM;
-/** None */
-int NONE  =  0;
+
 /** North */
 int NORTH =  1;
 /** South */
@@ -41,6 +64,7 @@ int SOUTH =  4;
 int WEST  =  8;
 /** East */
 int EAST  = 16;
+
 /** North-East: a bit-wise OR of {@link #NORTH} and {@link #EAST} */
 int NORTH_EAST = NORTH | EAST;
 /** North-West: a bit-wise OR of {@link #NORTH} and {@link #WEST} */
