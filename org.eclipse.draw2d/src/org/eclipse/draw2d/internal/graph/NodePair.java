3fbbff8373be1bfcf457d6b00de005b1a698c230
rhudson <rhudson>
2003-09-18 16:26:32 +0000

    *** empty log message ***

--- org.eclipse.draw2d/src/org/eclipse/draw2d/internal/graph/NodePair.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/internal/graph/NodePair.java
@@ -11,7 +11,7 @@ public class NodePair {
 public Node n1;
 public Node n2;
 
-public NodePair() {}
+public NodePair() { }
 
 public NodePair(Node n1, Node n2) {
 	this.n1 = n1;
@@ -34,7 +34,7 @@ public int hashCode() {
  * @see java.lang.Object#toString()
  */
 public String toString() {
-	return "[" + n1 + ", " + n2 + "]";
+	return "[" + n1 + ", " + n2 + "]"; //$NON-NLS-1$ //$NON-NLS-2$
 }
 
 }
