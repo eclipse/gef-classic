2b202cabc3adb44159d90a531b4b041a16898042
rhudson <rhudson>
2003-09-19 21:09:40 +0000

    footer feedback

--- org.eclipse.gef.examples.flow/src/org/eclipse/gef/examples/flow/policies/StructuredActivityDirectEditPolicy.java
+++ org.eclipse.gef.examples.flow/src/org/eclipse/gef/examples/flow/policies/StructuredActivityDirectEditPolicy.java
@@ -1,8 +1,10 @@
 package org.eclipse.gef.examples.flow.policies;
 
-import org.eclipse.gef.examples.flow.figures.StartTag;
-import org.eclipse.gef.examples.flow.figures.SubgraphFigure;
+import org.eclipse.draw2d.Label;
+
 import org.eclipse.gef.requests.DirectEditRequest;
+
+import org.eclipse.gef.examples.flow.figures.SubgraphFigure;
 
 /**
  * StructuredActivityDirectEditPolicy
@@ -16,7 +18,8 @@ public class StructuredActivityDirectEditPolicy
  */
 protected void showCurrentEditValue(DirectEditRequest request) {
 	String value = (String)request.getCellEditor().getValue();
-	((StartTag)((SubgraphFigure)getHostFigure()).getHeader()).setText(value);
+	((Label)((SubgraphFigure)getHostFigure()).getHeader()).setText(value);
+	((Label)((SubgraphFigure)getHostFigure()).getFooter()).setText(value);
 	
 	//hack to prevent async layout from placing the cell editor twice.
 	getHostFigure().getUpdateManager().performUpdate();
