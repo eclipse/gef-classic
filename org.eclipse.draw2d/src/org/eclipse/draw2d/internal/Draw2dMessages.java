6f3aba5c725ccd6f5b8f73dd1aed30b9634238ba
rhudson <rhudson>
2003-09-19 15:13:23 +0000

    *** empty log message ***

--- org.eclipse.draw2d/src/org/eclipse/draw2d/internal/Draw2dMessages.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/internal/Draw2dMessages.java
@@ -36,12 +36,21 @@ static class Helper {
 	}
 }
 
-String ERR_Figure_Add_Exception_OutOfBounds = Helper.getString("%Figure.Add.Exception.OutOfBounds_EXC_");				//$NON-NLS-1$
-String ERR_Figure_Add_Exception_IllegalArgument = Helper.getString("%Figure.Add.Exception.IllegalArgument_EXC_");		//$NON-NLS-1$
-String ERR_Figure_Remove_Exception_IllegalArgument = Helper.getString("%Figure.Remove.Exception.IllegalArgument_EXC_");	//$NON-NLS-1$
-String ERR_Figure_SetConstraint_Exception_IllegalArgument = Helper.getString("%Figure.SetConstraint.Exception.IllegalArgument_EXC_");	//$NON-NLS-1$
-String ERR_PointList_InsertPoint_Exception_IndexOutOfBounds = Helper.getString("%PointList.InsertPoint.Exception.IndexOutOfBounds_EXC_");	//$NON-NLS-1$
-String ERR_PointList_RemovePoint_Exception_IndexOutOfBounds = Helper.getString("%PointList.RemovePoint.Exception.IndexOutOfBounds_EXC_");	//$NON-NLS-1$
-String ERR_SWTEventDispatcher_SetControl_Exception_Runtime = Helper.getString("%SWTEventDispatcher.SetControl.Exception.Runtime_EXC_");	//$NON-NLS-1$
+//$TODO Error messages do not need to be translated.  Be consistent with rest of Eclipse.
+
+String ERR_Figure_Add_Exception_OutOfBounds =
+	Helper.getString("%Figure.Add.Exception.OutOfBounds_EXC_");				//$NON-NLS-1$
+String ERR_Figure_Add_Exception_IllegalArgument =
+	Helper.getString("%Figure.Add.Exception.IllegalArgument_EXC_");		//$NON-NLS-1$
+String ERR_Figure_Remove_Exception_IllegalArgument =
+	Helper.getString("%Figure.Remove.Exception.IllegalArgument_EXC_");	//$NON-NLS-1$
+String ERR_Figure_SetConstraint_Exception_IllegalArgument =
+	Helper.getString("%Figure.SetConstraint.Exception.IllegalArgument_EXC_");	//$NON-NLS-1$
+String ERR_PointList_InsertPoint_Exception_IndexOutOfBounds =
+	Helper.getString("%PointList.InsertPoint.Exception.IndexOutOfBounds_EXC_");	//$NON-NLS-1$
+String ERR_PointList_RemovePoint_Exception_IndexOutOfBounds =
+	Helper.getString("%PointList.RemovePoint.Exception.IndexOutOfBounds_EXC_");	//$NON-NLS-1$
+String ERR_SWTEventDispatcher_SetControl_Exception_Runtime =
+	Helper.getString("%SWTEventDispatcher.SetControl.Exception.Runtime_EXC_");	//$NON-NLS-1$
 
 }
