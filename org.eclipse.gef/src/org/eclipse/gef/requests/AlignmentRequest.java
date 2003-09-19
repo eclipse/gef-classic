1e29488bb0614308811fe41a9ae18413263c2572
rhudson <rhudson>
2003-09-19 18:48:35 +0000

    removed nested blocks

--- org.eclipse.gef/src/org/eclipse/gef/requests/AlignmentRequest.java
+++ org.eclipse.gef/src/org/eclipse/gef/requests/AlignmentRequest.java
@@ -37,30 +37,24 @@ public AlignmentRequest(Object type) {
 
 private void doNormalAlignment(Rectangle result, Rectangle reference) {
 	switch (alignment) {
-		case PositionConstants.LEFT: {
+		case PositionConstants.LEFT:
 			result.x = reference.x;
 			break;
-		}
-		case PositionConstants.RIGHT: {
+		case PositionConstants.RIGHT:
 			result.x = reference.x + reference.width - result.width;
 			break;
-		}
-		case PositionConstants.TOP: {
+		case PositionConstants.TOP:
 			result.y = reference.y;
 			break;
-		}
-		case PositionConstants.BOTTOM: {
+		case PositionConstants.BOTTOM:
 			result.y = reference.y + reference.height - result.height;
 			break;
-		}
-		case PositionConstants.CENTER: {
+		case PositionConstants.CENTER:
 			result.x = reference.x + (reference.width / 2) - (result.width / 2);
 			break;
-		}
-		case PositionConstants.MIDDLE: {
+		case PositionConstants.MIDDLE:
 			result.y = reference.y + (reference.height / 2) - (result.height / 2);
 			break;
-		}
 	}
 }
 
@@ -68,38 +62,32 @@ private void doPrecisionAlignment(
 	PrecisionRectangle result,
 	PrecisionRectangle reference) {
 	switch (alignment) {
-		case PositionConstants.LEFT: {
+		case PositionConstants.LEFT:
 			result.setX(reference.preciseX);
 			break;
-		}
-		case PositionConstants.RIGHT: {
+		case PositionConstants.RIGHT:
 			result.setX(
 				reference.preciseX + reference.preciseWidth - result.preciseWidth);
 			break;
-		}
-		case PositionConstants.TOP: {
+		case PositionConstants.TOP:
 			result.setY(reference.preciseY);
 			break;
-		}
-		case PositionConstants.BOTTOM: {
+		case PositionConstants.BOTTOM:
 			result.setY(
 				reference.preciseY + reference.preciseHeight - result.preciseHeight);
 			break;
-		}
-		case PositionConstants.CENTER: {
+		case PositionConstants.CENTER:
 			result.setX(
 				reference.preciseX
 					+ (reference.preciseWidth / 2)
 					- (result.preciseWidth / 2));
 			break;
-		}
-		case PositionConstants.MIDDLE: {
+		case PositionConstants.MIDDLE:
 			result.setY(
 				reference.preciseY
 					+ (reference.preciseHeight / 2)
 					- (result.preciseHeight / 2));
 			break;
-		}
 	}
 
 	
