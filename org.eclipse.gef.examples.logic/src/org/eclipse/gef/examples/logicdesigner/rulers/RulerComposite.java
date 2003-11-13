7c11db28f161d767f7fa496c831ae8fa533c620c
pshah <pshah>
2003-11-13 23:32:33 +0000

    Simplified the notificaiton mechanism.  No need to layout on zoom or unit changes anymore because the ruler's width does not depend on its height anymore.  Layout is not done recursively either.  Also setting the font for the ruler graphical viewers.

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/rulers/RulerComposite.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/rulers/RulerComposite.java
@@ -3,21 +3,14 @@
  */
 package org.eclipse.gef.examples.logicdesigner.rulers;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.widgets.*;
 
 import org.eclipse.draw2d.*;
-import org.eclipse.draw2d.geometry.Dimension;
-import org.eclipse.draw2d.geometry.Transposer;
 
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.GraphicalViewer;
-import org.eclipse.gef.editparts.ZoomListener;
-import org.eclipse.gef.editparts.ZoomManager;
 import org.eclipse.gef.ui.parts.GraphicalViewerKeyHandler;
 import org.eclipse.gef.ui.parts.ScrollingGraphicalViewer;
 
@@ -28,15 +21,20 @@ public class RulerComposite
 	extends Composite
 {
 	
-private Dimension cachedEditorSize;
 private GraphicalViewer left, right, top, bottom;
 private FigureCanvas editor;
 private GraphicalViewer diagramViewer;
-private boolean layingOut = false, needToLayout = false;
+private boolean layingOut = false;
+private Font font;
 
 public RulerComposite(Composite parent, int style) {
 	super(parent, style);
 }
+
+/*
+ * @TODO:Pratik    add methods to remove rulers, add them to a specific index, hide them,
+ * etc.
+ */
 
 public void addRuler(Ruler ruler, int orientation) {
 	getRulerContainer(orientation).setContents(ruler);
@@ -65,6 +63,14 @@ protected GraphicalViewer createRulerContainer(int orientation) {
 	viewer.setKeyHandler(new GraphicalViewerKeyHandler(viewer));
 	FigureCanvas canvas = getFigureCanvas(viewer);
 	canvas.setScrollBarVisibility(FigureCanvas.NEVER);
+	if (font == null) {
+		FontData[] data = canvas.getFont().getFontData();
+		for (int i = 0; i < data.length; i++) {
+			data[i].height -= 1;
+		}
+		font = new Font(Display.getCurrent(), data);
+	}
+	canvas.setFont(font);
 	if (editor != null) {
 		if (isHorizontal) {
 			canvas.getViewport().setHorizontalRangeModel(
@@ -75,6 +81,13 @@ protected GraphicalViewer createRulerContainer(int orientation) {
 		}
 	}
 	return viewer;
+}
+
+public void dispose() {
+	super.dispose();
+	if (font != null) {
+		font.dispose();
+	}
 }
 
 public GraphicalViewer getRulerContainer(int orientation) {
@@ -132,98 +145,54 @@ public void layout(boolean change) {
 		checkWidget();
 		layingOut = true;
 		invalidateChildren(true);	
-		relayout(editor.getSize());	
-		needToLayout = false;
+		doLayout();	
 		layingOut = false;
 	}
 }
 
-private boolean layoutChildIfChanged(GraphicalViewer viewer, boolean isHorizontal) {
-	boolean result = false;
-	if (viewer != null) {
-		FigureCanvas child = getFigureCanvas(viewer);
-		if (child.getContents() != null) {
-			Transposer transposer = new Transposer();
-			transposer.setEnabled(isHorizontal);
-			invalidateChild(viewer, false);
-			Dimension prefSize = transposer.t(child.getContents().getPreferredSize());
-			if (prefSize.width != transposer.t(new Dimension(child.getSize())).width) {
-				result = true;
-				layout(true);
-			} else if (prefSize.height 
-					!= transposer.t(child.getContents().getSize()).height) {
-				child.getLightweightSystem().getRootFigure().revalidate();
-				child.getLightweightSystem().getUpdateManager().performUpdate();
-			} else {
-				child.getContents().repaint();
-			}
-		}
-	}
-	return result;
-}
-
-public boolean layoutIfNecessary() {
-	return layoutChildIfChanged(left, false) || layoutChildIfChanged(right, false) 
-			|| layoutChildIfChanged(top, true) || layoutChildIfChanged(bottom, true);
-}
-
-private void relayout(Point editorCache) {
-	Point size, editorSize;
+private void doLayout() {
 	int leftWidth, rightWidth, topHeight, bottomHeight;
-
-	int vBarSize = 0, hBarSize = 0;
-	if (editor.getVerticalBar().isVisible()) {
-		vBarSize = editor.computeTrim(0, 0, 0, 0).width;
-	}
-	if (editor.getHorizontalBar().isVisible()) {
-		hBarSize = editor.computeTrim(0, 0, 0, 0).height;
-	}
 	leftWidth = left == null ? 0 
-			: left.getControl().computeSize(SWT.DEFAULT, editorCache.y - hBarSize).x;
+			: left.getControl().computeSize(SWT.DEFAULT, SWT.DEFAULT).x;
 	rightWidth = right == null ? 0 
-			: right.getControl().computeSize(SWT.DEFAULT, editorCache.y - hBarSize).x;
+			: right.getControl().computeSize(SWT.DEFAULT, SWT.DEFAULT).x;
 	topHeight = top == null ? 0 
-			: top.getControl().computeSize(editorCache.x - vBarSize, SWT.DEFAULT).y;
+			: top.getControl().computeSize(SWT.DEFAULT, SWT.DEFAULT).y;
 	bottomHeight = bottom == null ? 0 
-			: bottom.getControl().computeSize(editorCache.x - vBarSize, SWT.DEFAULT).y;
+			: bottom.getControl().computeSize(SWT.DEFAULT, SWT.DEFAULT).y;
 
-	size = getSize();
-	// editorSize can go into the negative if the RulerComposite is not big enough to fit
-	// all the rulers.  However, that is not a problem because editor.setSize(int, int)
-	// checks to ensure that the size is at least 0, 0.
-	editorSize = new Point(size.x - (leftWidth + rightWidth), 
+	Point size = getSize();
+	Point editorSize = new Point(size.x - (leftWidth + rightWidth), 
 	                       size.y - (topHeight + bottomHeight));
-	if (!editorSize.equals(editorCache)) {
-		editor.setSize(editorSize);
-		/*
-		 * @TODO:Pratik
-		 * This is a hack.  Setting the editorSize (line above) is, for some reason, not
-		 * causing the scrollbars to disappear in the case where the editor has just
-		 * been opened (even though the size being set is big enough so that the 
-		 * scrollbars are not needed).  Calling setBounds(editor.getLocation().x, 
-		 * editor.getLocation().y,editorSize.x,editorSize.y) does not do it either.  
-		 * However, calling setLocation(editor.getLocation()) (line below) does it.  What 
-		 * a piece of crap.
-		 */
-		editor.setLocation(editor.getLocation());
-		invalidateChildren(true);
-		relayout(editorSize);
-	} else {
-		editor.setLocation(leftWidth, topHeight);
-		if (left != null) {
-			left.getControl().setBounds(0, topHeight, leftWidth, editorSize.y - hBarSize);
-		}
-		if (right != null) {
-			right.getControl().setBounds(leftWidth + editorSize.x, topHeight, 
-					rightWidth, editorSize.y - hBarSize);
-		}
-		if (top != null) {
-			top.getControl().setBounds(leftWidth, 0, editorSize.x - vBarSize, topHeight);
-		}
-		if (bottom != null) {
-			bottom.getControl().setBounds(leftWidth, topHeight + editorSize.y, 
-					editorSize.x - vBarSize, bottomHeight);
-		}
+	if (!editor.getSize().equals(editorSize)) {
+		editor.setSize(editorSize);		
+	}
+	Point editorLocation = new Point(leftWidth, topHeight);
+	if (!editor.getLocation().equals(editorLocation)) {
+		editor.setLocation(editorLocation);
+	}
+
+	int vBarWidth = 0, hBarHeight = 0;
+	if (editor.getVerticalBar().getVisible()) {
+		vBarWidth = editor.computeTrim(0, 0, 0, 0).width;
+	}
+	if (editor.getHorizontalBar().getVisible()) {
+		hBarHeight = editor.computeTrim(0, 0, 0, 0).height;
+	}
+	
+	if (left != null) {
+		left.getControl().setBounds(0, topHeight, leftWidth, editorSize.y - hBarHeight);
+	}
+	if (right != null) {
+		right.getControl().setBounds(leftWidth + editorSize.x, topHeight, 
+				rightWidth, editorSize.y - hBarHeight);
+	}
+	if (top != null) {
+		top.getControl().setBounds(leftWidth, 0, editorSize.x - vBarWidth, topHeight);
+	}
+	if (bottom != null) {
+		bottom.getControl().setBounds(leftWidth, topHeight + editorSize.y, 
+				editorSize.x - vBarWidth, bottomHeight);
 	}
 }
 
@@ -236,6 +205,10 @@ public void setGraphicalViewer(GraphicalViewer primaryViewer) {
 	diagramViewer = primaryViewer;
 	editor = getFigureCanvas(primaryViewer);
 
+	/*
+	 * @TODO:Pratik
+	 * can all these graphical viewers share the same factory?
+	 */
 	if (left != null) {
 		left.setEditPartFactory(new RulerEditPartFactory(diagramViewer));
 		getFigureCanvas(left).getViewport().setVerticalRangeModel(
@@ -257,55 +230,20 @@ public void setGraphicalViewer(GraphicalViewer primaryViewer) {
 				editor.getViewport().getHorizontalRangeModel());
 	}
 
-	addListener(SWT.Resize, new Listener() {
+	// layout whenever the scrollbars are shown or hidden, and whenever the RulerComposite
+	// is resized
+	Listener layoutListener = new Listener() {
 		public void handleEvent(Event event) {
-			layout(true);
-		}
-	});
-	
-	// layout whenever the scrollbars are shown or hidden
-	Listener scrollBarListener = new Listener() {
-		public void handleEvent(Event event) {
-			needToLayout = true;
-		}
-	};
-	editor.getHorizontalBar().addListener(SWT.Show, scrollBarListener);
-	editor.getHorizontalBar().addListener(SWT.Hide, scrollBarListener);
-	editor.getVerticalBar().addListener(SWT.Show, scrollBarListener);
-	editor.getVerticalBar().addListener(SWT.Hide, scrollBarListener);
-
-	cachedEditorSize = editor.getContents().getSize();
-	PropertyChangeListener rangeModelListener = new PropertyChangeListener() {
-		public void propertyChange(PropertyChangeEvent evt) {
-			String property = evt.getPropertyName();
-			if (property.equals(RangeModel.PROPERTY_MAXIMUM)
-					|| property.equals(RangeModel.PROPERTY_MINIMUM)
-					|| property.equals(RangeModel.PROPERTY_EXTENT)) {
-				if (!layingOut) {
-					if (needToLayout) {
-						layout(true);
-					} else if (!editor.getContents().getSize().equals(cachedEditorSize)) {
-						layoutIfNecessary();
-					}
-				}
-				cachedEditorSize = editor.getContents().getSize();
+			if (!layingOut) {
+				layout(true);				
 			}
 		}
 	};
-	editor.getViewport().getHorizontalRangeModel()
-			.addPropertyChangeListener(rangeModelListener);
-	editor.getViewport().getVerticalRangeModel()
-			.addPropertyChangeListener(rangeModelListener);
-
-	ZoomManager manager = 
-			(ZoomManager)diagramViewer.getProperty(ZoomManager.class.toString());
-	if (manager != null) {
-		manager.addZoomListener(new ZoomListener() {
-			public void zoomChanged(double zoom) {
-				layoutIfNecessary();
-			}
-		});		
-	}
+	addListener(SWT.Resize, layoutListener);
+	editor.getHorizontalBar().addListener(SWT.Show, layoutListener);
+	editor.getHorizontalBar().addListener(SWT.Hide, layoutListener);
+	editor.getVerticalBar().addListener(SWT.Show, layoutListener);
+	editor.getVerticalBar().addListener(SWT.Hide, layoutListener);
 }
 
 }
