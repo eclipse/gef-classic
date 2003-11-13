adfeaa5b72754f8630cd9730a42ce97deae7f75b
pshah <pshah>
2003-11-13 23:38:49 +0000

    Introducing the new, slick, MS Word-like rulers.  Voila!  A lot has been simplified since the ruler's width does not depend on how long it is.  The ruler repaints itself when zoom or its unit of measurement changes.

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/rulers/RulerFigure.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/rulers/RulerFigure.java
@@ -4,7 +4,7 @@
 package org.eclipse.gef.examples.logicdesigner.rulers;
 
 
-import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
 
 import org.eclipse.draw2d.*;
@@ -23,68 +23,39 @@ public class RulerFigure
 /**
  * These fields allow the client to customize the look of the ruler.
  */
-public int smallMarkWidth = 5;
-public int mediumMarkWidth = 10;
-public int textLeftMargin = 2;
-public int textRightMargin = 2;
-public int textTopMargin = 2;
-public int textBottomMargin = 4;
-public int minPixelsBetweenMarks = 3;
+public int smallMarkWidth = 2;
+public int mediumMarkWidth = 4;
+public int textMargin = 3;
+public int minPixelsBetweenMarks = 5;
+public int minPixelsBetweenMajorMarks = 47;
 
 protected Transposer transposer = new Transposer();
-protected FigureCanvas editor;
 protected ZoomManager zoom;
 
-private static final Dimension BLANK_HSPACE = new Dimension(0, 2);
-private static final Dimension BLANK_VSPACE = new Dimension(2, 0);
+/*
+ * This is an artificial border.  When asked for the preferred size, the figure adds 
+ * this width to its preferred width.  However, when painting, it doesn't touch
+ * these pixels (which will be to the right in a vertical ruler).  Hence, those pixels
+ * will have the parent's background color filled.  That is the intended "border."
+ */
+private static final int BORDER_WIDTH = 2;
 
 private boolean horizontal;
-private int unit, interval, divisions, cachedHeight, cachedY, cachedLength = 0;
+private int unit, interval, divisions;
 private double dpu = -1.0;
-private Dimension textSize, cachedPrefSize, cachedTextSize;
 
 private ZoomListener zoomListener = new ZoomListener() {
 	public void zoomChanged(double newZoomValue) {
-		layout();
+		handleZoomChanged();
 	}
 };
 
-public RulerFigure(FigureCanvas editorCanvas, boolean isHorizontal, int measurementUnit) {
-	editor = editorCanvas;
+public RulerFigure(boolean isHorizontal, int measurementUnit) {
 	setHorizontal(isHorizontal);
 	setUnit(measurementUnit);
 	setBackgroundColor(ColorConstants.white);
 	setOpaque(true);
 	setLayoutManager(new RulerLayout());
-}
-
-private void calculateTextSize(Rectangle transposedBounds) {
-	int numLength = ("" //$NON-NLS-1$
-			+ (int)((transposedBounds.height + transposedBounds.y) / getDPU())).length();
-	if (transposedBounds.y < 0) {
-		numLength = Math.max(numLength, 
-				("" + (int)(transposedBounds.y / getDPU())).length()); //$NON-NLS-1$
-	}
-	numLength = Math.max(2, numLength);
-	textSize = getSizeOfText(numLength);
-}
-
-/*
- * @TODO:Pratik
- * 
- * this is really a hack.  is this acceptable?  the ruler should not really
- * know about the rulercomposite.  maybe it can be passed in as a parameter to the 
- * constructor of this class.  or maybe you can get rid of this method completely.  it 
- * then becomes the client's responsibility to force a layout when necessary (say, when 
- * the units change).
- */
-protected void forceSWTLayoutIfNecessary() {
-	Composite parent = editor.getParent();
-	if (parent instanceof RulerComposite) {
-		((RulerComposite)parent).layoutIfNecessary();
-	} else {
-		parent.layout(true);
-	}
 }
 
 protected double getDPU() {
@@ -104,63 +75,44 @@ protected double getDPU() {
 	return dpu;
 }
 
-// @TODO:Pratik    you should change this so that the preferred size is being calculated
-// based on the range model, and not the size of the editor.  this way, the editor
-// won't have to be passed around.
 public Dimension getPreferredSize(int wHint, int hHint) {
-	Rectangle viewBounds = transposer.t(editor.getContents().getClientArea());
-	if (cachedHeight != viewBounds.height || cachedY != viewBounds.y 
-			|| cachedPrefSize == null) {
-		cachedHeight = viewBounds.height;
-		cachedY = viewBounds.y;
-		calculateTextSize(viewBounds);
-		cachedPrefSize = transposer.t(new Dimension(transposer.t(textSize).width 
-				+ textLeftMargin + textRightMargin + mediumMarkWidth, 
-				viewBounds.height));
-		cachedPrefSize.expand(isHorizontal() ? BLANK_HSPACE : BLANK_VSPACE);
-		if (getBorder() != null) {
-			cachedPrefSize.expand(getBorder().getInsets(this).getWidth(), 
-					getBorder().getInsets(this).getHeight());
-		}
+	Dimension prefSize = new Dimension();
+	if (isHorizontal()) {
+		// the plus 2 is to draw the two lines of border at the right
+		prefSize.height = (textMargin * 2) + BORDER_WIDTH
+				+ FigureUtilities.getFontMetrics(getFont()).getAscent();
+	} else {
+		prefSize.width = (textMargin * 2) + BORDER_WIDTH
+				+ FigureUtilities.getFontMetrics(getFont()).getAscent();
 	}
-	return cachedPrefSize.getCopy();
-}
-
-// length must be greater than or equal to 1
-private Dimension getSizeOfText(int length) {
-	if (length != cachedLength) {
-		String number = "8"; //$NON-NLS-1$
-		for (int i = 1; i < length; i++) {
-			number += "8"; //$NON-NLS-1$
-		}
-		cachedTextSize = FigureUtilities.getStringExtents(number, getFont());
-		cachedLength = length;
-	}
-	return cachedTextSize;
+	return prefSize;
 }
 
 public int getUnit() {
 	return unit;
 }
 
+protected void handleZoomChanged() {
+	dpu = -1.0;
+	repaint();
+	layout();
+}
+
 /* (non-Javadoc)
  * @see org.eclipse.draw2d.Figure#invalidate()
  */
 public void invalidate() {
-	if (isValid()) {
-		super.invalidate();
-		// we have to invalidate the cachedLength and the cachedTextSize because the
-		// font could have changed
-		cachedLength = 0;
-		cachedTextSize = null;
-	}
-	cachedPrefSize = null;
+	super.invalidate();
 	dpu = -1.0;
 }
 
 public boolean isHorizontal() {
 	return horizontal;
 }
+
+/*
+ * @TODO:Pratik    re-comment this algorithm and the setInterval method
+ */
 
 /*
  * (non-Javadoc)
@@ -175,8 +127,7 @@ protected void paintFigure(Graphics graphics) {
 	// entire ruler will not be repainted everytime.
 	Rectangle clippedBounds = clip;
 	clippedBounds.x = figClientArea.x;
-	clippedBounds.width = figClientArea.width - 2;// leave two pixels for the two lines
-												// drawn at the end of this method
+	clippedBounds.width = figClientArea.width - BORDER_WIDTH;
 	
 	// Paint the background
 	if (isOpaque()) {
@@ -187,14 +138,12 @@ protected void paintFigure(Graphics graphics) {
 	 * A major mark is one that goes all the way from the left edge to the right edge of
 	 * a ruler and for which a number is displayed.  Determine the minimum number of
 	 * pixels that are to be left between major marks.  This will, in turn, help
-	 * determine how many units are to be displayed per major mark.  A major should have
-	 * at least enough pixels to display the text and its padding.  We take into the
+	 * determine how many units are to be displayed per major mark.  A major mark should
+	 * have at least enough pixels to display the text and its padding.  We take into the
 	 * consideration the max of text's width and height so that for horizontal and
 	 * vertical rulers that are of the same height, the number of units per major mark is
 	 * the same.
 	 */
-	int minPixelsBetweenMajorMarks = Math.max(textSize.width, textSize.height) 
-			+ textTopMargin + textBottomMargin;
 	int unitsPerMajorMark = (int)(minPixelsBetweenMajorMarks / dotsPerUnit);
 	if (minPixelsBetweenMajorMarks % dotsPerUnit != 0.0) {
 		unitsPerMajorMark++;
@@ -244,7 +193,7 @@ protected void paintFigure(Graphics graphics) {
 		if (getUnit() == Ruler.UNIT_CENTIMETERS) {
 			divsPerMajorMark = 10;
 		} else if (getUnit() == Ruler.UNIT_INCHES) {
-			divsPerMajorMark = 16;
+			divsPerMajorMark = 8;
 		}
 		while (dotsPerUnit * unitsPerMajorMark / divsPerMajorMark < minPixelsBetweenMarks) {
 			divsPerMajorMark /= 2;
@@ -275,9 +224,11 @@ protected void paintFigure(Graphics graphics) {
 			break;
 		case 16:
 		case 8:
-			mediumMarkerDivNum = 2;
+			mediumMarkerDivNum = 4;
 			break;
 		case 4:
+			mediumMarkerDivNum = 2;
+			break;
 		case 2:
 			mediumMarkerDivNum = 1;
 	}
@@ -301,34 +252,67 @@ protected void paintFigure(Graphics graphics) {
 	}
 	// endMark is the last visible mark (doesn't have to be a major mark) that is to be painted
 	int endMark = (int)(((clippedBounds.y + clippedBounds.height) / dotsPerDivision));
+	int leading = FigureUtilities.getFontMetrics(getFont()).getLeading();
+	Rectangle forbiddenZone = new Rectangle();
 	for (int div = startMark; div <= endMark; div++) {
 		// y is the vertical position of the mark 
 		int y = (int)(div * dotsPerDivision);
-		Point start = new Point(clippedBounds.getRight().x - smallMarkWidth, y);
-		Point end = new Point(clippedBounds.getRight().x, y);
 		if (div % divsPerMajorMark == 0) {
-			// this is a major mark.  so it should go all the way across, and the number
-			// should be displayed 
-			start.x = clippedBounds.x;
-			graphics.drawText("" + (div / divsPerMajorMark) * unitsPerMajorMark,  //$NON-NLS-1$
-					transposer.t(start.getTranslated(textLeftMargin, textTopMargin)));
+			String num = "" + (div / divsPerMajorMark) * unitsPerMajorMark; //$NON-NLS-1$
+			if (isHorizontal()) {
+				Dimension numSize = FigureUtilities.getStringExtents(num, getFont());
+				numSize.width--;
+				Point textLocation = new Point(y - (numSize.width / 2), 
+						clippedBounds.x + textMargin - leading); 
+				forbiddenZone.setLocation(textLocation);
+				forbiddenZone.setSize(numSize);
+				forbiddenZone.expand(1, 1);
+				graphics.fillRectangle(forbiddenZone);
+				graphics.drawText(num, textLocation);
+			} else {
+				Image numImage = ImageUtilities.getRotatedLeft(num, getFont(), 
+						getForegroundColor(), getBackgroundColor());
+				Point textLocation = new Point(clippedBounds.x + textMargin,
+						y - (numImage.getBounds().height / 2));
+				forbiddenZone.setLocation(textLocation);
+				forbiddenZone.setSize(numImage.getBounds().width, 
+						numImage.getBounds().height);
+				forbiddenZone.expand(1, 1);
+				graphics.fillRectangle(forbiddenZone);
+				graphics.drawImage(numImage, textLocation);
+				numImage.dispose();
+			}
 		} else if ((div % divsPerMajorMark) % mediumMarkerDivNum == 0) {
 			// this is a medium mark, so its length should be longer than the small marks  
-			start.x = clippedBounds.getRight().x - mediumMarkWidth;
+			Point start = transposer.t(
+					new Point((clippedBounds.getRight().x - mediumMarkWidth) / 2, y));
+			Point end = transposer.t(new Point(((clippedBounds.getRight().x 
+					- mediumMarkWidth) / 2) + mediumMarkWidth, y));
+			if (!forbiddenZone.contains(start)) {
+				graphics.drawLine(start, end);				
+			}
+		} else {
+			// small mark
+			Point start = transposer.t(
+					new Point((clippedBounds.getRight().x - smallMarkWidth) / 2, y));
+			Point end = transposer.t(new Point(((clippedBounds.getRight().x 
+					- smallMarkWidth) / 2) + smallMarkWidth, y));
+			if (!forbiddenZone.contains(start)) {
+				graphics.drawLine(start, end);				
+			}
 		}
-		graphics.drawLine(transposer.t(start), transposer.t(end));
 	}
 
 	// draw the lines on the right of the ruler
-	clippedBounds.width += 2;
-	graphics.setForegroundColor(ColorConstants.button);
-	Point reduction = transposer.t(new Point(-1, 0));
-	Point topRight = transposer.t(clippedBounds.getTopRight()).translate(reduction);
-	Point bottomRight = transposer.t(clippedBounds.getBottomRight()).translate(reduction);
-	graphics.drawLine(topRight, bottomRight);
-	topRight.translate(reduction);
-	bottomRight.translate(reduction);
-	graphics.drawLine(topRight, bottomRight);
+//	clippedBounds.width += BORDER_WIDTH;
+//	graphics.setForegroundColor(ColorConstants.button);
+//	Point reduction = transposer.t(new Point(-1, 0));
+//	Point topRight = transposer.t(clippedBounds.getTopRight()).translate(reduction);
+//	Point bottomRight = transposer.t(clippedBounds.getBottomRight()).translate(reduction);
+//	graphics.drawLine(topRight, bottomRight);
+//	topRight.translate(reduction);
+//	bottomRight.translate(reduction);
+//	graphics.drawLine(topRight, bottomRight);
 }
 
 public void setHorizontal(boolean isHorizontal) {
@@ -359,8 +343,8 @@ public void setInterval(int unitsPerMajorMark, int divisionsPerMajorMark) {
 public void setUnit(int newUnit) {
 	if (unit != newUnit) {
 		unit = newUnit;
-		invalidate();
-		forceSWTLayoutIfNecessary();
+		dpu = -1.0;
+		repaint();
 	}
 }
 
