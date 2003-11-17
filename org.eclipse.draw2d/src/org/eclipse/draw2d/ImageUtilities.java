0165ef8200dac67e5e4c126fc8227d0a11f90f9c
rhudson <rhudson>
2003-11-17 19:14:42 +0000

    *** empty log message ***

--- org.eclipse.draw2d/src/org/eclipse/draw2d/ImageUtilities.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/ImageUtilities.java
@@ -1,11 +1,7 @@
-/*
- * Created on Nov 12, 2003
- */
 package org.eclipse.draw2d;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.widgets.Display;
 
 import org.eclipse.draw2d.geometry.Dimension;
@@ -15,26 +11,42 @@ import org.eclipse.draw2d.geometry.Dimension;
  */
 public class ImageUtilities {
 
-public static Image getRotatedLeft(String str, Font font, 
-								   Color foreground, Color background) {
+/**
+ * Returns a newly created image for the given string.  The string will be rendered using
+ * the provided colors and fonts.  The caller is responsible for disposing the returned
+ * Image.  Strings cannot contain newlines or tab characters.
+ * @param string the String to be rendered
+ * @param font the font
+ * @param foreground the text's color
+ * @param background the background color
+ * @return an Image which must be disposed
+ */
+public static Image createRotatedImageOfString(
+		String string,
+		Font font,
+		Color foreground,
+		Color background) {
 	Display display = Display.getCurrent();
-	if (display == null) {
-		display = Display.getDefault();
-	}
-	FontMetrics fMetrics = FigureUtilities.getFontMetrics(font);
-	Dimension strSize = FigureUtilities.getStringExtents(str, font);
+	if (display == null)
+		SWT.error(SWT.ERROR_THREAD_INVALID_ACCESS);
+	
+	FontMetrics metrics = FigureUtilities.getFontMetrics(font);
+	Dimension strSize = FigureUtilities.getStringExtents(string, font);
 	strSize.width--;
-	Image srcImage = new Image(display, strSize.width, fMetrics.getAscent());
+	Image srcImage = new Image(display, strSize.width, metrics.getAscent());
 	GC gc = new GC(srcImage);
 	gc.setFont(font);
 	gc.setForeground(foreground);
 	gc.setBackground(background);
 	gc.fillRectangle(srcImage.getBounds());
-	gc.drawText(str, 0, 0 - fMetrics.getLeading());
-	return rotateLeft(srcImage);
+	gc.drawString(string, 0, 0 - metrics.getLeading());
+	Image result = createRotatedImage(srcImage);
+	gc.dispose();
+	srcImage.dispose();
+	return result;
 }
 
-public static Image rotateLeft(Image srcImage) {
+static Image createRotatedImage(Image srcImage) {
 	/*
 	 * @TODO:Pratik    optimize this by directly manipulating the byte array
 	 */
@@ -50,7 +62,7 @@ public static Image rotateLeft(Image srcImage) {
 			destData.setPixel(y, srcData.width - x - 1,	srcData.getPixel(x, y));
 		}
 	}
-	srcImage.dispose();
+
 	return new Image(display, destData);	
 }
 	
