babddce102021f4c1493184f7e5b89751c3ffe10
pshah <pshah>
2003-11-17 21:46:35 +0000

    Snap to guide now shows feedback by drawing a thick, red line along the edge that is going to snap to a guide.

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/edit/LogicXYLayoutEditPolicy.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/edit/LogicXYLayoutEditPolicy.java
@@ -10,21 +10,27 @@
  *******************************************************************************/
 package org.eclipse.gef.examples.logicdesigner.edit;
 
+import java.util.*;
+
+import org.eclipse.draw2d.*;
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
+
 import org.eclipse.gef.*;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.NonResizableEditPolicy;
+import org.eclipse.gef.requests.ChangeBoundsRequest;
+import org.eclipse.gef.requests.CreateRequest;
 
 import org.eclipse.gef.examples.logicdesigner.LogicMessages;
 import org.eclipse.gef.examples.logicdesigner.model.*;
 import org.eclipse.gef.examples.logicdesigner.model.commands.*;
 
-import org.eclipse.gef.requests.ChangeBoundsRequest;
-import org.eclipse.gef.requests.CreateRequest;
-
 public class LogicXYLayoutEditPolicy
 	extends org.eclipse.gef.editpolicies.XYLayoutEditPolicy
 {
+	
+private List figures = new ArrayList();
 
 protected Command createAddCommand(EditPart childEditPart, Object constraint) {
 
@@ -53,11 +59,22 @@ protected Command createChangeConstraintCommand(EditPart child, Object constrain
 	return locationCommand;
 }
 
-protected Command createChangeConstraintCommand(Request request, EditPart child,
-		Object constraint) {
+protected Command createChangeConstraintCommand(ChangeBoundsRequest request, 
+		EditPart child, Object constraint) {
 	SetConstraintCommand cmd = (SetConstraintCommand)createChangeConstraintCommand(
 			child, constraint);
-	cmd.setExtendedData((ChangeBoundsRequest)request);
+	Guide hGuide = (Guide)request.getExtendedData().get(SnapToGuides.HORIZONTAL_GUIDE);
+	if (hGuide != null) {
+		int hAlignment = ((Integer)request.getExtendedData()
+				.get(SnapToGuides.HORIZONTAL_ANCHOR)).intValue();
+		cmd.setHorizontalGuide(hGuide, hAlignment);
+	}
+	Guide vGuide = (Guide)request.getExtendedData().get(SnapToGuides.VERTICAL_GUIDE);
+	if (vGuide != null) {
+		int vAlignment = ((Integer)request.getExtendedData()
+				.get(SnapToGuides.VERTICAL_ANCHOR)).intValue();
+		cmd.setVerticalGuide(vGuide, vAlignment);
+	}
 	return cmd;
 }
 
@@ -68,6 +85,16 @@ protected EditPolicy createChildEditPolicy(EditPart child) {
 		return new NonResizableEditPolicy();
 	}
 	return super.createChildEditPolicy(child);
+}
+
+protected void eraseLayoutTargetFeedback(Request request) {
+	for (Iterator iter = figures.iterator(); iter.hasNext();) {
+		IFigure fig = (IFigure)iter.next();
+		if (getLayer(LayerConstants.FEEDBACK_LAYER).getChildren().contains(fig)) {
+			fig.getParent().remove(fig);
+		}
+	}
+	figures.clear();
 }
 
 protected Command getCreateCommand(CreateRequest request) {
@@ -86,6 +113,69 @@ protected Command getDeleteDependantCommand(Request request) {
 
 protected Command getOrphanChildrenCommand(Request request) {
 	return null;
+}
+
+protected void showLayoutTargetFeedback(Request request) {
+	eraseLayoutTargetFeedback(request);
+	if (request.getType().equals(REQ_MOVE) || request.getType().equals(REQ_RESIZE)) {
+		ChangeBoundsRequest req = (ChangeBoundsRequest)request;
+		Guide vGuide = (Guide)req.getExtendedData()
+				.get(SnapToGuides.VERTICAL_GUIDE);
+		if (vGuide != null) {
+			int alignment = ((Integer)req.getExtendedData()
+					.get(SnapToGuides.VERTICAL_ANCHOR)).intValue();
+			IFigure part = ((GraphicalEditPart)req.getEditParts().get(0)).getFigure();
+			Point start = null, end = null;
+			Rectangle figBounds = part.getBounds();
+			switch (alignment) {
+				case -1:
+					start = figBounds.getTopLeft().translate(req.getMoveDelta());
+					end = figBounds.getBottomLeft().translate(req.getMoveDelta());
+					break;
+				case 0:
+					start = figBounds.getTop().translate(req.getMoveDelta());
+					end = figBounds.getBottom().translate(req.getMoveDelta());
+					break;
+				case 1:
+					start = figBounds.getTopRight().translate(req.getMoveDelta());
+					end = figBounds.getBottomRight().translate(req.getMoveDelta());
+			}
+			Figure fig = new Figure();
+			fig.setOpaque(true);
+			fig.setBackgroundColor(ColorConstants.red);
+			getLayer(LayerConstants.FEEDBACK_LAYER).add(fig);
+			fig.setBounds(new Rectangle(start.x - 1, start.y, 3, end.y - start.y));
+			figures.add(fig);
+		}
+		Guide hGuide = (Guide)req.getExtendedData()
+				.get(SnapToGuides.HORIZONTAL_GUIDE);
+		if (hGuide != null) {
+			int alignment = ((Integer)req.getExtendedData()
+					.get(SnapToGuides.HORIZONTAL_ANCHOR)).intValue();
+			IFigure part = ((GraphicalEditPart)req.getEditParts().get(0)).getFigure();
+			Point start = null, end = null;
+			Rectangle figBounds = part.getBounds();
+			switch (alignment) {
+				case -1:
+					start = figBounds.getTopLeft().translate(req.getMoveDelta());
+					end = figBounds.getTopRight().translate(req.getMoveDelta());
+					break;
+				case 0:
+					start = figBounds.getLeft().translate(req.getMoveDelta());
+					end = figBounds.getRight().translate(req.getMoveDelta());
+					break;
+				case 1:
+					start = figBounds.getBottomLeft().translate(req.getMoveDelta());
+					end = figBounds.getBottomRight().translate(req.getMoveDelta());
+			}
+			Figure fig = new Figure();
+			fig.setOpaque(true);
+			fig.setBackgroundColor(ColorConstants.red);
+			getLayer(LayerConstants.FEEDBACK_LAYER).add(fig);
+			fig.setBounds(new Rectangle(start.x, start.y - 1, end.x - start.x, 3));
+			figures.add(fig);
+		}
+	}
 }
 
 }
