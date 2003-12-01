149e5f5ac654c9f04120775145a8f2177936a2ef
pshah <pshah>
2003-12-01 04:49:29 +0000

    Added the following features: (1) Guides cannot be created within five pixels of each other.  (2) Button down + dragging creates the guide and starts dragging it.  This is done by forwarding events to the GuideEditPart's drag tracker.  (3) Creating a guide will now select it as well.

--- org.eclipse.gef/src/org/eclipse/gef/internal/ui/rulers/RulerEditPart.java
+++ org.eclipse.gef/src/org/eclipse/gef/internal/ui/rulers/RulerEditPart.java
@@ -10,13 +10,18 @@
  *******************************************************************************/
 package org.eclipse.gef.internal.ui.rulers;
 
+import java.util.Iterator;
 import java.util.List;
+
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.graphics.Cursor;
 
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.Point;
 
 import org.eclipse.gef.*;
 import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.UnexecutableCommand;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 import org.eclipse.gef.editparts.ZoomManager;
 import org.eclipse.gef.tools.SimpleDragTracker;
@@ -60,7 +65,13 @@ public void activate() {
  * @see org.eclipse.gef.editparts.AbstractEditPart#createEditPolicies()
  */
 protected void createEditPolicies() {
-	// no edit policies for a ruler
+	/*
+	 * @TODO:Pratik   the right way of creating guides and figuring out the target
+	 * edit part is by installing an edit policy with container role.  talk to randy about
+	 * how this should work.  would isMove() in GuideEditPart's drag tracker still have to
+	 * return true all the time?
+	 */
+//	installEditPolicy(EditPolicy.CONTAINER_ROLE, );
 }
 
 /* (non-Javadoc)
@@ -85,28 +96,90 @@ public void deactivate() {
  */
 public DragTracker getDragTracker(Request request) {
 	return new SimpleDragTracker() {
+		private DragTracker guideTracker;
+		private int position = Integer.MAX_VALUE;
+		protected Cursor getDisabledCursor() {
+			return SharedCursors.NO;
+		}
 		protected boolean handleButtonDown(int button) {
 			updateSourceRequest();
 			setCurrentCommand(getCommand());
-			executeCurrentCommand();
+			if (getCurrentCommand().canExecute()) {
+				executeCurrentCommand();							
+			} else {
+				setState(STATE_INVALID);
+			}
+			if (button != MOUSE_BUTTON1) {
+				setState(STATE_INVALID);
+			}
 			return true;
 		}
 		protected Command getCommand() {
 			Point pt = getStartLocation();
 			getFigure().translateToRelative(pt);
-			int position = isHorizontal() ? pt.x : pt.y;
+			position = isHorizontal() ? pt.x : pt.y;
 			ZoomManager zoom = (ZoomManager)diagramViewer
 					.getProperty(ZoomManager.class.toString());
 			if (zoom != null) {
-				position *= (zoom.getUIMultiplier() / zoom.getZoom());
+				position = (int)Math.round(
+						position * (zoom.getUIMultiplier() / zoom.getZoom()));
 			}
-			return getRulerProvider().getCreateGuideCommand(position);
+			if (isCreationValid()) {
+				return getRulerProvider().getCreateGuideCommand(position);				
+			} else {
+				return UnexecutableCommand.INSTANCE;
+			}
 		}
 		protected String getCommandName() {
 			return REQ_CREATE;
 		}
 		protected String getDebugName() {
 			return "Guide creation"; //$NON-NLS-1$
+		}
+		protected Object getNewGuide() {
+			Iterator guides = getRulerProvider().getGuides().iterator();
+			while (guides.hasNext()) {
+				Object guide = guides.next();
+				if (getRulerProvider().getGuidePosition(guide) == position) {
+					return guide;
+				}
+			}
+			return null;
+		}
+		protected boolean isCreationValid() {
+			boolean result = true;
+			Iterator guides = getRulerProvider().getGuides().iterator();
+			while (guides.hasNext()) {
+				int guidePos = getRulerProvider().getGuidePosition(guides.next());
+				if (Math.abs(guidePos - position) < GuideEditPart.MIN_DISTANCE_BW_GUIDES) {
+					result = false;
+					break;
+				}
+			}
+			return result;
+		}
+		public void mouseDown(MouseEvent mouseEvent, EditPartViewer viewer) {
+			super.mouseDown(mouseEvent, viewer);
+			if (getState() != STATE_INVALID) {
+				guideTracker = ((EditPart)getViewer().getEditPartRegistry()
+						.get(getNewGuide())).getDragTracker(null);
+				guideTracker.setEditDomain(getDomain());
+				guideTracker.activate();
+				guideTracker.setViewer(getViewer());
+				guideTracker.mouseDown(mouseEvent, viewer);
+			}
+		}
+		public void mouseDrag(MouseEvent mouseEvent, EditPartViewer viewer) {
+			if (getState() != STATE_INVALID) {
+				guideTracker.mouseDrag(mouseEvent, viewer);
+			}
+		}
+		public void mouseUp(MouseEvent mouseEvent, EditPartViewer viewer) {
+			if (getState() != STATE_INVALID) {
+				guideTracker.mouseUp(mouseEvent, viewer);
+				guideTracker.deactivate();
+				guideTracker = null;				
+			}
 		}
 	};
 }
@@ -144,6 +217,10 @@ public EditPart getTargetEditPart(Request request) {
 
 public void handleGuideReparented(Object guide) {
 	refreshChildren();
+	EditPart guidePart = (EditPart)getViewer().getEditPartRegistry().get(guide);
+	if (guidePart != null) {
+		getViewer().select(guidePart);
+	}
 }
 
 public void handleUnitsChanged(int newUnit) {
