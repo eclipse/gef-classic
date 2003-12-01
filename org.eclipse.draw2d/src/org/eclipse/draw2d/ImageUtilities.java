35703b5f3799048753dbb7d2f373bd5c4ecdadee
pshah <pshah>
2003-12-01 04:33:37 +0000

    Optimized createRotatedImage(Image) so that it directly manipulates the byte array instead of invoking setPixel() for every pixel.

--- org.eclipse.draw2d/src/org/eclipse/draw2d/ImageUtilities.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/ImageUtilities.java
@@ -12,27 +12,27 @@ import org.eclipse.draw2d.geometry.Dimension;
 public class ImageUtilities {
 
 /**
- * Returns a newly created image for the given string.  The string will be rendered using
- * the provided colors and fonts.  The caller is responsible for disposing the returned
- * Image.  Strings cannot contain newlines or tab characters.
+ * Returns a new Image with the given String rotated left (by 90 degrees).  The String
+ * will be rendered using the provided colors and fonts.  The client is responsible for
+ * disposing the returned Image.  Strings cannot contain newline or tab characters.
+ * 
  * @param string the String to be rendered
  * @param font the font
  * @param foreground the text's color
  * @param background the background color
  * @return an Image which must be disposed
  */
-public static Image createRotatedImageOfString(
-		String string,
-		Font font,
-		Color foreground,
-		Color background) {
+public static Image createRotatedImageOfString(String string, Font font, 
+                                               Color foreground, Color background) {
 	Display display = Display.getCurrent();
 	if (display == null)
 		SWT.error(SWT.ERROR_THREAD_INVALID_ACCESS);
 	
 	FontMetrics metrics = FigureUtilities.getFontMetrics(font);
 	Dimension strSize = FigureUtilities.getStringExtents(string, font);
-	strSize.width--;
+	// the width is being reduced by 1 because FigureUtilties, for some reason, increases
+	// it by 1
+	strSize.width--;		
 	Image srcImage = new Image(display, strSize.width, metrics.getAscent());
 	GC gc = new GC(srcImage);
 	gc.setFont(font);
@@ -46,24 +46,37 @@ public static Image createRotatedImageOfString(
 	return result;
 }
 
-static Image createRotatedImage(Image srcImage) {
-	/*
-	 * @TODO:Pratik    optimize this by directly manipulating the byte array
-	 */
+/**
+ * Returns a new Image that is the given Image rotated left by 90 degrees.  The client is
+ * responsible for disposing the returned Image.
+ * <p> 
+ * <b>IMPORTANT:</b> Rotating images that have padded scanlines may have undesired
+ * effects.
+ * 
+ * @param	srcImage	the Image that is to be rotated left
+ * @return	the rotated Image (the client is responsible for disposing it)
+ */
+public static Image createRotatedImage(Image srcImage) {
 	Display display = Display.getCurrent();
-	if (display == null) {
-		display = Display.getDefault();
-	}
+	if (display == null)
+		SWT.error(SWT.ERROR_THREAD_INVALID_ACCESS);
+
 	ImageData srcData = srcImage.getImageData();
-	ImageData destData = new ImageData(srcData.height, srcData.width, srcData.depth, 
-			srcData.palette); 
-	for (int y = 0; y < srcData.height; y++) {
-		for (int x = 0; x < srcData.width; x++) {
-			destData.setPixel(y, srcData.width - x - 1,	srcData.getPixel(x, y));
+	int bytesPerPixel = srcData.bytesPerLine / srcData.width;
+	int destBytesPerLine = srcData.height * bytesPerPixel;
+	byte[] newData = new byte[srcData.data.length];
+	for (int srcY = 0; srcY < srcData.height; srcY++) {
+		for (int srcX = 0; srcX < srcData.width; srcX++) {
+			int destX = srcY;
+			int destY = srcData.width - srcX - 1;
+			int destIndex = (destY * destBytesPerLine) + (destX * bytesPerPixel);
+			int srcIndex = (srcY * srcData.bytesPerLine) + (srcX * bytesPerPixel);
+			System.arraycopy(srcData.data, srcIndex, newData, destIndex, bytesPerPixel);
 		}
 	}
-
-	return new Image(display, destData);	
+	// destBytesPerLine is used as scanlinePad to ensure that no padding is required
+	return new Image(display, new ImageData(srcData.height, srcData.width, 
+					srcData.depth, srcData.palette, destBytesPerLine, newData));
 }
-	
+
 }
