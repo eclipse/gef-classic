d7bb51de24c0bd50883187f20c027b6214185de4
pshah <pshah>
2003-12-05 22:23:30 +0000

    Added the feature to toggle ruler visibility

--- org.eclipse.gef/src/org/eclipse/gef/ui/actions/GEFActionConstants.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/actions/GEFActionConstants.java
@@ -133,6 +133,20 @@ public static final String ZOOM_IN = "org.eclipse.gef.zoom_in"; //$NON-NLS-1$
  */
 public static final String ZOOM_OUT = "org.eclipse.gef.zoom_out"; //$NON-NLS-1$
 
+/** 
+ * Toggle horizontal ruler visibility contribution id<br>
+ * Value: <code>"org.eclipse.gef.rulers.horizontal.visibility"</code>
+ */
+public static final String TOGGLE_HORIZONTAL_RULER_VISIBILITY 
+		= "org.eclipse.gef.rulers.horizontal.visibility"; //$NON-NLS-1$
+
+/** 
+ * Toggle vertical ruler visibility contribution id<br>
+ * Value: <code>"org.eclipse.gef.rulers.vertical.visibility"</code>
+ */
+public static final String TOGGLE_VERTICAL_RULER_VISIBILITY 
+		= "org.eclipse.gef.rulers.vertical.visibility"; //$NON-NLS-1$
+
 /**
  * Adds standard group separators to the given MenuManager.
  * @param menu the MenuManager
