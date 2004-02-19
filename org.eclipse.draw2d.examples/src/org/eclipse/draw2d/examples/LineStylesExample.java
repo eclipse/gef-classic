7802801f90c4282144ef90a773247b99ab886f2c
msorens <msorens>
2004-02-19 20:23:55 +0000

    Checked in to test w/ buffering

--- org.eclipse.draw2d.examples/src/org/eclipse/draw2d/examples/LineStylesExample.java
+++ org.eclipse.draw2d.examples/src/org/eclipse/draw2d/examples/LineStylesExample.java
@@ -2,6 +2,7 @@ package org.eclipse.draw2d.examples;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.*;
 
 public class LineStylesExample {
@@ -12,7 +13,10 @@ protected void run(){
 	
 	shell.addListener(SWT.Paint, new Listener() {
 		public void handleEvent(Event e) {
-			GC gc = e.gc;
+			GC rgc = e.gc;
+			Image a = new Image(Display.getDefault(), 400, 400);
+			GC gc = new GC(a);
+						
 			gc.setLineStyle(SWT.LINE_DASH);
 			gc.drawLine(10, 10, 300, 10);
 			gc.setLineStyle(SWT.LINE_DOT);
@@ -21,6 +25,10 @@ protected void run(){
 			gc.drawLine(10, 30, 300, 30);
 			gc.setLineStyle(SWT.LINE_DASHDOTDOT);
 			gc.drawLine(10, 40, 300, 40);
+			
+			rgc.drawImage(a, 0, 0);
+			rgc.dispose();
+			gc.dispose();
 		}
 	});
 	
