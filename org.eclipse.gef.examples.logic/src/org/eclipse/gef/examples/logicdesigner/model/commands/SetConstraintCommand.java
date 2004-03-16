4a074c3a9f040ffc6cf666cf5638caeb572b3348
pshah <pshah>
2004-03-16 18:32:41 +0000

    Removed the functionality of changing guides into its own separate command.

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/model/commands/SetConstraintCommand.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/model/commands/SetConstraintCommand.java
@@ -13,7 +13,6 @@ package org.eclipse.gef.examples.logicdesigner.model.commands;
 import org.eclipse.draw2d.geometry.*;
 
 import org.eclipse.gef.examples.logicdesigner.LogicMessages;
-import org.eclipse.gef.examples.logicdesigner.model.LogicGuide;
 import org.eclipse.gef.examples.logicdesigner.model.LogicSubpart;
 
 public class SetConstraintCommand
@@ -24,55 +23,12 @@ private Point newPos;
 private Dimension newSize;
 private Point oldPos;
 private Dimension oldSize;
-private GuideChange hGuideChange = new GuideChange();
-private GuideChange vGuideChange = new GuideChange();
 private LogicSubpart part;
-
-protected void changeGuide(LogicGuide oldGuide, LogicGuide newGuide, int newAlignment) {
-	if (oldGuide != null && oldGuide != newGuide) {
-		oldGuide.detachPart(part);
-	}
-	// You need to re-attach the part even if the oldGuide and the newGuide are the same
-	// because the alignment could have changed
-	if (newGuide != null) {
-		newGuide.attachPart(part, newAlignment);
-	}
-}
-
-public void clearHorizontalGuide() {
-	hGuideChange.changeGuide = true;
-}
-
-public void clearVerticalGuide() {
-	vGuideChange.changeGuide = true;
-}
 
 public void execute() {
 	oldSize = part.getSize();
 	oldPos  = part.getLocation();
-	part.setLocation(newPos);
-	part.setSize(newSize);
-	
-	vGuideChange.oldGuide = part.getVerticalGuide();
-	hGuideChange.oldGuide = part.getHorizontalGuide();
-	
-	if (hGuideChange.oldGuide != null)
-		hGuideChange.oldAlign = hGuideChange.oldGuide.getAlignment(part);
-	
-	if (vGuideChange.oldGuide != null) {
-		vGuideChange.oldAlign = vGuideChange.oldGuide.getAlignment(part);
-	}
-	
-	if (vGuideChange.changeGuide) {
-		changeGuide(vGuideChange.oldGuide, vGuideChange.newGuide, vGuideChange.newAlign);
-		vGuideChange.changeGuide = false;
-	}
-	
-	if (hGuideChange.changeGuide) { 
-		changeGuide(hGuideChange.oldGuide, hGuideChange.newGuide, hGuideChange.newAlign);
-		hGuideChange.changeGuide = false;
-	}
-	
+	redo();
 }
 
 public String getLabel() {
@@ -84,14 +40,6 @@ public String getLabel() {
 public void redo() {
 	part.setSize(newSize);
 	part.setLocation(newPos);
-	changeGuide(vGuideChange.oldGuide, vGuideChange.newGuide, vGuideChange.newAlign);
-	changeGuide(hGuideChange.oldGuide, hGuideChange.newGuide, hGuideChange.newAlign);
-}
-
-public void setHorizontalGuide(LogicGuide guide, int alignment) {
-	hGuideChange.newGuide = guide;
-	hGuideChange.newAlign = alignment;
-	hGuideChange.changeGuide = true;
 }
 
 public void setLocation(Rectangle r) {
@@ -111,23 +59,9 @@ public void setSize(Dimension p) {
 	newSize = p;
 }
 
-public void setVerticalGuide(LogicGuide guide, int alignment) {
-	vGuideChange.newGuide = guide;
-	vGuideChange.newAlign = alignment;
-	vGuideChange.changeGuide = true;
-}
-
 public void undo() {
 	part.setSize(oldSize);
 	part.setLocation(oldPos);
-	changeGuide(vGuideChange.newGuide, vGuideChange.oldGuide, vGuideChange.oldAlign);
-	changeGuide(hGuideChange.newGuide, hGuideChange.oldGuide, hGuideChange.oldAlign);
-}
-
-class GuideChange {
-	protected LogicGuide oldGuide, newGuide;
-	protected int oldAlign, newAlign;
-	protected boolean changeGuide;
 }
 
 }
