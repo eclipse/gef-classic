7d6035566964296fa28753c301e5ac8783a4be92
pshah <pshah>
2004-04-15 03:47:56 +0000

    Simplified interface.  Saving state is now being done by FlyoverPaletteAutomaton

--- org.eclipse.gef/src/org/eclipse/gef/ui/palette/PaletteViewerProvider.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/palette/PaletteViewerProvider.java
@@ -10,8 +10,6 @@
  *******************************************************************************/
 package org.eclipse.gef.ui.palette;
 
-import java.util.NoSuchElementException;
-
 import org.eclipse.swt.widgets.Composite;
 
 import org.eclipse.jface.util.Assert;
@@ -24,98 +22,32 @@ import org.eclipse.gef.dnd.TemplateTransferDragSourceListener;
  */
 public class PaletteViewerProvider {
 	
-protected PaletteViewer newViewer;
-private PaletteViewer viewer;
 private EditDomain graphicalViewerEditDomain;
-private Memento state;
 
 public PaletteViewerProvider(EditDomain graphicalViewerDomain) {
 	Assert.isNotNull(graphicalViewerDomain);
 	graphicalViewerEditDomain = graphicalViewerDomain;
 }
 
-// Fails silently
-protected boolean applyState(PaletteViewer viewer) {
-	if (state == null)
-		return false;
-	try {
-		((MementoOriginator)viewer.getEditPartRegistry().get(viewer.getPaletteRoot()))
-				.setMemento(state);
-	} catch (NoSuchElementException nsee) {
-		 // thrown by iterators if hierarchies don't match
-		return false;
-	} catch (ClassCastException cce) {
-		// memento mismatch
-		return false;
-	} finally {
-		state = null;
-	}
-	return true;
+protected void configurePaletteViewer(PaletteViewer viewer) {
+	viewer.setContextMenu(new PaletteContextMenuProvider(viewer));
 }
 
-protected void configurePaletteViewer() {
-	getPaletteViewer().setContextMenu(new PaletteContextMenuProvider(getPaletteViewer()));
-}
-
-protected void captureState(PaletteViewer viewer) {
-	state = ((MementoOriginator)viewer.getEditPartRegistry().get(viewer.getPaletteRoot()))
-					.createMemento();
-	if (newViewer != null && newViewer != viewer) {
-		applyState(newViewer);
-		newViewer = null;
-	}
-}
-	
 public final PaletteViewer createPaletteViewer(Composite parent) {
 	PaletteViewer pViewer = new PaletteViewer();
-	setPaletteViewer(pViewer);
-	getPaletteViewer().createControl(parent);
-	configurePaletteViewer();
-	hookPaletteViewer();
-	initializePaletteViewer();
-	if (!applyState(getPaletteViewer()))
-		newViewer = getPaletteViewer();
-	setPaletteViewer(null);
+	pViewer.createControl(parent);
+	configurePaletteViewer(pViewer);
+	hookPaletteViewer(pViewer);
 	return pViewer;
 }
 
-public final void destroyPaletteViewer(PaletteViewer viewer) {
-	Assert.isNotNull(viewer);
-	setPaletteViewer(viewer);
-	captureState(getPaletteViewer());
-	unhookPaletteViewer();
-	if (getEditDomain().getPaletteViewer() == getPaletteViewer())
-		getEditDomain().setPaletteViewer(null);
-	if (getPaletteViewer().getControl() != null 
-			&& !getPaletteViewer().getControl().isDisposed())
-		getPaletteViewer().getControl().dispose();
-	setPaletteViewer(null);
-}
-
-protected final EditDomain getEditDomain() {
+public final EditDomain getEditDomain() {
 	return graphicalViewerEditDomain;
 }
 
-protected final PaletteViewer getPaletteViewer() {
-	return viewer;
+protected void hookPaletteViewer(PaletteViewer viewer) {
+	getEditDomain().setPaletteViewer(viewer);
+	viewer.addDragSourceListener(new TemplateTransferDragSourceListener(viewer));
 }
 
-protected void hookPaletteViewer() {
-	getEditDomain().setPaletteViewer(getPaletteViewer());
-	 // note that this listener is never removed.  however, it shouldn't be a problem.
-	 // it'll be garbage collected along with the viewer.
-	getPaletteViewer().addDragSourceListener(
-			new TemplateTransferDragSourceListener(getPaletteViewer()));
-}
-
-protected void initializePaletteViewer() {
-}
-
-protected final void setPaletteViewer(PaletteViewer pViewer) {
-	viewer = pViewer;
-}
-
-protected void unhookPaletteViewer() {
-}
-	
 }
