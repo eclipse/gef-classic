863c9e4b2eb5031cb20c3a989559aae537525145
rhudson <rhudson>
2004-05-26 20:12:49 +0000

    set tooltip and remove hover image

--- org.eclipse.gef/src/org/eclipse/gef/ui/actions/DeleteRetargetAction.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/actions/DeleteRetargetAction.java
@@ -11,9 +11,9 @@
 package org.eclipse.gef.ui.actions;
 
 import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.IWorkbenchActionConstants;
-import org.eclipse.ui.internal.WorkbenchImages;
+import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.RetargetAction;
+import org.eclipse.ui.internal.WorkbenchImages;
 
 import org.eclipse.gef.internal.GEFMessages;
 
@@ -26,9 +26,8 @@ public class DeleteRetargetAction extends RetargetAction {
  * Constructs a new DeleteRetargetAction with the default ID, label and image.
  */
 public DeleteRetargetAction() {
-	super(IWorkbenchActionConstants.DELETE, GEFMessages.DeleteAction_Label);
-	setHoverImageDescriptor(
-		WorkbenchImages.getImageDescriptor(ISharedImages.IMG_TOOL_DELETE_HOVER));
+	super(ActionFactory.DELETE.getId(), GEFMessages.DeleteAction_Label);
+	setToolTipText(GEFMessages.DeleteAction_Tooltip);
 	
 	setImageDescriptor(WorkbenchImages.getImageDescriptor(ISharedImages.IMG_TOOL_DELETE));
 	
