34820858e6afb8d556955491a318f3e3745fe4bb
pshah <pshah>
2004-06-18 15:51:32 +0000

    Javadoc

--- org.eclipse.gef/src/org/eclipse/gef/editparts/GuideLayer.java
+++ org.eclipse.gef/src/org/eclipse/gef/editparts/GuideLayer.java
@@ -18,7 +18,8 @@ import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Rectangle;
 
 /**
- * A layer where the guide lines are displayed
+ * This is a layer where the guide lines are displayed.  The figures in this layer should
+ * have a Boolean constraint indicating whether or not they are horizontal guide lines.
  * 
  * @author Pratik Shah
  * @since 3.0
@@ -31,14 +32,16 @@ private Map constraints;
 
 /**
  * @param	child	the figure whose constraint is to be found
- * @return the constraint set for the given IFigure; <code>null</code> if none exists
+ * @return the constraint (Boolean indicating whether or not it is horizontal) set for the
+ * given IFigure; <code>null</code>, if none exists
  */
 public Object getConstraint(IFigure child) {
 	return getConstraints().get(child);
 }
 
 /**
- * @return the Map of IFigures to their constraints 
+ * @return the Map of IFigures to their constraints (Booleans indicating whether or not
+ * they are horizontal guide lines)
  */
 public Map getConstraints() {
 	if (constraints == null) {
@@ -52,8 +55,15 @@ public Map getConstraints() {
  */
 public Rectangle getFreeformExtent() {
 	/*
-	 * These ints are initialized to 5 so that when the final extent is expanded by 5,
-	 * the bounds do not go into the negative (unless necessary).
+	 * The freeform extents are just big enough to include all the children: tall enough 
+	 * to include the highest and the lowest horizontal guide lines, and wide enough
+	 * to include the leftmost and the rightmost vertical guide lines.  They always
+	 * include the point 5,5 and are further expanded by 5-pixels in all directions so
+	 * that none of the guide lines are on the edge.
+	 */
+	/*
+	 * These ints are initialized to 5, and not 0, so that when the final extent is
+	 * expanded by 5, the bounds do not go into the negative (unless necessary).
 	 */
 	int maxX = 5, minX = 5, maxY = 5, minY = 5;
 	Iterator children = getChildren().iterator();
@@ -74,12 +84,10 @@ public Rectangle getFreeformExtent() {
 	}
 	Rectangle r = new Rectangle(
 			minX, minY, maxX - minX + 1, maxY - minY + 1);
-	if (r.width > 1) {
+	if (r.width > 1)
 		r.expand(5, 0);
-	}
-	if (r.height > 1) {
+	if (r.height > 1)
 		r.expand(0, 5);
-	}
 	return r;
 }
 
@@ -100,6 +108,8 @@ public void remove(IFigure child) {
 }
 
 /**
+ * The constraint is expected to be a Boolean indicating whether the given guide
+ * line figure is horizontal or not.
  * @see org.eclipse.draw2d.IFigure#setConstraint(org.eclipse.draw2d.IFigure, java.lang.Object)
  */
 public void setConstraint(IFigure child, Object constraint) {
