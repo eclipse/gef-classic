21776d82f362db314ae2429f301156ba6a36ba78
pshah <pshah>
2004-06-21 23:19:24 +0000

    Copyright Statement

--- org.eclipse.gef/src/org/eclipse/gef/Tool.java
+++ org.eclipse.gef/src/org/eclipse/gef/Tool.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,9 +11,7 @@
 package org.eclipse.gef;
 
 import org.eclipse.swt.dnd.DragSourceEvent;
-import org.eclipse.swt.events.FocusEvent;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.*;
 
 /**
  * A <code>Tool</code> interprets Mouse and Keyboard input from an {@link EditDomain} and
