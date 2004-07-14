f55e3ebb276701fd2a96e1cb78e36429eac38b83
pshah <pshah>
2004-07-14 15:56:18 +0000

    Fix for Bug# 69098 - The last fix was not correct; now I try to locate the view in the part's page

--- org.eclipse.gef/src/org/eclipse/gef/ui/views/palette/PaletteView.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/views/palette/PaletteView.java
@@ -95,7 +95,7 @@ protected boolean isImportant(IWorkbenchPart part) {
 	// Fix for Bug# 69098
 	// We only want a palette page to be created when this view is visible in the current
 	// perspective, i.e., when both this view and the given editor are on the same page.
-	return getSite().getPage() == part.getSite().getPage() 
+	return part.getSite().getPage().findViewReference(ID) != null 
 			&& part instanceof IEditorPart;
 }
 
