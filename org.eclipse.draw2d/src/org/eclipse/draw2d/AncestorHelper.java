85db54e7f8f476b23e3f9000a3e4848fb39e6020
rhudson <rhudson>
2004-08-11 21:32:53 +0000

    doc and cleanup

--- org.eclipse.draw2d/src/org/eclipse/draw2d/AncestorHelper.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/AncestorHelper.java
@@ -15,59 +15,108 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A helper object which tracks the parent chain hierarchy.
+ * since 3.0
+ */
 class AncestorHelper
 	implements PropertyChangeListener, FigureListener 
 {
 
-protected final IFigure root;
+/**
+ * The base figure whose ancestor chain is being observed.
+ */
+protected final IFigure base;
+/**
+ * The list of ancestor listeners.
+ */
 protected List listeners = null;
 
-public AncestorHelper(IFigure root) {
-	this.root = root;
-	addAncestors(root);
+/**
+ * Constructs a new helper on the given base figure and starts listening to figure and
+ * property changes on the base figure's parent chain.  When no longer needed, the helper
+ * should be disposed.
+ * @since 2.1
+ * @param baseFigure
+ */
+public AncestorHelper(IFigure baseFigure) {
+	this.base = baseFigure;
+	addAncestors(baseFigure);
 }
 
+/**
+ * Appends a new listener to the list of listeners.
+ * @since 2.1
+ * @param listener the listener
+ */
 public void addAncestorListener(AncestorListener listener) {
 	if (listeners == null) 
 		listeners = new ArrayList();
 	listeners.add(listener);
 }
 
+/**
+ * Hooks up internal listeners used for maintaining the proper figure listeners.
+ * @since 2.1
+ * @param rootFigure the root figure
+ */
 protected void addAncestors(IFigure rootFigure) {
-	for (IFigure ancestor = rootFigure; 
-				ancestor != null; 
-				ancestor = ancestor.getParent()) {
+	for (IFigure ancestor = rootFigure;
+			ancestor != null;
+			ancestor = ancestor.getParent()) {
 		ancestor.addFigureListener(this);
-		ancestor.addPropertyChangeListener(this);
+		ancestor.addPropertyChangeListener("parent", this);
 	}
 }
 
+/**
+ * Removes all internal listeners.
+ * @since 2.1
+ */
 public void dispose() {
-	removeAncestors(root);
-	for (int i = 0; i < listeners.size(); i++)
-		removeAncestorListener(((AncestorListener)listeners.get(i)));
+	removeAncestors(base);
+	listeners.clear();
 }
 
 public void figureMoved(IFigure ancestor) {
 	fireAncestorMoved(ancestor);
 }
 
+/**
+ * Fires notification to the listener list 
+ * @since 2.1
+ * @param ancestor the figure which moved
+ */
 protected void fireAncestorMoved(IFigure ancestor) {
-//	if(root!=ancestor)((Figure)root).fireMoved();
 	for (int i = 0; i < listeners.size(); i++)
 		((AncestorListener)listeners.get(i)).ancestorMoved(ancestor);
 }
 
+/**
+ * Fires notification to the listener list 
+ * @since 2.1
+ * @param ancestor the figure which moved
+ */
 protected void fireAncestorAdded(IFigure ancestor) {
 	for (int i = 0; i < listeners.size(); i++)
 		((AncestorListener)listeners.get(i)).ancestorAdded(ancestor);
 }
 
+/**
+ * Fires notification to the listener list 
+ * @since 2.1
+ * @param ancestor the figure which moved
+ */
 protected void fireAncestorRemoved(IFigure ancestor) {
 	for (int i = 0; i < listeners.size(); i++)
 		((AncestorListener)listeners.get(i)).ancestorRemoved(ancestor);
 }
 
+/**
+ * Returns the total number of listeners.
+ * @since 2.1
+ * @return the number of listeners
+ */
 public int getNumberOfListeners() {
 	return listeners.size();
 }
@@ -87,16 +136,26 @@ public void propertyChange(PropertyChangeEvent event) {
 	}
 }
 
+/**
+ * Removes the first occurence of the given listener
+ * @since 2.1
+ * @param listener the listener to remove
+ */
 public void removeAncestorListener(AncestorListener listener) {
 	listeners.remove(listener);
 }
 
+/**
+ * Unhooks listeners starting at the given figure
+ * @since 2.1
+ * @param rootFigure
+ */
 protected void removeAncestors(IFigure rootFigure) {
 	for (IFigure ancestor = rootFigure; 
 				ancestor != null; 
 				ancestor = ancestor.getParent()) {
 		ancestor.removeFigureListener(this);
-		ancestor.removePropertyChangeListener(this);
+		ancestor.removePropertyChangeListener("parent", this); //$NON-NLS-1$
 	}
 }
 
