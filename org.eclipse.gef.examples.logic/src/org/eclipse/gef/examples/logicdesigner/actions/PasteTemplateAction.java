906352d8fb1c98c63b43a24e98d2336f9a480821
pshah <pshah>
2005-01-11 21:17:12 +0000

    Fix for Bug# 82603 - createPasteCommand() returns null if the getFactory() returns null; Fix for Bug# 71395 - getClipboardContents() returns the template only if the TemplateTransfer is still on the system clipboard; JavaDoc and some other code clean-up; Changed ID from GEFActionConstants.PASTE to ActionFactory.PASTE.getId()

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/actions/PasteTemplateAction.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/actions/PasteTemplateAction.java
@@ -10,27 +10,36 @@
  *******************************************************************************/
 package org.eclipse.gef.examples.logicdesigner.actions;
 
+import java.util.List;
+
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TransferData;
+import org.eclipse.swt.widgets.Display;
+
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.actions.ActionFactory;
 
 import org.eclipse.draw2d.geometry.Point;
 
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.dnd.TemplateTransfer;
 import org.eclipse.gef.requests.CreateRequest;
 import org.eclipse.gef.requests.CreationFactory;
-import org.eclipse.gef.ui.actions.Clipboard;
-import org.eclipse.gef.ui.actions.GEFActionConstants;
 import org.eclipse.gef.ui.actions.SelectionAction;
 
 import org.eclipse.gef.examples.logicdesigner.LogicMessages;
 
 /**
- * If the current object on the default GEF {@link org.eclipse.gef.ui.actions.Clipboard}
- * is a template and the current viewer is a graphical viewer, this action will paste the
- * template to the viewer.
- * @author Eric Bordeau
+ * If the current object on the clipboard is a valid template, this action will paste 
+ * the template to the viewer.
+ * 
+ * @author Eric Bordeau, Pratik Shah
+ * @see org.eclipse.gef.ui.actions.CopyTemplateAction
  */
-public abstract class PasteTemplateAction extends SelectionAction {
+public abstract class PasteTemplateAction 
+	extends SelectionAction 
+{
 
 /**
  * Constructor for PasteTemplateAction.
@@ -41,14 +50,11 @@ public PasteTemplateAction(IWorkbenchPart editor) {
 }
 
 /**
- * Returns <code>true</code> if the {@link Clipboard clipboard's} contents are not
- * <code>null</code> and the command returned by {@link #createPasteCommand()} can
- * execute.
+ * @return <code>true</code> if {@link #createPasteCommand()} returns an executable command
+ * @see org.eclipse.gef.ui.actions.WorkbenchPartAction#calculateEnabled()
  */
 protected boolean calculateEnabled() {
-	Command command = null;
-	if (getClipboardContents() != null)
-		command = createPasteCommand();
+	Command command = createPasteCommand();
 	return command != null && command.canExecute();
 }
 
@@ -58,40 +64,63 @@ protected boolean calculateEnabled() {
  * @return the paste command
  */
 protected Command createPasteCommand() {
-	if (getSelectedObjects() == null || getSelectedObjects().isEmpty())
+	List selection = getSelectedObjects();
+	if (selection == null || selection.size() != 1)
+		return null;
+	Object template = getClipboardContents();
+	if (template  == null)
 		return null;
 	CreateRequest request = new CreateRequest();
-	request.setFactory(getFactory(getClipboardContents()));
+	CreationFactory factory = getFactory(template);
+	if (factory == null)
+		return null;
+	request.setFactory(factory);
 	request.setLocation(getPasteLocation());
-	Object obj = getSelectedObjects().get(0);
+	Object obj = selection.get(0);
 	if (obj instanceof EditPart)
 		return ((EditPart)obj).getCommand(request);
 	return null;
 }
 
 /**
- * Returns the contents of the default GEF {@link Clipboard}.
+ * Returns the template on the clipboard, if there is one.  Note that the template on
+ * the clipboard might be from a palette from another type of editor.
  * @return the clipboard's contents
  */
 protected Object getClipboardContents() {
-	return Clipboard.getDefault().getContents();
+	Object result = null;
+	Clipboard cb = new Clipboard(Display.getDefault());
+	TransferData[] transferTypes = cb.getAvailableTypes();
+	for (int i = 0; i < transferTypes.length; i++) {
+		if (TemplateTransfer.getInstance().isSupportedType(transferTypes[i])) {
+			result = org.eclipse.gef.ui.actions.Clipboard.getDefault().getContents();
+			break;
+		}
+	}
+	cb.dispose();
+	return result;
 }
 
 /**
  * Returns the appropriate Factory object to be used for the specified template. This
- * Factory is used on the CreateRequest that is sent to the target EditPart.
- * @param template the template Object
+ * Factory is used on the CreateRequest that is sent to the target EditPart.  Note that
+ * the given template might be from a palette for a different GEF-based editor.  In that
+ * case, this method can return <code>null</code>.
+ * @param template the template Object; it will never be <code>null</code>
  * @return a Factory
  */
 protected abstract CreationFactory getFactory(Object template);
 
+/**
+ * @return the location at which  
+ */
 protected abstract Point getPasteLocation();
 
 /**
  * @see org.eclipse.gef.ui.actions.EditorPartAction#init()
  */
 protected void init() {
-	setId(GEFActionConstants.PASTE);
+	setId(ActionFactory.PASTE.getId());
 	setText(LogicMessages.PasteAction_ActionLabelText);
 }
 
