f43b2181df28d439390525646cc203116b0c6437
rhudson <rhudson>
2005-02-21 18:29:59 +0000

    updated for new/deleted test cases

--- org.eclipse.draw2d.test/src/org/eclipse/draw2d/test/Draw2dTestSuite.java
+++ org.eclipse.draw2d.test/src/org/eclipse/draw2d/test/Draw2dTestSuite.java
@@ -31,7 +31,8 @@ public Draw2dTestSuite() {
 	addTest(new TestSuite(XYLayoutTest.class));
 	addTest(new TestSuite(TextFlowWrapTest.class));
 	addTest(new TestSuite(LocalOptimizerTest.class));
-	addTest(new TestSuite(ScaledGraphicsTest.class));
+//	addTest(new TestSuite(CaretTests.class));
+	addTest(new TestSuite(FlowBorderTests.class));
 	addTest(new TestSuite(PaintDamageEraseTest.class));
 	addTest(new TestSuite(LayeredPaneTest.class));
 	addTest(new TestSuite(ConnectionEndPointMoveTest.class));
