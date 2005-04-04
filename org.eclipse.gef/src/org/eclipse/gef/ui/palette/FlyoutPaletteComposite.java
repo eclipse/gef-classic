46e6ada03be6f287afbfde79e25bc9d9f3fd9c6d
pshah <pshah>
2005-04-04 19:11:46 +0000

    fix for bug# 83975 - removed usage of DragCursors

--- org.eclipse.gef/src/org/eclipse/gef/ui/palette/FlyoutPaletteComposite.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/palette/FlyoutPaletteComposite.java
@@ -67,7 +67,6 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.XMLMemento;
-import org.eclipse.ui.internal.DragCursors;
 
 import org.eclipse.draw2d.ActionEvent;
 import org.eclipse.draw2d.ActionListener;
@@ -75,6 +74,7 @@ import org.eclipse.draw2d.Border;
 import org.eclipse.draw2d.Button;
 import org.eclipse.draw2d.ButtonBorder;
 import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Cursors;
 import org.eclipse.draw2d.FocusEvent;
 import org.eclipse.draw2d.FocusListener;
 import org.eclipse.draw2d.Graphics;
@@ -139,6 +139,9 @@ private static final Image LEFT_ARROW = new Image(null, ImageDescriptor.createFr
 		Internal.class, "icons/palette_left.gif").getImageData()); //$NON-NLS-1$
 private static final Image RIGHT_ARROW = new Image(null, ImageDescriptor.createFromFile(
 		Internal.class, "icons/palette_right.gif").getImageData()); //$NON-NLS-1$
+
+private static Cursor DRAG_LEFT;
+private static Cursor DRAG_RIGHT;
 
 private PropertyChangeSupport listeners = new PropertyChangeSupport(this);
 private Composite paletteContainer;
@@ -900,12 +903,29 @@ private class TitleDragManager
 						// update the cursor
 						Cursor cursor;
 						if (invalid)
-							cursor = DragCursors.getCursor(DragCursors.INVALID);
+							cursor = Cursors.NO;
 						else if ((!switchDock && dock == PositionConstants.EAST)
-								|| (switchDock && dock == PositionConstants.WEST))
-							cursor = DragCursors.getCursor(DragCursors.RIGHT);
-						else
-							cursor = DragCursors.getCursor(DragCursors.LEFT);
+								|| (switchDock && dock == PositionConstants.WEST)) {
+							if (DRAG_RIGHT == null) {
+								ImageDescriptor src = ImageDescriptor.createFromFile(
+										Internal.class, "icons/right_source.bmp"); //$NON-NLS-1$
+								ImageDescriptor mask = ImageDescriptor.createFromFile(
+										Internal.class, "icons/right_mask.bmp"); //$NON-NLS-1$
+								DRAG_RIGHT = new Cursor(null, src.getImageData(),
+										mask.getImageData(), 16, 16);
+							}
+							cursor = DRAG_RIGHT;
+						} else {
+							if (DRAG_LEFT == null) {
+								ImageDescriptor src = ImageDescriptor.createFromFile(
+										Internal.class, "icons/left_source.bmp"); //$NON-NLS-1$
+								ImageDescriptor mask = ImageDescriptor.createFromFile(
+										Internal.class, "icons/left_mask.bmp"); //$NON-NLS-1$
+								DRAG_LEFT = new Cursor(null, src.getImageData(),
+										mask.getImageData(), 16, 16);
+							}
+							cursor = DRAG_LEFT;
+						}
 						tracker.setCursor(cursor);
 						// update the rectangle only if it has changed
 						if (!tracker.getRectangles()[0].equals(placeHolder))
