84629a32410936f178215a80d0f06285fa65cb16
pshah <pshah>
2005-04-05 21:30:41 +0000

    fixed a compile error

--- org.eclipse.draw2d.test/src/org/eclipse/draw2d/test/PointListTests.java
+++ org.eclipse.draw2d.test/src/org/eclipse/draw2d/test/PointListTests.java
@@ -23,37 +23,37 @@ public void testIntersects() {
 	points.addPoint(0, 0);
 	points.addPoint(0, 3);
 	// simple case
-	assertTrue(points.intersects(Rectangle.SINGLETON.setBounds(-10, 0, 20, 3)));
+	assertTrue(points.intersects(new Rectangle(-10, 0, 20, 3)));
 	// line starts and ends on opposite edges of the rectangle
-	assertTrue(points.intersects(Rectangle.SINGLETON.setBounds(-2, 1, 4, 1)));
+	assertTrue(points.intersects(new Rectangle(-2, 1, 4, 1)));
 	// parallel
-	assertTrue(points.intersects(Rectangle.SINGLETON.setBounds(0, 0, 10, 10)));
+	assertTrue(points.intersects(new Rectangle(0, 0, 10, 10)));
 	// vertical
-	assertTrue(points.intersects(Rectangle.SINGLETON.setBounds(0, 3, 10, 10)));
+	assertTrue(points.intersects(new Rectangle(0, 3, 10, 10)));
 	// rect is close, but not on the line -- off by a pixel
-	assertFalse(points.intersects(Rectangle.SINGLETON.setBounds(1, 1, 50, 50)));
+	assertFalse(points.intersects(new Rectangle(1, 1, 50, 50)));
 	// empty rect located on the line
-	assertFalse(points.intersects(Rectangle.SINGLETON.setBounds(0, 1, 0, 0)));
+	assertFalse(points.intersects(new Rectangle(0, 1, 0, 0)));
 	
 	points.addPoint(10, 10);
 	// line cuts through two adjacent sides of the rectangle
-	assertTrue(points.intersects(Rectangle.SINGLETON.setBounds(-50, 5, 58, 100)));
+	assertTrue(points.intersects(new Rectangle(-50, 5, 58, 100)));
 	points.removePoint(2);
 	
 	points.addPoint(-1, -3);
 	// line doesn't intersect with the rect's diagonals
-	assertTrue(points.intersects(Rectangle.SINGLETON.setBounds(-10, 2, 40, 50)));
+	assertTrue(points.intersects(new Rectangle(-10, 2, 40, 50)));
 	points.removeAllPoints();
 	
 	points.addPoint(10, 10);
 	points.addPoint(8, 12);
 	// line starts on rectangle's bottom-right corner
-	assertTrue(points.intersects(Rectangle.SINGLETON.setBounds(0, 0, 10, 10)));
+	assertTrue(points.intersects(new Rectangle(0, 0, 10, 10)));
 	// line is tangential to the rectangle's bottom-right corner
-	assertTrue(points.intersects(Rectangle.SINGLETON.setBounds(0, 0, 9, 11)));
+	assertTrue(points.intersects(new Rectangle(0, 0, 9, 11)));
 	
 	points.addPoint(new PrecisionPoint(1.51, 1.51));
-	assertTrue(points.intersects(Rectangle.SINGLETON.setBounds(0, 0, 1, 1)));
+	assertTrue(points.intersects(new Rectangle(0, 0, 1, 1)));
 }
 
 }
