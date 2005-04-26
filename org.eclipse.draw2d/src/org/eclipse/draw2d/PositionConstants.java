ee6a26ddb2773c93c7f8c63c243a6010193e91b3
pshah <pshah>
2005-04-26 03:21:56 +0000

    Added ALWAYS_LEFT and ALWAYS_RIGHT

--- org.eclipse.draw2d/src/org/eclipse/draw2d/PositionConstants.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/PositionConstants.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2000, 2005 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
+ * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
@@ -46,6 +46,8 @@ int CENTER =  2;
 int RIGHT  =  4;
 /** Bit-wise OR of LEFT, CENTER, and RIGHT */
 int LEFT_CENTER_RIGHT = LEFT | CENTER | RIGHT;
+int ALWAYS_LEFT = 64;
+int ALWAYS_RIGHT = 128;
 
 /** Top */
 int TOP    =  8;
