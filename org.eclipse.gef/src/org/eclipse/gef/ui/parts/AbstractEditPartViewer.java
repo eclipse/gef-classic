3377827ac469f45767c6b84841ed2ae0ebcb4e56
rhudson <rhudson>
2005-04-29 17:57:23 +0000

    Reverting - HashSet does not preserve order

--- org.eclipse.gef/src/org/eclipse/gef/ui/parts/AbstractEditPartViewer.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/parts/AbstractEditPartViewer.java
@@ -16,8 +16,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -716,26 +714,21 @@ public void setSelection(ISelection newSelection) {
 	if (!(newSelection instanceof IStructuredSelection))
 		return;
 	
-	// Convert to HashSet to optimize performance.  Has the added benefit of removing
-	// multiple copies from the list.
-	Collection editparts = new HashSet(((IStructuredSelection)newSelection).toList());
+	List editparts = ((IStructuredSelection)newSelection).toList();
 	List selection = primGetSelectedEditParts();
 
 	setFocus(null);
-	for (int i = 0; i < selection.size(); i++) {
-		EditPart part = (EditPart)selection.get(i);
-		if (!editparts.contains(part))
-			part.setSelected(EditPart.SELECTED_NONE);
-	}
+	for (int i = 0; i < selection.size(); i++)
+		((EditPart)selection.get(i)).setSelected(EditPart.SELECTED_NONE);
 	selection.clear();
 
-	for (Iterator itr = editparts.iterator(); itr.hasNext();) {
-		EditPart part = (EditPart)itr.next();
+	for (int i = 0; i < editparts.size(); i++) {
+		EditPart part = (EditPart)editparts.get(i);
 		selection.add(part);
-		if (itr.hasNext())
-			part.setSelected(EditPart.SELECTED);
-		else
+		if (i == editparts.size() - 1)
 			part.setSelected(EditPart.SELECTED_PRIMARY);
+		else
+			part.setSelected(EditPart.SELECTED);
 	}
 	fireSelectionChanged();
 }
