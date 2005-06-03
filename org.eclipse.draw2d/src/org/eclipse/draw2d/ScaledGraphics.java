b287544a4d9f9c5a4411f6024059bf556a67632c
rhudson <rhudson>
2005-06-03 20:15:06 +0000

    Delegate new graphics functions to wrappered graphics

--- org.eclipse.draw2d/src/org/eclipse/draw2d/ScaledGraphics.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/ScaledGraphics.java
@@ -36,15 +36,44 @@ public class ScaledGraphics
 	extends Graphics
 {
 
+private static class FontHeightCache {
+	Font font;
+	int height;
+}
+
+static class FontKey {
+	Font font;
+	int height;
+	protected FontKey() { }
+	protected FontKey(Font font, int height) {
+		this.font = font;
+		this.height = height;
+	}
+	
+	public boolean equals(Object obj) {
+		return (((FontKey)obj).font.equals(font) 
+				&& ((FontKey)obj).height == height);
+	}
+
+	public int hashCode() {
+		return font.hashCode() ^ height;
+	}
+
+	protected void setValues(Font font, int height) {
+		this.font = font;
+		this.height = height;
+	}
+}
+
 /**
  * The internal state of the scaled graphics.
  */
 protected static class State {
-	private double zoom;
 	private double appliedX;
 	private double appliedY;
 	private Font font;
-	private int lineWidth; 
+	private int lineWidth;
+	private double zoom; 
 
 	/**
 	 * Constructs a new, uninitialized State object.
@@ -87,58 +116,30 @@ protected static class State {
 	}
 }
 
-static class FontKey {
-	Font font;
-	int height;
-	protected FontKey() { }
-	protected FontKey(Font font, int height) {
-		this.font = font;
-		this.height = height;
-	}
-	
-	public boolean equals(Object obj) {
-		return (((FontKey)obj).font.equals(font) 
-				&& ((FontKey)obj).height == height);
-	}
-
-	public int hashCode() {
-		return font.hashCode() ^ height;
-	}
-
-	protected void setValues(Font font, int height) {
-		this.font = font;
-		this.height = height;
-	}
-}
-
-private static class FontHeightCache {
-	Font font;
-	int height;
-}
-
-private static final Rectangle TEMP = new Rectangle();
-//private static final Point PT = new Point();
-private Map fontCache = new HashMap();
-private Map fontDataCache = new HashMap();
-
-private FontHeightCache localCache = new FontHeightCache();
-private FontHeightCache targetCache = new FontHeightCache();
-private Graphics graphics;
-private Font localFont;
-private int localLineWidth;
-private double fractionalX;
-private double fractionalY;
-double zoom = 1.0;
-private List stack = new ArrayList();
-private int stackPointer = 0;
-private FontKey fontKey = new FontKey();
-private boolean allowText = true;
 private static int[][] intArrayCache = new int[8][];
+private static final Rectangle TEMP = new Rectangle();
 
 static {
 	for (int i = 0; i < intArrayCache.length; i++)
 		intArrayCache[i] = new int[i + 1];
 }
+
+private boolean allowText = true;
+//private static final Point PT = new Point();
+private Map fontCache = new HashMap();
+private Map fontDataCache = new HashMap();
+private FontKey fontKey = new FontKey();
+private double fractionalX;
+private double fractionalY;
+private Graphics graphics;
+private FontHeightCache localCache = new FontHeightCache();
+private Font localFont;
+private int localLineWidth;
+private List stack = new ArrayList();
+private int stackPointer = 0;
+private FontHeightCache targetCache = new FontHeightCache();
+
+double zoom = 1.0;
 
 /**
  * Constructs a new ScaledGraphics based on the given Graphics object.
@@ -153,6 +154,10 @@ public ScaledGraphics(Graphics g) {
 /** @see Graphics#clipRect(Rectangle) */
 public void clipRect(Rectangle r) {
 	graphics.clipRect(zoomClipRect(r));
+}
+
+Font createFont(FontData data) {
+	return new Font(Display.getCurrent(), data);
 }
 
 /** @see Graphics#dispose() */
@@ -177,19 +182,6 @@ public void drawArc(int x, int y, int w, int h, int offset, int sweep) {
 	if (z.isEmpty() || sweep == 0)
 		return;
 	graphics.drawArc(z, offset, sweep);
-}
-
-/** @see Graphics#fillArc(int, int, int, int, int, int) */
-public void fillArc(int x, int y, int w, int h, int offset, int sweep) {
-	Rectangle z = zoomFillRect(x, y, w, h);
-	if (z.isEmpty() || sweep == 0)
-		return;
-	graphics.fillArc(z, offset, sweep);
-}
-
-/** @see Graphics#fillGradient(int, int, int, int, boolean) */
-public void fillGradient(int x, int y, int w, int h, boolean vertical) {
-	graphics.fillGradient(zoomFillRect(x, y, w, h), vertical);
 }
 
 /** @see Graphics#drawFocus(int, int, int, int) */
@@ -231,9 +223,10 @@ public void drawOval(int x, int y, int w, int h) {
 	graphics.drawOval(zoomRect(x, y, w, h));
 }
 
-/** @see Graphics#fillOval(int, int, int, int) */
-public void fillOval(int x, int y, int w, int h) {
-	graphics.fillOval(zoomFillRect(x, y, w, h));
+/** @see Graphics#drawPoint(int, int) */
+public void drawPoint(int x, int y) {
+	graphics.drawPoint((int)Math.floor(x * zoom + fractionalX),
+			(int)Math.floor(y * zoom + fractionalY));
 }
 
 /**
@@ -246,24 +239,6 @@ public void drawPolygon(int[] points) {
 /** @see Graphics#drawPolygon(PointList) */
 public void drawPolygon(PointList points) {
 	graphics.drawPolygon(zoomPointList(points.toIntArray()));
-}
-
-/** @see Graphics#drawPoint(int, int) */
-public void drawPoint(int x, int y) {
-	graphics.drawPoint((int)Math.floor(x * zoom + fractionalX),
-			(int)Math.floor(y * zoom + fractionalY));
-}
-
-/**
- * @see Graphics#fillPolygon(int[])
- */
-public void fillPolygon(int[] points) {
-	graphics.fillPolygon(zoomPointList(points));
-}
-
-/** @see Graphics#fillPolygon(PointList) */
-public void fillPolygon(PointList points) {
-	graphics.fillPolygon(zoomPointList(points.toIntArray()));
 }
 
 /**
@@ -283,21 +258,9 @@ public void drawRectangle(int x, int y, int w, int h) {
 	graphics.drawRectangle(zoomRect(x, y, w, h));
 }
 
-/** @see Graphics#fillRectangle(int, int, int, int) */
-public void fillRectangle(int x, int y, int w, int h) {
-	graphics.fillRectangle(zoomFillRect(x, y, w, h));
-}
-
 /** @see Graphics#drawRoundRectangle(Rectangle, int, int) */
 public void drawRoundRectangle(Rectangle r, int arcWidth, int arcHeight) {
 	graphics.drawRoundRectangle(zoomRect(r.x, r.y, r.width, r.height),
-		(int)(arcWidth * zoom),
-		(int)(arcHeight * zoom));
-}
-
-/** @see Graphics#fillRoundRectangle(Rectangle, int, int) */
-public void fillRoundRectangle(Rectangle r, int arcWidth, int arcHeight) {
-	graphics.fillRoundRectangle(zoomFillRect(r.x, r.y, r.width, r.height),
 		(int)(arcWidth * zoom),
 		(int)(arcHeight * zoom));
 }
@@ -308,12 +271,6 @@ public void drawString(String s, int x, int y) {
 		graphics.drawString(s, zoomTextPoint(x, y));
 }
 
-/** @see Graphics#fillString(String, int, int) */
-public void fillString(String s, int x, int y) {
-	if (allowText)
-		graphics.fillString(s, zoomTextPoint(x, y));
-}
-
 /** @see Graphics#drawText(String, int, int) */
 public void drawText(String s, int x, int y) {
 	if (allowText)
@@ -321,7 +278,7 @@ public void drawText(String s, int x, int y) {
 }
 
 /**
- * @see org.eclipse.draw2d.Graphics#drawText(java.lang.String, int, int, int)
+ * @see Graphics#drawText(String, int, int, int)
  */
 public void drawText(String s, int x, int y, int style) {
 	if (allowText)
@@ -341,15 +298,105 @@ public void drawTextLayout(TextLayout layout, int x, int y, int selectionStart,
 	scaled.dispose();
 }
 
+/** @see Graphics#fillArc(int, int, int, int, int, int) */
+public void fillArc(int x, int y, int w, int h, int offset, int sweep) {
+	Rectangle z = zoomFillRect(x, y, w, h);
+	if (z.isEmpty() || sweep == 0)
+		return;
+	graphics.fillArc(z, offset, sweep);
+}
+
+/** @see Graphics#fillGradient(int, int, int, int, boolean) */
+public void fillGradient(int x, int y, int w, int h, boolean vertical) {
+	graphics.fillGradient(zoomFillRect(x, y, w, h), vertical);
+}
+
+/** @see Graphics#fillOval(int, int, int, int) */
+public void fillOval(int x, int y, int w, int h) {
+	graphics.fillOval(zoomFillRect(x, y, w, h));
+}
+
+/**
+ * @see Graphics#fillPolygon(int[])
+ */
+public void fillPolygon(int[] points) {
+	graphics.fillPolygon(zoomPointList(points));
+}
+
+/** @see Graphics#fillPolygon(PointList) */
+public void fillPolygon(PointList points) {
+	graphics.fillPolygon(zoomPointList(points.toIntArray()));
+}
+
+/** @see Graphics#fillRectangle(int, int, int, int) */
+public void fillRectangle(int x, int y, int w, int h) {
+	graphics.fillRectangle(zoomFillRect(x, y, w, h));
+}
+
+/** @see Graphics#fillRoundRectangle(Rectangle, int, int) */
+public void fillRoundRectangle(Rectangle r, int arcWidth, int arcHeight) {
+	graphics.fillRoundRectangle(zoomFillRect(r.x, r.y, r.width, r.height),
+		(int)(arcWidth * zoom),
+		(int)(arcHeight * zoom));
+}
+
+/** @see Graphics#fillString(String, int, int) */
+public void fillString(String s, int x, int y) {
+	if (allowText)
+		graphics.fillString(s, zoomTextPoint(x, y));
+}
+
 /** @see Graphics#fillText(String, int, int) */
 public void fillText(String s, int x, int y) {
 	if (allowText)
 		graphics.fillText(s, zoomTextPoint(x, y));
 }
 
+/**
+ * @see Graphics#getAbsoluteScale()
+ */
+public double getAbsoluteScale() {
+	return zoom * graphics.getAbsoluteScale();
+}
+
+/**
+ * @see Graphics#getAlpha()
+ */
+public int getAlpha() {
+	return graphics.getAlpha();
+}
+
+/**
+ * @see Graphics#getAntialias()
+ */
+public int getAntialias() {
+	return graphics.getAntialias();
+}
+
 /** @see Graphics#getBackgroundColor() */
 public Color getBackgroundColor() {
 	return graphics.getBackgroundColor();
+}
+
+Font getCachedFont(FontKey key) {
+	Font font = (Font)fontCache.get(key);
+	if (font != null)
+		return font;
+	key = new FontKey(key.font, key.height);
+	FontData data = key.font.getFontData()[0];
+	data.setHeight(key.height);
+	Font zoomedFont = createFont(data);
+	fontCache.put(key, zoomedFont);
+	return zoomedFont;
+}
+
+FontData getCachedFontData(Font f) {
+	FontData data = (FontData)fontDataCache.get(f);
+	if (data != null)
+		return data;
+	data = getLocalFont().getFontData()[0];
+	fontDataCache.put(f, data);
+	return data;
 }
 
 /** @see Graphics#getClip(Rectangle) */
@@ -370,6 +417,13 @@ public Rectangle getClip(Rectangle rect) {
 	return rect;
 }
 
+/**
+ * @see Graphics#getFillRule()
+ */
+public int getFillRule() {
+	return graphics.getFillRule();
+}
+
 /** @see Graphics#getFont() */
 public Font getFont() {
 	return getLocalFont();
@@ -383,6 +437,27 @@ public FontMetrics getFontMetrics() {
 /** @see Graphics#getForegroundColor() */
 public Color getForegroundColor() {
 	return graphics.getForegroundColor();
+}
+
+/**
+ * @see Graphics#getInterpolation()
+ */
+public int getInterpolation() {
+	return graphics.getInterpolation();
+}
+
+/**
+ * @see Graphics#getLineCap()
+ */
+public int getLineCap() {
+	return graphics.getLineCap();
+}
+
+/**
+ * @see Graphics#getLineJoin()
+ */
+public int getLineJoin() {
+	return graphics.getLineJoin();
 }
 
 /** @see Graphics#getLineStyle() */
@@ -404,10 +479,10 @@ private int getLocalLineWidth() {
 }
 
 /**
- * @see org.eclipse.draw2d.Graphics#getAbsoluteScale()
+ * @see Graphics#getTextAntialias()
  */
-public double getAbsoluteScale() {
-	return zoom * graphics.getAbsoluteScale();
+public int getTextAntialias() {
+	return graphics.getTextAntialias();
 }
 
 /** @see Graphics#getXORMode() */
@@ -456,37 +531,18 @@ public void scale(double amount) {
 	setScale(zoom * amount);
 }
 
-void setScale(double value) {
-	if (zoom == value)
-		return;
-	this.zoom = value;
-	graphics.setFont(zoomFont(getLocalFont()));
-	graphics.setLineWidth(zoomLineWidth(localLineWidth));
+/**
+ * @see Graphics#setAlpha(int)
+ */
+public void setAlpha(int alpha) {
+	graphics.setAlpha(alpha);
 }
 
-Font getCachedFont(FontKey key) {
-	Font font = (Font)fontCache.get(key);
-	if (font != null)
-		return font;
-	key = new FontKey(key.font, key.height);
-	FontData data = key.font.getFontData()[0];
-	data.setHeight(key.height);
-	Font zoomedFont = createFont(data);
-	fontCache.put(key, zoomedFont);
-	return zoomedFont;
-}
-
-FontData getCachedFontData(Font f) {
-	FontData data = (FontData)fontDataCache.get(f);
-	if (data != null)
-		return data;
-	data = getLocalFont().getFontData()[0];
-	fontDataCache.put(f, data);
-	return data;
-}
-
-Font createFont(FontData data) {
-	return new Font(Display.getCurrent(), data);
+/**
+ * @see Graphics#setAntialias(int)
+ */
+public void setAntialias(int value) {
+	graphics.setAntialias(value);
 }
 
 /** @see Graphics#setBackgroundColor(Color) */
@@ -499,6 +555,13 @@ public void setClip(Rectangle r) {
 	graphics.setClip(zoomClipRect(r));
 }
 
+/**
+ * @see Graphics#setFillRule(int)
+ */
+public void setFillRule(int rule) {
+	graphics.setFillRule(rule);
+}
+
 /** @see Graphics#setFont(Font) */
 public void setFont(Font f) {
 	setLocalFont(f);
@@ -507,6 +570,31 @@ public void setFont(Font f) {
 /** @see Graphics#setForegroundColor(Color) */
 public void setForegroundColor(Color rgb) {
 	graphics.setForegroundColor(rgb);
+}
+
+public void setInterpolation(int interpolation) {
+	graphics.setInterpolation(interpolation);
+}
+
+/**
+ * @see Graphics#setLineCap(int)
+ */
+public void setLineCap(int cap) {
+	graphics.setLineCap(cap);
+}
+
+/**
+ * @see Graphics#setLineDash(int[])
+ */
+public void setLineDash(int[] dash) {
+	graphics.setLineDash(dash);
+}
+
+/**
+ * @see Graphics#setLineJoin(int)
+ */
+public void setLineJoin(int join) {
+	graphics.setLineJoin(join);
 }
 
 /** @see Graphics#setLineStyle(int) */
@@ -527,6 +615,21 @@ private void setLocalFont(Font f) {
 private void setLocalLineWidth(int width) {
 	localLineWidth = width;
 	graphics.setLineWidth(zoomLineWidth(width));
+}
+
+void setScale(double value) {
+	if (zoom == value)
+		return;
+	this.zoom = value;
+	graphics.setFont(zoomFont(getLocalFont()));
+	graphics.setLineWidth(zoomLineWidth(localLineWidth));
+}
+
+/**
+ * @see Graphics#setTextAntialias(int)
+ */
+public void setTextAntialias(int value) {
+	graphics.setTextAntialias(value);
 }	
 
 /** @see Graphics#setXORMode(boolean) */
@@ -543,6 +646,79 @@ public void translate(int dx, int dy) {
 	fractionalX = dxFloat - Math.floor(dxFloat);
 	fractionalY = dyFloat - Math.floor(dyFloat);
 	graphics.translate((int)Math.floor(dxFloat), (int)Math.floor(dyFloat));
+}
+
+private Rectangle zoomClipRect(Rectangle r) {
+	TEMP.x = (int)(Math.floor(r.x * zoom + fractionalX));
+	TEMP.y = (int)(Math.floor(r.y * zoom + fractionalY));
+	TEMP.width = (int)(Math.ceil(((r.x + r.width) * zoom + fractionalX))) - TEMP.x;
+	TEMP.height = (int)(Math.ceil(((r.y + r.height) * zoom + fractionalY))) - TEMP.y;
+	return TEMP;
+}
+
+private Rectangle zoomFillRect(int x, int y, int w, int h) {
+	TEMP.x = (int)(Math.floor((x * zoom + fractionalX)));
+	TEMP.y = (int)(Math.floor((y * zoom + fractionalY)));
+	TEMP.width = (int)(Math.floor(((x + w - 1) * zoom + fractionalX))) - TEMP.x + 1;
+	TEMP.height = (int)(Math.floor(((y + h - 1) * zoom + fractionalY))) - TEMP.y + 1;
+	return TEMP;
+}
+
+Font zoomFont(Font f) {
+	if (f == null)
+		f = Display.getCurrent().getSystemFont();
+	FontData data = getCachedFontData(f);
+	int zoomedFontHeight = zoomFontHeight(data.getHeight());
+	allowText = zoomedFontHeight > 0;
+	fontKey.setValues(f, zoomedFontHeight);
+	return getCachedFont(fontKey);
+}
+
+int zoomFontHeight(int height) {
+	return (int)(zoom * height);
+}
+
+int zoomLineWidth(int w) {
+	return w;
+}
+
+private int[] zoomPointList(int[] points) {
+	int[] scaled = null;
+
+	// Look in cache for a integer array with the same length as 'points'
+	for (int i = 0; i < intArrayCache.length; i++) {
+		if (intArrayCache[i].length == points.length) {
+			scaled = intArrayCache[i];
+			
+			// Move this integer array up one notch in the array
+			if (i != 0) {
+				int[] temp = intArrayCache[i - 1];
+				intArrayCache[i - 1] = scaled;
+				intArrayCache[i] = temp;	
+			}
+		}
+	}
+	
+	// If no match is found, take the one that is last and resize it.
+	if (scaled == null) {
+		intArrayCache[intArrayCache.length - 1] = new int[points.length];
+		scaled = intArrayCache[intArrayCache.length - 1];
+	}
+	
+	// Scale the points
+	for (int i = 0; (i + 1) < points.length; i += 2) {
+		scaled[i] = (int)(Math.floor((points[i] * zoom + fractionalX)));
+		scaled[i + 1] = (int)(Math.floor((points[i + 1] * zoom + fractionalY)));
+	}
+	return scaled;
+}	
+
+private Rectangle zoomRect(int x, int y, int w, int h) {
+	TEMP.x = (int)(Math.floor(x * zoom + fractionalX));
+	TEMP.y = (int)(Math.floor(y * zoom + fractionalY));
+	TEMP.width = (int)(Math.floor(((x + w) * zoom + fractionalX))) - TEMP.x;
+	TEMP.height = (int)(Math.floor(((y + h) * zoom + fractionalY))) - TEMP.y;
+	return TEMP;
 }
 
 private TextLayout zoomTextLayout(TextLayout layout) {
@@ -604,87 +780,6 @@ private Point zoomTextPoint(int x, int y) {
 	return new Point(((int)(Math.floor((x * zoom) + fractionalX))),
 						(int)(Math.floor((y + localCache.height - 1) * zoom 
 											- targetCache.height + 1 + fractionalY)));
-}
-
-private int[] zoomPointList(int[] points) {
-	int[] scaled = null;
-
-	// Look in cache for a integer array with the same length as 'points'
-	for (int i = 0; i < intArrayCache.length; i++) {
-		if (intArrayCache[i].length == points.length) {
-			scaled = intArrayCache[i];
-			
-			// Move this integer array up one notch in the array
-			if (i != 0) {
-				int[] temp = intArrayCache[i - 1];
-				intArrayCache[i - 1] = scaled;
-				intArrayCache[i] = temp;	
-			}
-		}
-	}
-	
-	// If no match is found, take the one that is last and resize it.
-	if (scaled == null) {
-		intArrayCache[intArrayCache.length - 1] = new int[points.length];
-		scaled = intArrayCache[intArrayCache.length - 1];
-	}
-	
-	// Scale the points
-	for (int i = 0; (i + 1) < points.length; i += 2) {
-		scaled[i] = (int)(Math.floor((points[i] * zoom + fractionalX)));
-		scaled[i + 1] = (int)(Math.floor((points[i + 1] * zoom + fractionalY)));
-	}
-	return scaled;
-}
-
-private Rectangle zoomFillRect(int x, int y, int w, int h) {
-	TEMP.x = (int)(Math.floor((x * zoom + fractionalX)));
-	TEMP.y = (int)(Math.floor((y * zoom + fractionalY)));
-	TEMP.width = (int)(Math.floor(((x + w - 1) * zoom + fractionalX))) - TEMP.x + 1;
-	TEMP.height = (int)(Math.floor(((y + h - 1) * zoom + fractionalY))) - TEMP.y + 1;
-	return TEMP;
-}
-
-Font zoomFont(Font f) {
-	if (f == null)
-		f = Display.getCurrent().getSystemFont();
-	FontData data = getCachedFontData(f);
-	int zoomedFontHeight = zoomFontHeight(data.getHeight());
-	allowText = zoomedFontHeight > 0;
-	fontKey.setValues(f, zoomedFontHeight);
-	return getCachedFont(fontKey);
-}
-
-int zoomFontHeight(int height) {
-	return (int)(zoom * height);
-}	
-
-private Rectangle zoomClipRect(Rectangle r) {
-	TEMP.x = (int)(Math.floor(r.x * zoom + fractionalX));
-	TEMP.y = (int)(Math.floor(r.y * zoom + fractionalY));
-	TEMP.width = (int)(Math.ceil(((r.x + r.width) * zoom + fractionalX))) - TEMP.x;
-	TEMP.height = (int)(Math.ceil(((r.y + r.height) * zoom + fractionalY))) - TEMP.y;
-	return TEMP;
-}
-
-private Rectangle zoomRect(int x, int y, int w, int h) {
-	TEMP.x = (int)(Math.floor(x * zoom + fractionalX));
-	TEMP.y = (int)(Math.floor(y * zoom + fractionalY));
-	TEMP.width = (int)(Math.floor(((x + w) * zoom + fractionalX))) - TEMP.x;
-	TEMP.height = (int)(Math.floor(((y + h) * zoom + fractionalY))) - TEMP.y;
-	return TEMP;
-}
-
-//private Rectangle zoomRect(Rectangle r) {
-//	TEMP.x = (int)(Math.floor((r.x * zoom + fractionalX)));
-//	TEMP.y = (int)(Math.floor((r.y * zoom + fractionalY)));
-//	TEMP.width = (int)(Math.floor((r.right() * zoom + fractionalX))) - TEMP.x;
-//	TEMP.height = (int)(Math.floor((r.bottom() * zoom + fractionalY))) - TEMP.y;
-//	return TEMP;
-//}
-
-int zoomLineWidth(int w) {
-	return w;
 }
 
 }
