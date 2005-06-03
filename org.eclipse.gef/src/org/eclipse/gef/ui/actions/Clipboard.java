09837ad0cc0752031997a1a50b27155ab87a213c
pshah <pshah>
2005-06-03 23:35:04 +0000

    fix for bug 71395 - GEF Clipboard now synchronizes with the system clipboard

--- org.eclipse.gef/src/org/eclipse/gef/ui/actions/Clipboard.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/actions/Clipboard.java
@@ -10,18 +10,33 @@
  *******************************************************************************/
 package org.eclipse.gef.ui.actions;
 
+import org.eclipse.swt.dnd.Transfer;
+
+import org.eclipse.gef.dnd.SimpleObjectTransfer;
+
 /**
- * A GEF clipboard for cut/copy/paste actions between GEF editors.  This will not work
- * between two instances of the workbench (but will work between multiple windows
- * belonging to a single instance of the workbench).  Setting the contents of the
+ * A GEF clipboard for cut/copy/paste actions between GEF editors.  It exists mainly for
+ * convenience and allows clients to add graphical objects to the system clipboard.  It
+ * will not work between two instances of the workbench (but will work between multiple 
+ * windows belonging to a single instance of the workbench).  Setting the contents of the
  * clipboard will erase the previous contents of the clipboard.
  * 
  * @author Eric Bordeau
+ * @author Pratik Shah
  */
 public class Clipboard {
 
 private static Clipboard defaultClipboard = new Clipboard();
-private Object contents;
+private static final SimpleObjectTransfer TRANSFER = new SimpleObjectTransfer() {
+	private final String TYPE_NAME = "org.eclipse.gef.graphical.obj.transfer"; //$NON-NLS-1$
+	private final int TYPE_ID = registerType(TYPE_NAME);
+	protected int[] getTypeIds() {
+		return new int[] {TYPE_ID};
+	}
+	protected String[] getTypeNames() {
+		return new String[] {TYPE_NAME};
+	}
+};
 
 /**
  * Returns the default clipboard.  
@@ -33,6 +48,9 @@ public static Clipboard getDefault() {
 
 /**
  * Constructs a new Clipboard object.
+ * @deprecated As of 3.1, the GEF Clipboard synchronizes with the system clipboard.
+ * Multiple instances of this class should not be created.  Use {@link #getDefault()}.
+ * This method will be removed in future releases.
  */
 public Clipboard() { }
 
@@ -41,16 +59,23 @@ public Clipboard() { }
  * @return contents of the clipboard
  */
 public Object getContents() {
+	org.eclipse.swt.dnd.Clipboard cb = new org.eclipse.swt.dnd.Clipboard(null);
+	Object contents = cb.getContents(TRANSFER);
+	cb.dispose();
 	return contents;
 }
 
 /**
- * Sets the contents of the clipboard.  This will erase the previous contents of the
- * clipboard.
+ * Sets the contents of the clipboard.  This will erase the previous contents of this as
+ * well as the system clipboard.  The provided contents will not be garbage-collected
+ * until some other contents are set using this method.
+ * 
  * @param contents the new contents
  */
 public void setContents(Object contents) {
-	this.contents = contents;
+	org.eclipse.swt.dnd.Clipboard cb = new org.eclipse.swt.dnd.Clipboard(null);
+	cb.setContents(new Object[] {contents}, new Transfer[] {TRANSFER});
+	cb.dispose();
 }
 
 }
