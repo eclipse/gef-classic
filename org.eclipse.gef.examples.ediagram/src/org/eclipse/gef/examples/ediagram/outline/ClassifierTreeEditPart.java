29545f266c944902df4430b466ce53ce71ca5f0e
pshah <pshah>
2005-06-08 06:37:42 +0000

    see bug 84910 comment 12 -- subclass refreshes properly if supertype's name changes

--- org.eclipse.gef.examples.ediagram/src/org/eclipse/gef/examples/ediagram/outline/ClassifierTreeEditPart.java
+++ org.eclipse.gef.examples.ediagram/src/org/eclipse/gef/examples/ediagram/outline/ClassifierTreeEditPart.java
@@ -11,6 +11,7 @@
 package org.eclipse.gef.examples.ediagram.outline;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -25,8 +26,13 @@ import org.eclipse.gef.examples.ediagram.EDiagramImages;
 
 
 public class ClassifierTreeEditPart extends BaseTreeEditPart {
+	private List superTypes = Collections.EMPTY_LIST;
 	public ClassifierTreeEditPart(EClassifier model) {
 		super(model);
+	}
+	public void deactivate() {
+		updateSuperTypes(Collections.EMPTY_LIST);
+		super.deactivate();
 	}
 	private EClassifier getEClassifier() {
 		return (EClassifier)getModel();
@@ -43,7 +49,7 @@ public class ClassifierTreeEditPart extends BaseTreeEditPart {
 		if (getModel() instanceof EClass) {
 			EClass subType = (EClass)getModel();
 			List children = new ArrayList();
-			List superTypes = subType.getESuperTypes();
+			updateSuperTypes(subType.getESuperTypes());
 			for (Iterator iter = superTypes.iterator(); iter.hasNext();) {
 				children.add(new InheritanceModel((EClass)iter.next(), subType));
 			}
@@ -55,12 +61,27 @@ public class ClassifierTreeEditPart extends BaseTreeEditPart {
 	protected void handlePropertyChanged(Notification msg) {
 		switch (msg.getFeatureID(EClassifier.class)) {
 			case EcorePackage.ECLASSIFIER__NAME:
-				refreshVisuals();
+				if (msg.getNotifier() == getModel())
+					refreshVisuals();
+				else
+					// a supertype's name has changed
+					refreshChildren();
 		}
 		switch (msg.getFeatureID(EClass.class)) {
 			case EcorePackage.ECLASS__ESTRUCTURAL_FEATURES:
 			case EcorePackage.ECLASS__ESUPER_TYPES:
 				refreshChildren();
+		}
+	}
+	protected void updateSuperTypes(List newTypes) {
+		for (Iterator iter = superTypes.iterator(); iter.hasNext();) {
+			EClass superClass = (EClass) iter.next();
+			superClass.eAdapters().remove(modelListener);
+		}
+		superTypes = newTypes;
+		for (Iterator iter = superTypes.iterator(); iter.hasNext();) {
+			EClass superClass = (EClass) iter.next();
+			superClass.eAdapters().add(modelListener);
 		}
 	}
 }
