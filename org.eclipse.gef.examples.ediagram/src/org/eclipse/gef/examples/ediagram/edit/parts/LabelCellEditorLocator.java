14827a026faacea7d8cfdce6e8f2bcc09fbb4892
pshah <pshah>
2005-06-21 02:04:35 +0000

    fix for bug 99203 - using trim to locate and size celleditor

--- org.eclipse.gef.examples.ediagram/src/org/eclipse/gef/examples/ediagram/edit/parts/LabelCellEditorLocator.java
+++ org.eclipse.gef.examples.ediagram/src/org/eclipse/gef/examples/ediagram/edit/parts/LabelCellEditorLocator.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.gef.examples.ediagram.edit.parts;
 
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Text;
 
 import org.eclipse.jface.viewers.CellEditor;
@@ -27,15 +26,6 @@ final public class LabelCellEditorLocator
 
 protected IFigure fig;
 
-private static int WIN_X_OFFSET = -4;
-private static int WIN_W_OFFSET = 5;
-private static int GTK_X_OFFSET = 0;
-private static int GTK_W_OFFSET = 0;
-private static int MAC_X_OFFSET = -3;
-private static int MAC_W_OFFSET = 9;
-private static int MAC_Y_OFFSET = -3;
-private static int MAC_H_OFFSET = 6;
-
 public LabelCellEditorLocator(IFigure figure) {
 	fig = figure;
 }
@@ -43,33 +33,17 @@ public LabelCellEditorLocator(IFigure figure) {
 public void relocate(CellEditor celleditor) {
 	Text text = (Text)celleditor.getControl();
 
-	Rectangle rect = null;
-	if (fig instanceof Label)	
-		rect = ((Label)fig).getTextBounds().intersect(fig.getClientArea());
-	else
-		rect = fig.getClientArea(Rectangle.SINGLETON);
+	Rectangle rect = fig.getClientArea(Rectangle.SINGLETON);
+	if (fig instanceof Label)
+		rect = ((Label)fig).getTextBounds().intersect(rect);
 	fig.translateToAbsolute(rect);
-	
-	int xOffset = 0;
-	int wOffset = 0;
-	int yOffset = 0;
-	int hOffset = 0;
-	
-	if (SWT.getPlatform().equalsIgnoreCase("gtk")) {
-		xOffset = GTK_X_OFFSET;
-		wOffset = GTK_W_OFFSET;
-	} else if (SWT.getPlatform().equalsIgnoreCase("carbon")) {
-		xOffset = MAC_X_OFFSET;
-		wOffset = MAC_W_OFFSET;
-		yOffset = MAC_Y_OFFSET;
-		hOffset = MAC_H_OFFSET;
-	} else {
-		xOffset = WIN_X_OFFSET;
-		wOffset = WIN_W_OFFSET;
-	}
 
-	text.setBounds(rect.x + xOffset, rect.y + yOffset, 
-			rect.width + wOffset, rect.height + hOffset);	
+	org.eclipse.swt.graphics.Rectangle trim = text.computeTrim(0, 0, 0, 0);
+	rect.translate(trim.x, trim.y);
+	rect.width += trim.width;
+	rect.height += trim.height;
+	
+	text.setBounds(rect.x, rect.y, rect.width, rect.height);
 }
 
 }
