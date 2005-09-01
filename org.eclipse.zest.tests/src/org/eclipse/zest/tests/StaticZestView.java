00c1c46dbed060d714887ca1fae3a85efe519b69
ibull <ibull>
2005-09-01 20:25:04 +0000

    Added a static zest test view for the static zest viewer.

---
+++ org.eclipse.zest.tests/src/org/eclipse/zest/tests/StaticZestView.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright 2005, CHISEL Group, University of Victoria, Victoria, BC, Canada.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     The Chisel Group, University of Victoria
+ *******************************************************************************/
+package org.eclipse.mylar.zest.tests;
+
+import org.eclipse.jface.viewers.DecoratingLabelProvider;
+import org.eclipse.mylar.zest.core.ZestStyles;
+import org.eclipse.mylar.zest.core.viewers.StaticGraphViewer;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+
+
+/**
+ * This sample class demonstrates how to plug-in a new
+ * workbench view. The view shows data obtained from the
+ * model. The sample creates a dummy model on the fly,
+ * but a real implementation would connect to the model
+ * available either in this or another plug-in (e.g. the workspace).
+ * The view is connected to the model using a content provider.
+ * <p>
+ * The view uses a label provider to define how model
+ * objects should be presented in the view. Each
+ * view can present the same model objects using
+ * different labels and icons, if needed. Alternatively,
+ * a single label provider can be shared between views
+ * in order to ensure that objects of the same type are
+ * presented in the same way everywhere.
+ * <p>
+ * 
+ * @author irbull
+ * @author ccallendar
+ */
+public class StaticZestView extends ViewPart {
+	
+	private StaticGraphViewer viewer;
+	
+	/**
+	 * The constructor.
+	 */
+	public StaticZestView() {
+	}
+	
+	/**
+	 * This is a callback that will allow us
+	 * to create the viewer and initialize it.
+	 */
+	public void createPartControl(Composite parent) {
+		viewer = new StaticGraphViewer(parent, ZestStyles.DIRECTED_GRAPH | 
+				ZestStyles.HIGHLIGHT_ADJACENT_NODES | ZestStyles.PANNING | 
+				ZestStyles.NO_OVERLAPPING_NODES | ZestStyles.STABILIZE | ZestStyles.ENFORCE_BOUNDS);
+		
+		viewer.setContentProvider(new GraphContentProvider() );
+		viewer.setLabelProvider(new DecoratingLabelProvider(new GraphLabelProvider(), 
+					PlatformUI.getWorkbench().getDecoratorManager().getLabelDecorator()));
+		
+		viewer.setInput(getViewSite());
+		
+		//makeActions();
+		//contributeToActionBars();
+		//hookContextMenu();
+	}
+	
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+	
+	
+}
