ad9f6a53c89dc362837be264efe053a8b7e7d496
rhudson <rhudson>
2005-10-07 19:07:55 +0000

    111976 transpose added

--- org.eclipse.draw2d/src/org/eclipse/draw2d/geometry/PointList.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/geometry/PointList.java
@@ -405,4 +405,19 @@ public void translate(int x, int y) {
 	}
 }
 
+/**
+ * Transposes all x and y values. Useful for orientation changes.
+ * @since 3.2
+ */
+public void transpose() {
+	int temp;
+	if (bounds != null)
+		bounds.transpose();
+	for (int i = 0; i < size * 2; i += 2) {
+		temp = points[i];
+		points[i] = points[i + 1];
+		points[i + 1] = temp;
+	}
+}
+
 }
