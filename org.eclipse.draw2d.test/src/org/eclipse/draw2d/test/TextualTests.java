485e9dd9539cfc2245632e2dbc59486d48051990
pshah <pshah>
2005-10-25 22:16:29 +0000

    added test for bug 113700 - testGetFirstAndLastOffsetForLine()

--- org.eclipse.draw2d.test/src/org/eclipse/draw2d/test/TextualTests.java
+++ org.eclipse.draw2d.test/src/org/eclipse/draw2d/test/TextualTests.java
@@ -12,9 +12,12 @@ package org.eclipse.draw2d.test;
 
 import java.util.List;
 
+import org.eclipse.swt.graphics.Font;
+
 import org.eclipse.draw2d.text.FlowPage;
 import org.eclipse.draw2d.text.InlineFlow;
 import org.eclipse.draw2d.text.TextFlow;
+import org.eclipse.draw2d.text.TextFragmentBox;
 
 /**
  * @author Pratik Shah
@@ -46,10 +49,37 @@ public void testLineRootBidiCommit() {
 	block.setFont(TAHOMA);
 	block.validate();
 	
+	// TODO enhance verification
 	List boldFragments = bold.getFragments();
 	assertTrue(boldFragments.size() == 3);
 	List italicFragments = italics.getFragments();
 	assertTrue(italicFragments.size() == 2);
+}
+
+// test for bug 113700
+public void testGetFirstAndLastOffsetForLine() {
+	Font xlFont = new Font(null, "Tahoma", 28, 0);
+	FlowPage block = new FlowPage();
+	TextFlow smallText = new TextFlow("wwww");
+	TextFlow largeText = new TextFlow("ABCD");
+	
+	block.setFont(TAHOMA);
+	largeText.setFont(xlFont);
+	block.add(smallText);
+	block.add(largeText);
+	block.setSize(-1, -1);
+	block.validate();
+	
+	TextFragmentBox box = (TextFragmentBox)largeText.getFragments().get(0);
+	int y = box.getBaseline();
+	try {
+		assertTrue(smallText.getFirstOffsetForLine(y) == 0);
+		assertTrue(smallText.getLastOffsetForLine(y) == 3);
+		assertTrue(smallText.getFirstOffsetForLine(y - 1) == -1);
+		assertTrue(smallText.getLastOffsetForLine(y + 1) == -1);
+	} finally {
+		xlFont.dispose();
+	}
 }
 
 }
