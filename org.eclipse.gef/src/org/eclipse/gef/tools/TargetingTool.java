9b32041cd795d80a2e5889207ad63894b0394e48
ahunter <ahunter>
2007-01-09 19:33:47 +0000

    [168582] gef_R32_Maintenance crevells 070109 Support extending of ViewportAutoexposeHelper to allow scrolling outside the viewport

--- org.eclipse.gef/src/org/eclipse/gef/tools/TargetingTool.java
+++ org.eclipse.gef/src/org/eclipse/gef/tools/TargetingTool.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -391,6 +391,15 @@ protected boolean updateTargetUnderMouse() {
 		return changed;
 	} else
 		return false;
+}
+
+
+/**
+ * Returns <code>null</code> or the current autoexpose helper.
+ * @return null or a helper
+ */
+protected AutoexposeHelper getAutoexposeHelper() {
+    return exposeHelper;
 }
 
 }
