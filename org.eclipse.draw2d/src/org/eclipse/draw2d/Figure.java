1e9e738a1f040cdc083b0611b69744a3edf98c99
pshah <pshah>
2007-05-18 20:44:13 +0000

    163721 - invoking repaint() when border or font changes

--- org.eclipse.draw2d/src/org/eclipse/draw2d/Figure.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/Figure.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1365,6 +1365,7 @@ public void setBackgroundColor(Color bg) {
 public void setBorder(Border border) {
 	this.border = border;
 	revalidate();
+	repaint();
 }
 
 /**
@@ -1512,6 +1513,7 @@ public void setFont(Font f) {
 	if (font != f) {
 		font = f;
 		revalidate();
+		repaint();
 	}
 }
 
