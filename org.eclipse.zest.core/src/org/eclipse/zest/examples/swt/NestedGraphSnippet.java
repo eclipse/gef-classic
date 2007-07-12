a05f7df3268922e8366aa2852a0acbbbd8c69d82
ibull <ibull>
2007-07-12 22:00:10 +0000

    Removed some dead code from this snippet.

--- org.eclipse.zest.core/src/org/eclipse/zest/examples/swt/NestedGraphSnippet.java
+++ org.eclipse.zest.core/src/org/eclipse/zest/examples/swt/NestedGraphSnippet.java
@@ -90,66 +90,15 @@ public class NestedGraphSnippet {
 		Shell shell = new Shell(d);
 		shell.setText("GraphSnippet1");
 		shell.setLayout(new FillLayout());
-		shell.setSize(1000, 800);
+		shell.setSize(500, 800);
 
 		Graph g = new Graph(shell, SWT.NONE);
 		g.setNodeStyle(ZestStyles.NODES_HIGHLIGHT_ADJACENT);
 		createContainer(g);
 
-		/*
-		GraphNode n1 = new GraphNode(g, SWT.NONE, "non container node", classImage);
-
-		GraphContainer p1 = new GraphContainer(g, SWT.NONE, "A Container with Multiple \nLines", image1);
-		GraphContainer p2 = new GraphContainer(g, SWT.NONE, "org.example.plugin", image1);
-		p2.setText("org\nexample\nplugin that is really really really really really really long");
-		GraphContainer p3 = new GraphContainer(g, SWT.NONE, "org.example.ui", image1);
-		GraphContainer p4 = new GraphContainer(g, SWT.NONE, "org.example.action", image1);
-
-		GraphNode a = new GraphNode(p1, ZestStyles.NODES_FISHEYE | ZestStyles.NODES_HIDE_TEXT, "SomeClass.java", classImage);
-		for (int i = 0; i < 4; i++) {
-			GraphNode b = new GraphNode(p1, ZestStyles.NODES_FISHEYE | ZestStyles.NODES_HIDE_TEXT, "SomeNestedClass.java", classImage);
-			new GraphConnection(g, SWT.NONE, a, b);
-			for (int j = 0; j < 4; j++) {
-				GraphNode c = new GraphNode(p1, ZestStyles.NODES_FISHEYE | ZestStyles.NODES_HIDE_TEXT, "DefaultAction.java", classImage);
-				new GraphConnection(g, SWT.NONE, b, c);
-				for (int k = 0; k < 4; k++) {
-					GraphNode e = new GraphNode(p1, ZestStyles.NODES_FISHEYE | ZestStyles.NODES_HIDE_TEXT, "LastAction(Hero).java", classImage);
-					new GraphConnection(g, SWT.NONE, c, e);
-				}
-			}
-		}
-		p1.setScale(0.25);
-
-		GraphNode aa = new GraphNode(p3, ZestStyles.NODES_FISHEYE | ZestStyles.NODES_HIDE_TEXT, "SomeClass.java", classImage);
-		for (int i = 0; i < 4; i++) {
-			GraphNode b = new GraphNode(p3, ZestStyles.NODES_FISHEYE | ZestStyles.NODES_HIDE_TEXT, "SomeNestedClass.java", classImage);
-			new GraphConnection(g, SWT.NONE, aa, b);
-			for (int j = 0; j < 4; j++) {
-				GraphNode c = new GraphNode(p3, ZestStyles.NODES_FISHEYE | ZestStyles.NODES_HIDE_TEXT, "DefaultAction.java", classImage);
-				new GraphConnection(g, SWT.NONE, b, c);
-			}
-		}
-		GraphNode c6 = new GraphNode(p2, SWT.NONE, "Activator.java", classImage);
-
-		new GraphConnection(g, SWT.NONE, n1, p1);
-		new GraphConnection(g, SWT.NONE, n1, p3);
-		new GraphConnection(g, SWT.NONE, n1, p4);
-		new GraphConnection(g, SWT.NONE, n1, p2);
-		new GraphConnection(g, SWT.NONE, p1, p3);
-		new GraphConnection(g, SWT.NONE, p2, p4);
-
-		new GraphConnection(g, SWT.NONE, c6, a);
-		*/
 		CompositeLayoutAlgorithm compositeLayoutAlgorithm = new CompositeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING, new LayoutAlgorithm[] { new GridLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), new HorizontalShift(LayoutStyles.NO_LAYOUT_NODE_RESIZING) });
 		//g.setLayoutAlgorithm(new GridLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
 		g.setLayoutAlgorithm(compositeLayoutAlgorithm, true);
-/*
-		CompositeLayoutAlgorithm compositeLayoutAlgorithm = new CompositeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING, new LayoutAlgorithm[] { new TreeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), new HorizontalShift(LayoutStyles.NO_LAYOUT_NODE_RESIZING) });
-		g.setLayoutAlgorithm(compositeLayoutAlgorithm, true);
-		p1.setLayoutAlgorithm(new RadialLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-		p2.setLayoutAlgorithm(new RadialLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-		p3.setLayoutAlgorithm(new RadialLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-		*/
 
 		shell.open();
 		while (!shell.isDisposed()) {
