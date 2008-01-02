1ed7ce35782590b1c4ed17e8d87d3b65677bbc5e
ahunter <ahunter>
2008-01-02 18:17:19 +0000

    [112692] gef-head crevells 080102 PaletteStack.setChildren should refresh active entry

--- org.eclipse.gef/src/org/eclipse/gef/palette/PaletteStack.java
+++ org.eclipse.gef/src/org/eclipse/gef/palette/PaletteStack.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -106,7 +106,7 @@ private void checkActiveEntry() {
 		activeEntry = null;
 	if (activeEntry == null && getChildren().size() > 0)
 		activeEntry = (PaletteEntry)getChildren().get(0);
-	if (!activeEntry.isVisible()) {
+	if (activeEntry != null && !activeEntry.isVisible()) {
 	    for (Iterator iterator = getChildren().iterator(); iterator.hasNext();) {
             PaletteEntry child = (PaletteEntry) iterator.next();
             if (child.isVisible()) {
