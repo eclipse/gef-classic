b0f25fd89ec3f829af33d41aebe17b962a9905ae
pshah <pshah>
2008-01-14 21:57:29 +0000

    [192760] - fixed incorrect color definitions

--- org.eclipse.draw2d/src/org/eclipse/draw2d/ColorConstants.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/ColorConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -113,15 +113,15 @@ Color titleForeground
 Color titleInactiveForeground 
 				= SystemColorFactory.getColor(SWT.COLOR_TITLE_INACTIVE_FOREGROUND);
 /**
- * @see SWT#COLOR_TITLE_INACTIVE_FOREGROUND
+ * @see SWT#COLOR_TITLE_INACTIVE_BACKGROUND
  */
 Color titleInactiveBackground 
-				= SystemColorFactory.getColor(SWT.COLOR_TITLE_INACTIVE_FOREGROUND);
+				= SystemColorFactory.getColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND);
 /**
- * @see SWT#COLOR_TITLE_INACTIVE_FOREGROUND
+ * @see SWT#COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT
  */
 Color titleInactiveGradient   
-				= SystemColorFactory.getColor(SWT.COLOR_TITLE_INACTIVE_FOREGROUND);
+				= SystemColorFactory.getColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT);
 
 /**
  * @see SWT#COLOR_INFO_FOREGROUND
