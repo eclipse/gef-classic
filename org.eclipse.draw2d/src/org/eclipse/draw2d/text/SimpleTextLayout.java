ecc6457bf1f98a6e97cfcd4e3f25aeced0450a40
ahunter <ahunter>
2008-02-25 21:48:16 +0000

    [219838] gef-head crevells 080225 Inconsistent truncation between drawers and tools on palette

--- org.eclipse.draw2d/src/org/eclipse/draw2d/text/SimpleTextLayout.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/text/SimpleTextLayout.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -55,7 +55,7 @@ protected void layout() {
         fragment.length = result - offset;
         fragment.offset = offset;
         fragment.setWidth(-1);
-        flowUtilities.setupFragment(fragment, font, text);
+        flowUtilities.setupFragment(fragment, font, text.substring(offset, result));
         getContext().addToCurrentLine(fragment);
         getContext().endLine();
         offset = result + delimeterLength;
