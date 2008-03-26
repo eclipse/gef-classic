d5ed0d653c6883edc0c0d0c2d05c83e6eb607196
ahunter <ahunter>
2008-03-26 19:56:51 +0000

    [214900] gef-head ahunter 080326 GEF should be using ICU instead of java.text

--- org.eclipse.draw2d/src/org/eclipse/draw2d/text/BidiProcessor.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/text/BidiProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,7 @@
  *******************************************************************************/
 package org.eclipse.draw2d.text;
 
-import java.text.Bidi;
+import com.ibm.icu.text.Bidi;
 import java.util.ArrayList;
 import java.util.List;
 
