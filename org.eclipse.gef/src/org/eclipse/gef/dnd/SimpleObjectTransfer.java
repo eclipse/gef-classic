a31f866d0bcf2c0867a2b84027235b47eb2eb671
ahunter <ahunter>
2008-07-11 21:02:19 +0000

    [141167] gef-head ahunter 080711 NPE in SimpleObjectTransfer.nativeToJava on Linux

--- org.eclipse.gef/src/org/eclipse/gef/dnd/SimpleObjectTransfer.java
+++ org.eclipse.gef/src/org/eclipse/gef/dnd/SimpleObjectTransfer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -54,6 +54,9 @@ public void javaToNative(Object object, TransferData transferData) {
  */
 public Object nativeToJava(TransferData transferData) {
 	byte bytes[] = (byte[])super.nativeToJava(transferData);
+	if (bytes == null) {
+		return null;
+	}
 	long startTime = Long.parseLong(new String(bytes));
 	return (this.startTime == startTime)
 		? getObject()
