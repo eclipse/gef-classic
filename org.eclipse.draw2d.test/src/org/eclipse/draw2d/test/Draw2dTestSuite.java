d319479d11235835c2a14acf034b3ea70dca6f86
ahunter <ahunter>
2008-08-08 14:39:43 +0000

    [227977] gef-R3_4_maintenance ahunter 080808 PrecisionPoint should override Point.equals

--- org.eclipse.draw2d.test/src/org/eclipse/draw2d/test/Draw2dTestSuite.java
+++ org.eclipse.draw2d.test/src/org/eclipse/draw2d/test/Draw2dTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -42,6 +42,8 @@ public Draw2dTestSuite() {
 	addTest(new TestSuite(LookAheadTest.class));
 	addTest(new TestSuite(TextualTests.class));
 	addTest(new TestSuite(PointListTests.class));
+    addTest(new TestSuite(PrecisionDimensionTest.class));
+    addTest(new TestSuite(PrecisionPointTest.class));
     addTest(new TestSuite(PrecisionRectangleTest.class));
     addTest(new TestSuite(ThumbnailTest.class));
     addTest(new TestSuite(FigureUtilitiesTest.class));
