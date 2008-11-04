9edea7ddcf5f78e94c87af1a557863b795a8d563
ahunter <ahunter>
2008-11-04 15:16:44 +0000

    [204460] gef-head ahunter 081104 NullPointerException with ScrollableThumbnail

--- org.eclipse.draw2d/src/org/eclipse/draw2d/parts/ScrollableThumbnail.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/parts/ScrollableThumbnail.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -85,10 +85,12 @@ private class ScrollSynchronizer
 	public void mouseDoubleClicked(MouseEvent me) { }
 
 	public void mouseDragged(MouseEvent me) {
-		Dimension d = me.getLocation().getDifference(startLocation);
-		d.scale(1.0f / getViewportScaleX(), 1.0f / getViewportScaleY());
-		viewport.setViewLocation(viewLocation.getTranslated(d));
-		me.consume();
+		if (startLocation != null) {
+			Dimension d = me.getLocation().getDifference(startLocation);
+			d.scale(1.0f / getViewportScaleX(), 1.0f / getViewportScaleY());
+			viewport.setViewLocation(viewLocation.getTranslated(d));
+			me.consume();
+		}
 	}
 
 	public void mousePressed(MouseEvent me) {
