aaee9ec1c69290ba74682b64cbff04965297c6cf
ahunter <ahunter>
2009-02-03 17:04:16 +0000

    [263290] gef-head mgobeil 090203 Shapes not drawn properly

--- org.eclipse.draw2d/src/org/eclipse/draw2d/RectangleFigure.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/RectangleFigure.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,7 +35,10 @@ protected void fillShape(Graphics graphics) {
 protected void outlineShape(Graphics graphics) {
 	int lineInset = (int)Math.ceil(Math.max(1.0, getLineWidthFloat() / 2.0));
 	Rectangle r = Rectangle.SINGLETON.setBounds(getBounds());
-	r.shrink(lineInset, lineInset);
+	r.x += lineInset - 1; 
+	r.y += lineInset - 1; 
+	r.width -= (lineInset + lineInset) - 1;
+	r.height -= (lineInset + lineInset) - 1;
 	graphics.drawRectangle(r);
 }
 
