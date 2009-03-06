ef3f6d09b27d6b436435151eddf4f7eed33eed42
ahunter <ahunter>
2009-03-06 16:37:31 +0000

    [226172] gef-head aboyko 090306 GridLayout.layout(...) ignores border insets

--- org.eclipse.draw2d/src/org/eclipse/draw2d/GridLayout.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/GridLayout.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -197,6 +197,13 @@ public class GridLayout extends AbstractLayout {
 			size.width = wHint;
 		if (hHint != SWT.DEFAULT)
 			size.height = hHint;
+		
+		/*
+		 * Adjust for the size of the border
+		 */
+		size.expand(container.getInsets().getWidth(),
+				container.getInsets().getHeight());
+		size.union(getBorderPreferredSize(container));		
 
 		return size;
 	}
