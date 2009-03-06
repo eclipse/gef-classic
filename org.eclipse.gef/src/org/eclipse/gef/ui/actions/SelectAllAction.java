8255a68487c7a16a4ac2993737fab323df0c7c88
ahunter <ahunter>
2009-03-06 15:36:43 +0000

    [151561] gef-head s.holzknecht 090306 SelectAll Action selects Editparts that are not selectable

--- org.eclipse.gef/src/org/eclipse/gef/ui/actions/SelectAllAction.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/actions/SelectAllAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,11 +10,16 @@
  *******************************************************************************/
 package org.eclipse.gef.ui.actions;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.actions.ActionFactory;
 
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.GraphicalViewer;
 import org.eclipse.gef.internal.GEFMessages;
 
@@ -44,8 +49,30 @@ public SelectAllAction(IWorkbenchPart part) {
  */
 public void run() {
 	GraphicalViewer viewer = (GraphicalViewer)part.getAdapter(GraphicalViewer.class);
-	if (viewer != null)
-		viewer.setSelection(new StructuredSelection(viewer.getContents().getChildren()));
+	if (viewer != null) {
+		viewer.setSelection(new StructuredSelection(getSelectableEditParts(viewer)));
+	}
+}
+
+/**
+ * Retrieves edit parts which can be selected
+ * @param viewer from which the edit parts are to be retrieved
+ * @return list of selectable EditParts
+ * @since 3.5
+ */
+private List getSelectableEditParts(GraphicalViewer viewer) {
+	List selectableChildren = new ArrayList();
+	List children = viewer.getContents().getChildren();
+	for (Iterator iter = children.iterator(); iter.hasNext();) {
+		Object child = iter.next();
+		if (child instanceof EditPart) {
+			EditPart childPart = (EditPart) child;
+			if (childPart.isSelectable() == true) {
+				selectableChildren.add(childPart);
+			}
+		}
+	}
+	return selectableChildren;
 }
 
 }
