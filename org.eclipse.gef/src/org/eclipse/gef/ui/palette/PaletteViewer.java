8afa3595bf6a75ca642b65236c4935f35c2cb92d
ahunter <ahunter>
2009-04-03 18:59:18 +0000

    [270028] gef-head aboyko 090403 PaletteViewer#setActiveTool(ToolEntry) may throw an NPE

--- org.eclipse.gef/src/org/eclipse/gef/ui/palette/PaletteViewer.java
+++ org.eclipse.gef/src/org/eclipse/gef/ui/palette/PaletteViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -378,7 +378,9 @@ public void setActiveTool(ToolEntry newMode) {
 	activeEntry = newMode;
 	if (activeEntry != null) {
 		ToolEntryEditPart editpart = getToolEntryEditPart(activeEntry);
-		editpart.setToolSelected(true);
+		if (editpart != null) {
+			editpart.setToolSelected(true);
+		}
 	}
 	fireModeChanged();
 }
