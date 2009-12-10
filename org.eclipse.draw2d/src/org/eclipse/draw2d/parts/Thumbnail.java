4900e517eddc64cbcb1c48f2b0de78957fb41715
ahunter <ahunter>
2009-12-10 18:29:50 +0000

    [282363] gef-head ahunter 091210 thumbnail outline of gef will throw null point exception in certain condition

--- org.eclipse.draw2d/src/org/eclipse/draw2d/parts/Thumbnail.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/parts/Thumbnail.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -141,7 +141,7 @@ class ThumbnailUpdater implements Runnable {
 	 * updating is required, {@link #stop()} is called.
 	 */
 	public void run() {
-		if (!isActive() || !isRunning())
+		if (!isActive() || !isRunning() || thumbnailGraphics == null)
 			return;
 		int v = getCurrentVTile();
 		int sy1 = v * tileSize.height;
