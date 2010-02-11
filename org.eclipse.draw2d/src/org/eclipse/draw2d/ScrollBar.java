1e1b7a477ee43a8a3385048f170df69ad5ed3b41
ahunter <ahunter>
2010-02-11 21:50:30 +0000

    [297903] GEF_HEAD ahunter 100211 Increase visibility to ThumbDragger in ScrollBar

--- org.eclipse.draw2d/src/org/eclipse/draw2d/ScrollBar.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/ScrollBar.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -598,6 +598,9 @@ protected void stepUp() {
 	setValue(getValue() - getStepIncrement());
 }
 
+/**
+ * @since 3.6
+ */
 protected class ThumbDragger
 	extends MouseMotionListener.Stub
 	implements MouseListener
