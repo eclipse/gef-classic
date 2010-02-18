00656e7783b429be60dfc80a106fd3fba2e3c812
ahunter <ahunter>
2010-02-18 04:17:22 +0000

    [303147] gef-head ahunter 100217 Trivial warnings cleanup: The method fireMoved() from the type Figure is deprecated

--- org.eclipse.gef.examples.flow/src/org/eclipse/gef/examples/flow/parts/ActivityDiagramPart.java
+++ org.eclipse.gef.examples.flow/src/org/eclipse/gef/examples/flow/parts/ActivityDiagramPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2005 IBM Corporation and others.
+ * Copyright (c) 2003, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -82,7 +82,7 @@ protected IFigure createFigure() {
 			bounds.width = rect.width;
 			bounds.height = rect.height;
 			if (resize || translate) {
-				fireMoved();
+				fireFigureMoved();
 				repaint();
 			}
 		}
