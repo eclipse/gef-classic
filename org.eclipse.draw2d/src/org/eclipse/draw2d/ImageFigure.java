67a86c2af9af315318f3189f37f0fa9a2b3d0b25
aboyko <aboyko>
2010-02-24 17:10:05 +0000

    [279825] gef-head aboyko 100304 Notify when image has changed for ImageFigure

--- org.eclipse.draw2d/src/org/eclipse/draw2d/ImageFigure.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/ImageFigure.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -27,7 +27,7 @@ import org.eclipse.draw2d.geometry.Rectangle;
  * @author Pratik Shah
  */
 public class ImageFigure 
-	extends Figure 
+	extends AbstractImageFigure 
 {
 
 private Image img;
@@ -156,6 +156,7 @@ public void setImage(Image image) {
 	else
 		size = new Dimension();
 	revalidate();
+	notifyImageChanged();
 	repaint();
 }
 
