b09cc73dc4200e7823b07b72a5099a7c51d904aa
ahunter <ahunter>
2010-05-19 20:00:54 +0000

    [308373] gef-head ahunter 100519 GEF Project should adopt Eclipse formatting guidelines - commit organize imports

--- org.eclipse.zest.core/src/org/eclipse/zest/core/widgets/internal/ExpandGraphLabel.java
+++ org.eclipse.zest.core/src/org/eclipse/zest/core/widgets/internal/ExpandGraphLabel.java
@@ -23,12 +23,12 @@ import org.eclipse.draw2d.Triangle;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
-import org.eclipse.zest.core.widgets.GraphContainer;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.zest.core.widgets.GraphContainer;
 
 public class ExpandGraphLabel extends Figure implements ActionListener {
 
