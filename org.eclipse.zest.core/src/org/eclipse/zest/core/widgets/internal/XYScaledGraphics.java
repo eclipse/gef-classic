68f5d61181df001b1126d1a92c21d59892652062
ahunter <ahunter>
2010-05-19 20:27:58 +0000

    [308373] gef-head ahunter 100519 GEF Project should adopt Eclipse formatting guidelines - commit update copyrights

--- org.eclipse.zest.core/src/org/eclipse/zest/core/widgets/internal/XYScaledGraphics.java
+++ org.eclipse.zest.core/src/org/eclipse/zest/core/widgets/internal/XYScaledGraphics.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: IBM Corporation - initial API and implementation Chisel Group,
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
  * University of Victoria - Adapted for XY Scaled Graphics
- ******************************************************************************/
+ *******************************************************************************/
 package org.eclipse.zest.core.widgets.internal;
 
 import java.util.ArrayList;
