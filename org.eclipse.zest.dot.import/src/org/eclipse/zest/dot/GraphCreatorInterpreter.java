f969c17cd580b5a1736189655eb4d9976f34afdb
fsteeg <fsteeg>
2010-06-20 21:58:28 +0000

    [277397] gef-head fsteeg 100620 Changed DOT grammar to use CamelCase for types (to stick to standard naming conventions when using the generated classes)

--- org.eclipse.zest.dot.import/src/org/eclipse/zest/dot/GraphCreatorInterpreter.java
+++ org.eclipse.zest.dot.import/src/org/eclipse/zest/dot/GraphCreatorInterpreter.java
@@ -24,16 +24,17 @@ import org.eclipse.zest.core.widgets.GraphNode;
 import org.eclipse.zest.core.widgets.ZestStyles;
 import org.eclipse.zest.dot.DotAst.Layout;
 import org.eclipse.zest.dot.DotAst.Style;
-import org.eclipse.zest.internal.dot.parser.dot.a_list;
-import org.eclipse.zest.internal.dot.parser.dot.attr_stmt;
-import org.eclipse.zest.internal.dot.parser.dot.attributetype;
-import org.eclipse.zest.internal.dot.parser.dot.edgeRHS_node;
-import org.eclipse.zest.internal.dot.parser.dot.edge_stmt_node;
-import org.eclipse.zest.internal.dot.parser.dot.graph;
-import org.eclipse.zest.internal.dot.parser.dot.graphtype;
-import org.eclipse.zest.internal.dot.parser.dot.node_id;
-import org.eclipse.zest.internal.dot.parser.dot.node_stmt;
-import org.eclipse.zest.internal.dot.parser.dot.stmt;
+import org.eclipse.zest.internal.dot.parser.dot.AList;
+import org.eclipse.zest.internal.dot.parser.dot.AttrList;
+import org.eclipse.zest.internal.dot.parser.dot.AttrStmt;
+import org.eclipse.zest.internal.dot.parser.dot.AttributeType;
+import org.eclipse.zest.internal.dot.parser.dot.DotGraph;
+import org.eclipse.zest.internal.dot.parser.dot.EdgeRhsNode;
+import org.eclipse.zest.internal.dot.parser.dot.EdgeStmtNode;
+import org.eclipse.zest.internal.dot.parser.dot.GraphType;
+import org.eclipse.zest.internal.dot.parser.dot.NodeId;
+import org.eclipse.zest.internal.dot.parser.dot.NodeStmt;
+import org.eclipse.zest.internal.dot.parser.dot.Stmt;
 import org.eclipse.zest.internal.dot.parser.dot.util.DotSwitch;
 import org.eclipse.zest.layouts.LayoutStyles;
 import org.eclipse.zest.layouts.algorithms.TreeLayoutAlgorithm;
@@ -82,32 +83,32 @@ final class GraphCreatorInterpreter extends DotSwitch<Object> implements
 	}
 
 	@Override
-	public Object casegraph(graph object) {
+	public Object caseDotGraph(DotGraph object) {
 		createGraph(object);
-		return super.casegraph(object);
+		return super.caseDotGraph(object);
 	}
 
 	@Override
-	public Object caseattr_stmt(attr_stmt object) {
+	public Object caseAttrStmt(AttrStmt object) {
 		createAttributes(object);
-		return super.caseattr_stmt(object);
+		return super.caseAttrStmt(object);
 	}
 
 	@Override
-	public Object casenode_stmt(node_stmt object) {
+	public Object caseNodeStmt(NodeStmt object) {
 		createNode(object);
-		return super.casenode_stmt(object);
+		return super.caseNodeStmt(object);
 	}
 
 	@Override
-	public Object caseedge_stmt_node(edge_stmt_node object) {
+	public Object caseEdgeStmtNode(EdgeStmtNode object) {
 		currentEdgeLabelValue = getAttributeValue(object, "label"); //$NON-NLS-1$
 		currentEdgeStyleValue = getAttributeValue(object, "style"); //$NON-NLS-1$
-		return super.caseedge_stmt_node(object);
+		return super.caseEdgeStmtNode(object);
 	}
 
 	@Override
-	public Object casenode_id(node_id object) {
+	public Object caseNodeId(NodeId object) {
 		if (!gotSource) {
 			currentEdgeSourceNodeId = object.getName();
 			gotSource = true;
@@ -136,29 +137,29 @@ final class GraphCreatorInterpreter extends DotSwitch<Object> implements
 			}
 			gotSource = false;
 		}
-		return super.casenode_id(object);
+		return super.caseNodeId(object);
 	}
 
 	@Override
-	public Object caseedgeRHS_node(edgeRHS_node object) {
+	public Object caseEdgeRhsNode(EdgeRhsNode object) {
 		// Set the flag for the node_id case handled above
 		gotSource = true;
-		return super.caseedgeRHS_node(object);
+		return super.caseEdgeRhsNode(object);
 	}
 
 	// private implementation of the cases above
 
-	private void createGraph(graph object) {
+	private void createGraph(DotGraph object) {
 		graph = new Graph(parent, style);
 		graph.setLayoutAlgorithm(new TreeLayoutAlgorithm(
 				LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
-		graphtype graphType = object.getType();
-		graph.setConnectionStyle(graphType == graphtype.DIGRAPH ? ZestStyles.CONNECTIONS_DIRECTED
+		GraphType graphType = object.getType();
+		graph.setConnectionStyle(graphType == GraphType.DIGRAPH ? ZestStyles.CONNECTIONS_DIRECTED
 				: ZestStyles.CONNECTIONS_SOLID);
 	}
 
-	private void createAttributes(final attr_stmt attrStmt) {
-		attributetype type = attrStmt.getType();
+	private void createAttributes(final AttrStmt attrStmt) {
+		AttributeType type = attrStmt.getType();
 		switch (type) {
 		case EDGE: {
 			globalEdgeStyle = getAttributeValue(attrStmt, "style"); //$NON-NLS-1$
@@ -181,7 +182,7 @@ final class GraphCreatorInterpreter extends DotSwitch<Object> implements
 		}
 	}
 
-	private void createNode(final node_stmt eStatementObject) {
+	private void createNode(final NodeStmt eStatementObject) {
 		String nodeId = eStatementObject.getName();
 		GraphNode node = new GraphNode(graph, SWT.NONE, nodeId);
 		node.setText(nodeId);
@@ -209,19 +210,19 @@ final class GraphCreatorInterpreter extends DotSwitch<Object> implements
 	 *            The name of the attribute to get the value for, e.g. "label"
 	 * @return The value of the given attribute, e.g. "hi"
 	 */
-	private String getAttributeValue(final stmt eStatementObject,
+	private String getAttributeValue(final Stmt eStatementObject,
 			final String attributeName) {
 		Iterator<EObject> nodeContents = eStatementObject.eContents()
 				.iterator();
 		while (nodeContents.hasNext()) {
 			EObject nodeContentElement = nodeContents.next();
-			if (nodeContentElement.eClass().getName().equals("attr_list")) { //$NON-NLS-1$
+			if (nodeContentElement instanceof AttrList) {
 				Iterator<EObject> attributeContents = nodeContentElement
 						.eContents().iterator();
 				while (attributeContents.hasNext()) {
 					EObject next = attributeContents.next();
-					if (next instanceof a_list) {
-						a_list attributeElement = (a_list) next;
+					if (next instanceof AList) {
+						AList attributeElement = (AList) next;
 						if (attributeElement.getName().equals(attributeName)) {
 							String label = attributeElement.getValue()
 									.replaceAll("\"", ""); //$NON-NLS-1$//$NON-NLS-2$
