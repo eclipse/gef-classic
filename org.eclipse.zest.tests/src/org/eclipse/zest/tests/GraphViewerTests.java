632d2602b58fba2fc172a473da8dce6490b77cf3
fsteeg <fsteeg>
2011-01-03 03:23:22 +0000

    [200732] gef-head fsteeg 110103 Small fix for running test in Indigo on Mac

--- org.eclipse.zest.tests/src/org/eclipse/zest/tests/GraphViewerTests.java
+++ org.eclipse.zest.tests/src/org/eclipse/zest/tests/GraphViewerTests.java
@@ -14,7 +14,6 @@ import junit.framework.TestCase;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DropTarget;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.zest.core.viewers.GraphViewer;
 
@@ -33,7 +32,7 @@ public class GraphViewerTests extends TestCase {
 	 * https://bugs.eclipse.org/bugs/show_bug.cgi?id=200732)
 	 */
 	public void testDisposalWithDropTarget() {
-		Shell shell = new Shell(new Display());
+		Shell shell = new Shell();
 		viewer = new GraphViewer(shell, SWT.NONE);
 		new DropTarget(viewer.getGraphControl(), DND.DROP_MOVE | DND.DROP_COPY);
 		shell.dispose();
