c2b87dba9350cfd7fd75f1b8b120cee4a211eca7
anyssen <anyssen>
2011-01-16 22:24:52 +0000

    [154743] gef-HEAD anyssen 110116 Unnecessary complication in AbstractEditPart#setModel()
    Simplified implementations of setModel() and setParent() to no longer perform a check before assigning values.

--- org.eclipse.gef/src/org/eclipse/gef/editparts/AbstractEditPart.java
+++ org.eclipse.gef/src/org/eclipse/gef/editparts/AbstractEditPart.java
@@ -1019,8 +1019,6 @@ public abstract class AbstractEditPart implements EditPart, RequestConstants,
 	 * @see EditPart#setModel(Object)
 	 */
 	public void setModel(Object model) {
-		if (getModel() == model)
-			return;
 		this.model = model;
 	}
 
@@ -1030,8 +1028,6 @@ public abstract class AbstractEditPart implements EditPart, RequestConstants,
 	 * @see EditPart#setParent(EditPart)
 	 */
 	public void setParent(EditPart parent) {
-		if (this.parent == parent)
-			return;
 		this.parent = parent;
 	}
 
