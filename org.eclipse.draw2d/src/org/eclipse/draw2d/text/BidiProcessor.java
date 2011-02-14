68345adeeae8fe03d9ee092dcac8c53bf6d2e734
anyssen <anyssen>
2011-02-14 20:49:17 +0000

    [81566] gef-HEAD anyssen 110214 BiDi does not work on Mac
    Removed Mac Carbon related workaround which is no longer needed.

--- org.eclipse.draw2d/src/org/eclipse/draw2d/text/BidiProcessor.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/text/BidiProcessor.java
@@ -29,13 +29,6 @@ import org.eclipse.swt.graphics.TextLayout;
  */
 public final class BidiProcessor {
 
-	/*
-	 * Workaround for MacOSX Carbon. AWT DLL cannot start properly on carbon.
-	 * Waiting for bug 82104
-	 */
-	private static final boolean isMacOSXCarbon = SWT.getPlatform().equals(
-			"carbon");//$NON-NLS-1$
-
 	/**
 	 * A helper class to hold information about contributions made to this
 	 * processor.
@@ -202,7 +195,7 @@ public final class BidiProcessor {
 	 */
 	public void process() {
 		try {
-			if (bidiText.length() == 0 || isMacOSXCarbon)
+			if (bidiText.length() == 0)
 				return;
 			char[] chars = new char[bidiText.length()];
 			bidiText.getChars(0, bidiText.length(), chars, 0);
