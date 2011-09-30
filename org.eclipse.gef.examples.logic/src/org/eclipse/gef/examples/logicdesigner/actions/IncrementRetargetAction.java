bcf9ffd8254b5712d6d7209e98a53525eeef2482
anyssen <anyssen>
2011-09-30 19:43:23 +0000

    [NONE] HEAD anyssen 110930 Fixed javadoc.

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/actions/IncrementRetargetAction.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/actions/IncrementRetargetAction.java
@@ -24,9 +24,6 @@ public class IncrementRetargetAction extends RetargetAction {
 
 	/**
 	 * Constructor for IncrementRetargetAction.
-	 * 
-	 * @param actionID
-	 * @param label
 	 */
 	public IncrementRetargetAction() {
 		super(
