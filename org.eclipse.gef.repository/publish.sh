f3e806e29b75bdf0977649f0a1d99358ee628598
anyssen <alexander.nyssen@itemis.de>
2013-05-06 19:37:01 +0200

    [NONE] Adopt to use eclipse-SDK-4.2.2-linux-gtk-x86_64 for publishing.

--- org.eclipse.gef.repository/publish.sh
+++ org.eclipse.gef.repository/publish.sh
@@ -146,8 +146,8 @@ cd $tmpDir
 
 # Download and prepare Eclipse SDK, which is needed to merge update site and postprocess repository 
 echo "Downloading eclipse to $PWD"
-cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops/R-3.7.2-201202080800/eclipse-SDK-3.7.2-linux-gtk-x86_64.tar.gz .
-tar -xvzf eclipse-SDK-3.7.2-linux-gtk-x86_64.tar.gz
+cp /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4/R-4.2.2-201302041200/eclipse-SDK-4.2.2-linux-gtk-x86_64.tar.gz .
+tar -xvzf eclipse-SDK-4.2.2-linux-gtk-x86_64.tar.gz
 cd eclipse
 chmod 700 eclipse
 cd ..
@@ -161,7 +161,7 @@ echo "Installing WTP Releng tools"
 ./eclipse/eclipse -nosplash --launcher.suppressErrors -clean -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/webtools/releng/repository/ -installIUs org.eclipse.wtp.releng.tools.feature.feature.group
 # Clean up
 echo "Cleaning up"
-rm eclipse-SDK-3.7.2-linux-gtk-x86_64.tar.gz
+rm eclipse-SDK-4.2.2-linux-gtk-x86_64.tar.gz
 
 # Prepare local update site (merging is performed later, if required)
 cp -R $localUpdateSite/* update-site/
