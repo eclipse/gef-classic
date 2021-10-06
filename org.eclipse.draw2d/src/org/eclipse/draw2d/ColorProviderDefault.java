24c2e3cb1455b5c4c4af2070aecace01090339fa
Serge Rider <serge@jkiss.org>
2021-10-06 17:35:01 +0400

    eclipse/gef-classic#128 Add color configuration
    Signed-off-by: Georgii Gvinepadze <fireshowel@gmail.com>
    
    eclipse/gef-classic#128 transfer dark theme commits to gef-classic
    
    Signed-off-by: Georgii Gvinepadze <fireshowel@gmail.com>

---
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/ColorProviderDefault.java
@@ -0,0 +1,136 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Georgii Gvinepadze - georgii.gvinepadze@dbeaver.com
+ *     Serge Rider - serge@dbeaver.com
+ *******************************************************************************/
+package org.eclipse.draw2d;
+
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.themes.IThemeManager;
+
+/**
+ * A collection of color-related constants.
+ */
+public class ColorProviderDefault implements ColorProvider {
+
+	private final IThemeManager themeManager;
+
+	public ColorProviderDefault() {
+		themeManager = PlatformUI.getWorkbench().getThemeManager();
+	}
+
+	@Override
+	public Color getButtonLightest() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_BUTTON_LIGHTEST);
+	}
+
+	@Override
+	public Color getButton() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_BUTTON);
+	}
+
+	@Override
+	public Color getButtonDarker() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_BUTTON_DARKER);
+	}
+
+	@Override
+	public Color getButtonDarkest() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_BUTTON_DARKEST);
+	}
+
+	@Override
+	public Color getListBackground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_LIST_BACKGROUND);
+	}
+
+	@Override
+	public Color getListForeground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_LIST_FOREGROUND);
+	}
+
+	@Override
+	public Color getLineForeground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_LINE_FOREGROUND);
+	}
+
+	@Override
+	public Color getMenuBackground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_MENU_BACKGROUND);
+	}
+
+	@Override
+	public Color getMenuForeground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_MENU_FOREGROUND);
+	}
+
+	@Override
+	public Color getMenuBackgroundSelected() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_MENU_BACKGROUND_SELECTED);
+	}
+
+	@Override
+	public Color getMenuForegroundSelected() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_MENU_FOREGROUND_SELECTED);
+	}
+
+	@Override
+	public Color getTitleBackground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_TITLE_BACKGROUND);
+	}
+
+	@Override
+	public Color getTitleGradient() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_TITLE_GRADIENT);
+	}
+
+	@Override
+	public Color getTitleForeground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_TITLE_FOREGROUND);
+	}
+
+	@Override
+	public Color getTitleInactiveForeground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_TITLE_INACTIVE_FOREGROUND);
+	}
+
+	@Override
+	public Color getTitleInactiveBackground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_TITLE_INACTIVE_BACKGROUND);
+	}
+
+	@Override
+	public Color getTitleInactiveGradient() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_TITLE_INACTIVE_GRADIENT);
+	}
+
+	@Override
+	public Color getTooltipForeground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_TOOLTIP_BACKGROUND);
+	}
+
+	@Override
+	public Color getTooltipBackground() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_TOOLTIP_FOREGROUND);
+	}
+
+	@Override
+	public Color getListHoverBackgroundColor() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_LIST_HOVER_BACKGROUND);
+	}
+
+	@Override
+	public Color getListSelectedBackgroundColor() {
+		return themeManager.getCurrentTheme().getColorRegistry().get(ThemeConstants.CONFIG_COLOR_LIST_SELECTED_BACKGROUND);
+	}
+
+
+}
--- org.eclipse.gef/src/org/eclipse/gef/editparts/GridLayer.java
+++ org.eclipse.gef/src/org/eclipse/gef/editparts/GridLayer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2010 IBM Corporation and others.
+ * Copyright (c) 2004, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,14 +10,9 @@
  *******************************************************************************/
 package org.eclipse.gef.editparts;
 
-import org.eclipse.draw2d.ColorConstants;
-import org.eclipse.draw2d.FigureUtilities;
-import org.eclipse.draw2d.FreeformLayer;
-import org.eclipse.draw2d.Graphics;
-import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.*;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
-
 import org.eclipse.gef.SnapToGrid;
 
 /**
@@ -50,7 +45,7 @@ public class GridLayer extends FreeformLayer {
 	 */
 	public GridLayer() {
 		super();
-		setForegroundColor(ColorConstants.lightGray);
+		setForegroundColor(ColorConstants.lineForeground);
 	}
 
 	/**
