eb1d17a5c0803b632ce797977c9a488ba7395ca5
Alois Zoitl <alois.zoitl@gmx.at>
2022-08-02 22:28:43 +0200

    Typified command list on compound command. Cleaned-up compound command.

--- org.eclipse.gef/src/org/eclipse/gef/commands/ForwardUndoCompoundCommand.java
+++ org.eclipse.gef/src/org/eclipse/gef/commands/ForwardUndoCompoundCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -26,7 +26,7 @@ public class ForwardUndoCompoundCommand extends CompoundCommand {
 
 	/**
 	 * Constructs a ForwardUndoCompoundCommand with the specified label
-	 * 
+	 *
 	 * @param label the label
 	 */
 	public ForwardUndoCompoundCommand(String label) {
@@ -36,6 +36,7 @@ public class ForwardUndoCompoundCommand extends CompoundCommand {
 	/**
 	 * @see org.eclipse.gef.commands.Command#getDebugLabel()
 	 */
+	@Override
 	public String getDebugLabel() {
 		return "ForwardUndoCommand: " + super.getDebugLabel();//$NON-NLS-1$
 	}
@@ -44,17 +45,18 @@ public class ForwardUndoCompoundCommand extends CompoundCommand {
 	 * Undo the command. For a Preorder compound command this means undoing all of
 	 * the commands that it contains. Do it in the same order as applied.
 	 */
+	@Override
 	public void undo() {
-		ListIterator itr = getCommands().listIterator();
+		ListIterator<? extends Command> itr = getCommands().listIterator();
 		try {
 			while (itr.hasNext()) {
-				((Command) itr.next()).undo();
+				itr.next().undo();
 			}
 		} catch (RuntimeException e) {
 			itr.previous(); // Skip over the one that failed. It cleaned itself
 							// up.
 			while (itr.hasPrevious()) {
-				((Command) itr.previous()).redo();
+				itr.previous().redo();
 			}
 			throw e;
 		}
