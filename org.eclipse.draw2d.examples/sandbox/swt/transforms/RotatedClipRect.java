6bdc4a371a1de809d4558b8547d682c9dc24f7e7
Pierre-Charles David <pierre-charles.david@obeo.fr>
2022-08-21 17:45:06 +0200

    Remove unused imports in Drawd2D examples
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- org.eclipse.draw2d.examples/sandbox/swt/transforms/RotatedClipRect.java
+++ org.eclipse.draw2d.examples/sandbox/swt/transforms/RotatedClipRect.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,51 +9,49 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package swt.transforms;
+
+import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.TextLayout;
 import org.eclipse.swt.graphics.Transform;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
-import org.eclipse.draw2d.ColorConstants;
-
 public class RotatedClipRect {
 
-static float angle = 4;
+	static float angle = 4;
 
-public static void main(String[] args) {
-	final Display display = new Display();
-	final Shell shell = new Shell(display);
-	shell.setText("Shell");
-	shell.setFont(new Font(display, "Arial", 12, 0));
+	public static void main(String[] args) {
+		final Display display = new Display();
+		final Shell shell = new Shell(display);
+		shell.setText("Shell");
+		shell.setFont(new Font(display, "Arial", 12, 0));
 
-	shell.addPaintListener(new PaintListener() {
-		public void paintControl(PaintEvent e) {
-			GC gc = e.gc;
-			Transform t = new Transform(display);
-			t.translate(60, 60);
-			t.rotate(angle);
-			gc.setTransform(t);
-			gc.setClipping(-20, -20, 40, 40);
-			
-			gc.setBackground(ColorConstants.blue);
-			gc.setForeground(ColorConstants.yellow);
-			gc.fillGradientRectangle(-100, -100, 200, 200, true);
-			angle += 0.3;
+		shell.addPaintListener(new PaintListener() {
+			public void paintControl(PaintEvent e) {
+				GC gc = e.gc;
+				Transform t = new Transform(display);
+				t.translate(60, 60);
+				t.rotate(angle);
+				gc.setTransform(t);
+				gc.setClipping(-20, -20, 40, 40);
+
+				gc.setBackground(ColorConstants.blue);
+				gc.setForeground(ColorConstants.yellow);
+				gc.fillGradientRectangle(-100, -100, 200, 200, true);
+				angle += 0.3;
+			}
+		});
+
+		shell.setSize(200, 200);
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
 		}
-	});
-	
-	shell.setSize(200, 200);
-	shell.open();
-	while (!shell.isDisposed()) {
-		if (!display.readAndDispatch())
-			display.sleep();
+		display.dispose();
 	}
-	display.dispose();
-}
 
 }
