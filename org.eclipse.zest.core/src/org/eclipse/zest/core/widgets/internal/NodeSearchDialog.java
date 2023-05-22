f14d79b22169d254fd497ed3f0b60485b291ece3
Sebastian Hollersbacher <s.hollersbacher@outlook.at>
2023-05-22 18:10:07 +0200

    Updated layout of search window
    Fixed bug when node does not exist

--- org.eclipse.zest.core/src/org/eclipse/zest/core/widgets/internal/NodeSearchDialog.java
+++ org.eclipse.zest.core/src/org/eclipse/zest/core/widgets/internal/NodeSearchDialog.java
@@ -6,33 +6,41 @@ import java.util.List;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.FocusAdapter;
 import org.eclipse.swt.events.FocusEvent;
-import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.zest.core.widgets.GraphNode;
 
 public class NodeSearchDialog {
-	private Shell shell;
+	private Shell dialog;
 	private List<GraphNode> nodes;
 
 	private List<GraphNode> searchNodes;
 	private GraphNode prevNode;
 	private int index = 0;
 
-	public NodeSearchDialog(Shell shell, List<GraphNode> nodes) {
+	public NodeSearchDialog(Shell parent, List<GraphNode> nodes) {
 		this.nodes = nodes;
-		this.shell = shell;
 		searchNodes = new ArrayList<>();
+		this.dialog = createDialog(parent);
 	}
 
-	public void open() {
-		Shell dialog = new Shell(this.shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+	private Shell createDialog(Shell parentShell) {
+		Shell dialog = new Shell(parentShell, SWT.DIALOG_TRIM | SWT.MAX);
 		dialog.setText("Find");
 		dialog.setSize(300, 200);
-		dialog.setLayout(new RowLayout());
+		GridLayout layout = new GridLayout(2, false);
+		dialog.setLayout(layout);
 
-		final Text text = new Text(dialog, SWT.SHADOW_IN);
+		// 1st row
+		final Label label = new Label(dialog, SWT.NONE);
+		label.setText("Find:");
+
+		final Text text = new Text(dialog, SWT.BORDER);
 		text.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusLost(FocusEvent e) {
@@ -48,12 +56,23 @@ public class NodeSearchDialog {
 				}
 			}
 		});
+		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+		text.setLayoutData(gridData);
 
-		Button nextButton = new Button(dialog, SWT.PUSH);
+		// 2nd row
+		new Label(dialog, SWT.NULL);
+
+		Composite comp = new Composite(dialog, 0);
+		comp.setLayout(new GridLayout(3, false));
+
+		Button nextButton = new Button(comp, SWT.PUSH);
 		nextButton.setText("Next");
 		nextButton.addListener(SWT.Selection, e -> {
+			if (searchNodes.isEmpty()) {
+				return;
+			}
 			GraphNode node = searchNodes.get(index % searchNodes.size());
-			node.getGraphModel().setSelection(new GraphNode[] { node });	// select node
+			node.getGraphModel().setSelection(new GraphNode[] { node }); // select node
 			index++;
 			if (prevNode != null && prevNode != node) {
 				prevNode.unhighlight();
@@ -61,15 +80,18 @@ public class NodeSearchDialog {
 			prevNode = node;
 		});
 
-		Button prevButton = new Button(dialog, SWT.PUSH);
+		Button prevButton = new Button(comp, SWT.PUSH);
 		prevButton.setText("Previous");
 		prevButton.addListener(SWT.Selection, e -> {
+			if (searchNodes.isEmpty()) {
+				return;
+			}
 			if (index < 0) {
 				index = searchNodes.size() - 1;
 			}
 			GraphNode node = searchNodes.get(index % searchNodes.size());
 			node.highlight();
-			node.getGraphModel().setSelection(new GraphNode[] { node });	// select node
+			node.getGraphModel().setSelection(new GraphNode[] { node }); // select node
 			index--;
 			if (prevNode != null && prevNode != node) {
 				prevNode.unhighlight();
@@ -77,10 +99,14 @@ public class NodeSearchDialog {
 			prevNode = node;
 		});
 
-		Button cancelButton = new Button(dialog, SWT.PUSH);
-		cancelButton.setText("Cancel");
-		cancelButton.addListener(SWT.Selection, e -> dialog.close());
+		Button closeButton = new Button(comp, SWT.PUSH);
+		closeButton.setText("Close");
+		closeButton.addListener(SWT.Selection, e -> dialog.close());
 
+		return dialog;
+	}
+
+	public void open() {
 		dialog.open();
 	}
 }
