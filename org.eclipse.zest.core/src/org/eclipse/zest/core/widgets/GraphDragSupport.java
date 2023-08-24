be2378fa48dd0e0fa9d28c2a68bc45790e97c40c
Philipp Bauer <baue.phil@gmail.com>
2023-08-24 09:08:53 +0200

    expose layer creation and drag support for more customizable Graph implementations

---
+++ org.eclipse.zest.core/src/org/eclipse/zest/core/widgets/GraphDragSupport.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 Johannes Kepler University Linz
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Philipp Bauer - initial implementation
+ */
+
+package org.eclipse.zest.core.widgets;
+
+import org.eclipse.draw2d.MouseListener;
+import org.eclipse.draw2d.MouseMotionListener;
+
+/** Super Interface for drag support for zest graphs @see{Graph}
+ *
+ * @since 3.2 */
+public interface GraphDragSupport extends MouseMotionListener, MouseListener {
+
+}
