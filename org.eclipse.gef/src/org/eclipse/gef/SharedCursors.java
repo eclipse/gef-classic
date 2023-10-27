bbb1947ffa7c36c5a390f7e2f818ba1df73fdf0c
Alois Zoitl <alois.zoitl@gmx.at>
2023-10-28 00:28:34 +0200

    Changed shared cursors from bmps and masks to single pngs
    Thx to Phillipus <p.beauvoir@dadabeatnik.com> for providing the cursor
    images.

--- org.eclipse.gef/src/org/eclipse/gef/SharedCursors.java
+++ org.eclipse.gef/src/org/eclipse/gef/SharedCursors.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2000, 2010 IBM Corporation and others.
  *
- * This program and the accompanying materials are made available under the 
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
  *
@@ -22,7 +22,7 @@ import org.eclipse.gef.internal.Internal;
 
 /**
  * A shared collection of Cursors.
- * 
+ *
  * @since 2.0
  */
 public class SharedCursors extends Cursors {
@@ -44,21 +44,33 @@ public class SharedCursors extends Cursors {
 	 */
 	public static final Cursor CURSOR_TREE_MOVE;
 
+	private static int deviceZoom = -1;
+
 	static {
-		CURSOR_PLUG = createCursor("icons/plugmask.gif", //$NON-NLS-1$
-				"icons/plug.bmp"); //$NON-NLS-1$
-		CURSOR_PLUG_NOT = createCursor("icons/plugmasknot.gif", //$NON-NLS-1$
-				"icons/plugnot.bmp"); //$NON-NLS-1$
-		CURSOR_TREE_ADD = createCursor("icons/Tree_Add_Mask.gif", //$NON-NLS-1$
-				"icons/Tree_Add.gif"); //$NON-NLS-1$
-		CURSOR_TREE_MOVE = createCursor("icons/Tree_Move_Mask.gif", //$NON-NLS-1$
-				"icons/Tree_Move.gif"); //$NON-NLS-1$
+		CURSOR_PLUG = createCursor("icons/plug-cursor.png"); //$NON-NLS-1$
+		CURSOR_PLUG_NOT = createCursor("icons/plugnot-cursor.png"); //$NON-NLS-1$
+		CURSOR_TREE_ADD = createCursor("icons/tree_add-cursor.png"); //$NON-NLS-1$
+		CURSOR_TREE_MOVE = createCursor("icons/tree_move-cursor.png"); //$NON-NLS-1$
 	}
 
-	private static Cursor createCursor(String sourceName, String maskName) {
+	private static Cursor createCursor(String sourceName) {
 		ImageDescriptor src = ImageDescriptor.createFromFile(Internal.class, sourceName);
-		ImageDescriptor mask = ImageDescriptor.createFromFile(Internal.class, maskName);
-		return new Cursor(null, src.getImageData(), mask.getImageData(), 0, 0);
+		return new Cursor(null, src.getImageData(getDeviceZoom()), 0, 0);
+	}
+
+	private static int getDeviceZoom() {
+		if (deviceZoom == -1) {
+			deviceZoom = 100; // default value
+			String deviceZoomProperty = System.getProperty("org.eclipse.swt.internal.deviceZoom"); //$NON-NLS-1$
+			if (deviceZoomProperty != null) {
+				try {
+					deviceZoom = Integer.parseInt(deviceZoomProperty);
+				} catch (NumberFormatException ex) {
+					// if the property can not be parsed we keep the default 100% zoom level
+				}
+			}
+		}
+		return deviceZoom;
 	}
 
 }
