1dc0ecc90fb2a59a01641a5dd27d870df4c8d0cf
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-06-24 10:19:49 +0200

    Use static imports for Assert methods in CommandStackTest
    Mainly done to remove the "Assert.*" prefix and improve readability.

--- org.eclipse.gef.tests/src/org/eclipse/gef/test/CommandStackTest.java
+++ org.eclipse.gef.tests/src/org/eclipse/gef/test/CommandStackTest.java
@@ -1,5 +1,10 @@
 package org.eclipse.gef.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,10 +12,9 @@ import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.gef.commands.CommandStackEvent;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-public class CommandStackTest extends Assert {
+public class CommandStackTest {
 
 	@SuppressWarnings("static-method")
 	@Test
@@ -25,114 +29,114 @@ public class CommandStackTest extends Assert {
 		};
 
 		// test execution pre and post events
-		Assert.assertEquals(0, commandStackEvents.size());
+		assertEquals(0, commandStackEvents.size());
 		stack.execute(c);
 
-		Assert.assertEquals(2, commandStackEvents.size());
-		Assert.assertEquals(c, commandStackEvents.get(0).getCommand());
-		Assert.assertTrue(commandStackEvents.get(0).isPreChangeEvent());
-		Assert.assertFalse(commandStackEvents.get(0).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(0).getSource());
-		Assert.assertEquals(CommandStack.PRE_EXECUTE, commandStackEvents.get(0).getDetail());
-		Assert.assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
+		assertEquals(2, commandStackEvents.size());
+		assertEquals(c, commandStackEvents.get(0).getCommand());
+		assertTrue(commandStackEvents.get(0).isPreChangeEvent());
+		assertFalse(commandStackEvents.get(0).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(0).getSource());
+		assertEquals(CommandStack.PRE_EXECUTE, commandStackEvents.get(0).getDetail());
+		assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
+		assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
 
-		Assert.assertEquals(c, commandStackEvents.get(1).getCommand());
-		Assert.assertFalse(commandStackEvents.get(1).isPreChangeEvent());
-		Assert.assertTrue(commandStackEvents.get(1).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(1).getSource());
-		Assert.assertEquals(CommandStack.POST_EXECUTE, commandStackEvents.get(1).getDetail());
-		Assert.assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
+		assertEquals(c, commandStackEvents.get(1).getCommand());
+		assertFalse(commandStackEvents.get(1).isPreChangeEvent());
+		assertTrue(commandStackEvents.get(1).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(1).getSource());
+		assertEquals(CommandStack.POST_EXECUTE, commandStackEvents.get(1).getDetail());
+		assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
+		assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
 
 		// // test undo pre and post events
 		commandStackEvents.clear();
-		Assert.assertEquals(0, commandStackEvents.size());
+		assertEquals(0, commandStackEvents.size());
 		stack.undo();
 
-		Assert.assertEquals(2, commandStackEvents.size());
-		Assert.assertEquals(c, commandStackEvents.get(0).getCommand());
-		Assert.assertTrue(commandStackEvents.get(0).isPreChangeEvent());
-		Assert.assertFalse(commandStackEvents.get(0).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(0).getSource());
-		Assert.assertEquals(CommandStack.PRE_UNDO, commandStackEvents.get(0).getDetail());
-		Assert.assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
+		assertEquals(2, commandStackEvents.size());
+		assertEquals(c, commandStackEvents.get(0).getCommand());
+		assertTrue(commandStackEvents.get(0).isPreChangeEvent());
+		assertFalse(commandStackEvents.get(0).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(0).getSource());
+		assertEquals(CommandStack.PRE_UNDO, commandStackEvents.get(0).getDetail());
+		assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
+		assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
 
-		Assert.assertEquals(c, commandStackEvents.get(1).getCommand());
-		Assert.assertFalse(commandStackEvents.get(1).isPreChangeEvent());
-		Assert.assertTrue(commandStackEvents.get(1).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(1).getSource());
-		Assert.assertEquals(CommandStack.POST_UNDO, commandStackEvents.get(1).getDetail());
-		Assert.assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
+		assertEquals(c, commandStackEvents.get(1).getCommand());
+		assertFalse(commandStackEvents.get(1).isPreChangeEvent());
+		assertTrue(commandStackEvents.get(1).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(1).getSource());
+		assertEquals(CommandStack.POST_UNDO, commandStackEvents.get(1).getDetail());
+		assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
+		assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
 
 		// // test redo pre and post events
 		commandStackEvents.clear();
-		Assert.assertEquals(0, commandStackEvents.size());
+		assertEquals(0, commandStackEvents.size());
 		stack.redo();
 
-		Assert.assertEquals(2, commandStackEvents.size());
-		Assert.assertEquals(c, commandStackEvents.get(0).getCommand());
-		Assert.assertTrue(commandStackEvents.get(0).isPreChangeEvent());
-		Assert.assertFalse(commandStackEvents.get(0).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(0).getSource());
-		Assert.assertEquals(CommandStack.PRE_REDO, commandStackEvents.get(0).getDetail());
-		Assert.assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
+		assertEquals(2, commandStackEvents.size());
+		assertEquals(c, commandStackEvents.get(0).getCommand());
+		assertTrue(commandStackEvents.get(0).isPreChangeEvent());
+		assertFalse(commandStackEvents.get(0).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(0).getSource());
+		assertEquals(CommandStack.PRE_REDO, commandStackEvents.get(0).getDetail());
+		assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
+		assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
 
-		Assert.assertEquals(c, commandStackEvents.get(1).getCommand());
-		Assert.assertFalse(commandStackEvents.get(1).isPreChangeEvent());
-		Assert.assertTrue(commandStackEvents.get(1).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(1).getSource());
-		Assert.assertEquals(CommandStack.POST_REDO, commandStackEvents.get(1).getDetail());
-		Assert.assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
+		assertEquals(c, commandStackEvents.get(1).getCommand());
+		assertFalse(commandStackEvents.get(1).isPreChangeEvent());
+		assertTrue(commandStackEvents.get(1).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(1).getSource());
+		assertEquals(CommandStack.POST_REDO, commandStackEvents.get(1).getDetail());
+		assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
+		assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
 
 		// test flush event
 		commandStackEvents.clear();
-		Assert.assertEquals(0, commandStackEvents.size());
+		assertEquals(0, commandStackEvents.size());
 		stack.flush();
 
-		Assert.assertEquals(2, commandStackEvents.size());
-		Assert.assertEquals(null, commandStackEvents.get(0).getCommand());
-		Assert.assertTrue(commandStackEvents.get(0).isPreChangeEvent());
-		Assert.assertFalse(commandStackEvents.get(0).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(0).getSource());
-		Assert.assertEquals(CommandStack.PRE_FLUSH, commandStackEvents.get(0).getDetail());
-		Assert.assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
+		assertEquals(2, commandStackEvents.size());
+		assertEquals(null, commandStackEvents.get(0).getCommand());
+		assertTrue(commandStackEvents.get(0).isPreChangeEvent());
+		assertFalse(commandStackEvents.get(0).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(0).getSource());
+		assertEquals(CommandStack.PRE_FLUSH, commandStackEvents.get(0).getDetail());
+		assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
+		assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
 
-		Assert.assertEquals(null, commandStackEvents.get(1).getCommand());
-		Assert.assertFalse(commandStackEvents.get(1).isPreChangeEvent());
-		Assert.assertTrue(commandStackEvents.get(1).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(1).getSource());
-		Assert.assertEquals(CommandStack.POST_FLUSH, commandStackEvents.get(1).getDetail());
-		Assert.assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
+		assertEquals(null, commandStackEvents.get(1).getCommand());
+		assertFalse(commandStackEvents.get(1).isPreChangeEvent());
+		assertTrue(commandStackEvents.get(1).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(1).getSource());
+		assertEquals(CommandStack.POST_FLUSH, commandStackEvents.get(1).getDetail());
+		assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
+		assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
 
 		// test mark save location event
 		commandStackEvents.clear();
-		Assert.assertEquals(0, commandStackEvents.size());
+		assertEquals(0, commandStackEvents.size());
 		stack.redo();
 		stack.markSaveLocation();
 
-		Assert.assertEquals(2, commandStackEvents.size());
-		Assert.assertEquals(null, commandStackEvents.get(0).getCommand());
-		Assert.assertTrue(commandStackEvents.get(0).isPreChangeEvent());
-		Assert.assertFalse(commandStackEvents.get(0).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(0).getSource());
-		Assert.assertEquals(CommandStack.PRE_MARK_SAVE, commandStackEvents.get(0).getDetail());
-		Assert.assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
+		assertEquals(2, commandStackEvents.size());
+		assertEquals(null, commandStackEvents.get(0).getCommand());
+		assertTrue(commandStackEvents.get(0).isPreChangeEvent());
+		assertFalse(commandStackEvents.get(0).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(0).getSource());
+		assertEquals(CommandStack.PRE_MARK_SAVE, commandStackEvents.get(0).getDetail());
+		assertNotEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.PRE_MASK);
+		assertEquals(0, commandStackEvents.get(0).getDetail() & CommandStack.POST_MASK);
 
-		Assert.assertEquals(null, commandStackEvents.get(1).getCommand());
-		Assert.assertFalse(commandStackEvents.get(1).isPreChangeEvent());
-		Assert.assertTrue(commandStackEvents.get(1).isPostChangeEvent());
-		Assert.assertEquals(stack, commandStackEvents.get(1).getSource());
-		Assert.assertEquals(CommandStack.POST_MARK_SAVE, commandStackEvents.get(1).getDetail());
-		Assert.assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
-		Assert.assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
+		assertEquals(null, commandStackEvents.get(1).getCommand());
+		assertFalse(commandStackEvents.get(1).isPreChangeEvent());
+		assertTrue(commandStackEvents.get(1).isPostChangeEvent());
+		assertEquals(stack, commandStackEvents.get(1).getSource());
+		assertEquals(CommandStack.POST_MARK_SAVE, commandStackEvents.get(1).getDetail());
+		assertEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.PRE_MASK);
+		assertNotEquals(0, commandStackEvents.get(1).getDetail() & CommandStack.POST_MASK);
 
 	}
 }
