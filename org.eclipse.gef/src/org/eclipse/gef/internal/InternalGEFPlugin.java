9d6e39ca62dc2d9e9cbbf5cfc5ee0ce9ac7545f4
Patrick Ziegler <ziegler.patrick@hotmail.de>
2024-07-17 21:52:46 +0200

    Fix errors in drag/shared cursors when using high-DPI screens
    The cursors used in the flyout palette only exist for the default 100%.
    If a system is using a higher zoom level, the creation of those cursors
    fails with an IllegalArgumentException. Instead, we should scale the
    cursor image to match the current level.
    
    As opposed to reading the zoom level only once, it is now calculated
    dynamically, as the zoom may change depending on the display or because
    it has been changed. The methods for handling this case have also been
    moved from the SharedCursors class to the InternalGEFPlugin, so that it
    becomes available to the DragCursors class.
    
    From there, we call scaledImageData() to convert the image
    descriptor to image data matching the requested zoom. The scaling is
    done internally by SWT.
    
    See https://github.com/eclipse/gef-classic/issues/464

--- org.eclipse.gef/src/org/eclipse/gef/internal/InternalGEFPlugin.java
+++ org.eclipse.gef/src/org/eclipse/gef/internal/InternalGEFPlugin.java
@@ -13,6 +13,11 @@
 
 package org.eclipse.gef.internal;
 
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -67,4 +72,43 @@ public class InternalGEFPlugin extends AbstractUIPlugin {
 		super.stop(context);
 	}
 
+	/**
+	 * Convenience method for getting the current zoom level of the active device.If
+	 * on MacOS or Linux (x11 window system) or if the device zoom couldn't
+	 * otherwise be determined, this method returns {@code 100} as default value.
+	 */
+	public static int getOrDefaultDeviceZoom() {
+		// On Mac and Linux X11 ImageData for cursors should always be created with 100%
+		// device zoom
+		if (Platform.getOS().equals(Platform.OS_MACOSX) || (Platform.getOS().equals(Platform.OS_LINUX)
+				&& "x11".equalsIgnoreCase(System.getenv("XDG_SESSION_TYPE")))) { //$NON-NLS-1$//$NON-NLS-2$
+			return 100;
+		}
+		String deviceZoom = System.getProperty("org.eclipse.swt.internal.deviceZoom", "100"); //$NON-NLS-1$ //$NON-NLS-2$
+		try {
+			return Integer.parseInt(deviceZoom);
+		} catch (NumberFormatException e) {
+			return 100;
+		}
+	}
+
+	/**
+	 * Convenience method to get the image data for a given zoom level. If no image
+	 * for the requested zoom level exists, the image data may be an auto-scaled
+	 * version of the native image and may look blurred or mangled.
+	 */
+	public static ImageData scaledImageData(ImageDescriptor descriptor, int zoom) {
+		// Default case: Image in matching resolution has been found
+		ImageData data = descriptor.getImageData(zoom);
+		if (data != null) {
+			return data;
+		}
+		// Otherwise artifically scale the image
+		Image image = descriptor.createImage();
+		try {
+			return image.getImageData(zoom);
+		} finally {
+			image.dispose();
+		}
+	}
 }
