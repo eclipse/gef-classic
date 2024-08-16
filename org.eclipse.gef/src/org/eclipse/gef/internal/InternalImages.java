8c27d09d09462b6daf3b16402ef0dd24cc9126a9
Alois Zoitl <alois.zoitl@gmx.at>
2024-08-16 22:48:49 +0200

    Create high-DPI variants for GEF icons #465
    Several icons where reworked:
      - text styles
      - add
      - pinned / unpinned
      - separator
      - customizer_diolog_title
    
    See https://github.com/eclipse/gef-classic/issues/465

--- org.eclipse.gef/src/org/eclipse/gef/internal/InternalImages.java
+++ org.eclipse.gef/src/org/eclipse/gef/internal/InternalImages.java
@@ -50,9 +50,6 @@ public class InternalImages {
 	public static final ImageDescriptor DESC_VERT_ALIGN_TOP_DIS;
 	public static final ImageDescriptor DESC_VERT_ALIGN_BOTTOM_DIS;
 
-	public static final ImageDescriptor ICON_MATCH_WIDTH;
-	public static final ImageDescriptor ICON_MATCH_HEIGHT;
-
 	public static final ImageDescriptor DESC_SEPARATOR;
 	public static final ImageDescriptor DESC_FOLDER_OPEN;
 	public static final ImageDescriptor DESC_FOLDER_CLOSED;
@@ -76,12 +73,12 @@ public class InternalImages {
 	/**
 	 * Can be used to access the cached pinned image by using {@link #get(String)}.
 	 */
-	public static final String IMG_PINNED = "icons/pinned.gif";//$NON-NLS-1$
+	public static final String IMG_PINNED = "icons/pinned.png";//$NON-NLS-1$
 
 	/**
 	 * Can be used to access the cached pinned image by using {@link #get(String)}.
 	 */
-	public static final String IMG_UNPINNED = "icons/unpinned.gif";//$NON-NLS-1$
+	public static final String IMG_UNPINNED = "icons/unpinned.png";//$NON-NLS-1$
 
 	/**
 	 * Can be used to access the cached pinned image by using {@link #get(String)}.
@@ -91,12 +88,12 @@ public class InternalImages {
 	private static final Map<String, Image> overloadedImages = new HashMap<>();
 
 	static {
-		DESC_BOLD = createDescriptor("icons/style_bold.gif"); //$NON-NLS-1$
-		DESC_ITALIC = createDescriptor("icons/style_italic.gif"); //$NON-NLS-1$
-		DESC_UNDERLINE = createDescriptor("icons/style_underline.gif"); //$NON-NLS-1$
+		DESC_BOLD = createDescriptor("icons/style_bold.png"); //$NON-NLS-1$
+		DESC_ITALIC = createDescriptor("icons/style_italic.png"); //$NON-NLS-1$
+		DESC_UNDERLINE = createDescriptor("icons/style_underline.png"); //$NON-NLS-1$
 
-		DESC_BLOCK_LTR = createDescriptor("icons/style_paragraph_ltr.gif"); //$NON-NLS-1$
-		DESC_BLOCK_RTL = createDescriptor("icons/style_paragraph_rtl.gif"); //$NON-NLS-1$
+		DESC_BLOCK_LTR = createDescriptor("icons/style_paragraph_ltr.png"); //$NON-NLS-1$
+		DESC_BLOCK_RTL = createDescriptor("icons/style_paragraph_rtl.png"); //$NON-NLS-1$
 
 		DESC_BLOCK_ALIGN_LEFT = createDescriptor("icons/style_paragraph_left.png"); //$NON-NLS-1$
 		DESC_BLOCK_ALIGN_CENTER = createDescriptor("icons/style_paragraph_center.png"); //$NON-NLS-1$
@@ -127,12 +124,9 @@ public class InternalImages {
 		DESC_HORZ_ALIGN_RIGHT_DIS = createDescriptor("icons/alignright_d.png"); //$NON-NLS-1$
 		DESC_VERT_ALIGN_TOP_DIS = createDescriptor("icons/aligntop_d.png"); //$NON-NLS-1$
 
-		DESC_SEPARATOR = createDescriptor("icons/separator.gif"); //$NON-NLS-1$
+		DESC_SEPARATOR = createDescriptor("icons/separator.png"); //$NON-NLS-1$
 		DESC_FOLDER_OPEN = PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJ_FOLDER);
 		DESC_FOLDER_CLOSED = createDescriptor("icons/folder_closed.png"); //$NON-NLS-1$
-
-		ICON_MATCH_WIDTH = createDescriptor("icons/sizehz.gif"); //$NON-NLS-1$
-		ICON_MATCH_HEIGHT = createDescriptor("icons/sizevt.gif"); //$NON-NLS-1$
 
 		DESC_PINNED = createAndCache(IMG_PINNED);
 		DESC_UNPINNED = createAndCache(IMG_UNPINNED);
