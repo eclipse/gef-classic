13725dd6b6bf83141fabc5c9bb1bfe8e681103a4
Phillipus <p.beauvoir@dadabeatnik.com>
2024-09-12 19:30:55 +0100

    Fix regression in Tooltip parent Shell on Linux Wayland
    This reverts 843e56c0df42aec34a487b97483a0b6c866fff95 and instead checks for a disposed Shell in SWTEventDispatcher#getToolTipHelper()
    
    See https://github.com/eclipse/gef-classic/issues/555

--- org.eclipse.draw2d/src/org/eclipse/draw2d/SWTEventDispatcher.java
+++ org.eclipse.draw2d/src/org/eclipse/draw2d/SWTEventDispatcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2024 IBM Corporation and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -336,7 +336,7 @@ public class SWTEventDispatcher extends EventDispatcher {
 	 * @return the ToolTipHelper
 	 */
 	protected ToolTipHelper getToolTipHelper() {
-		if (toolTipHelper == null) {
+		if (toolTipHelper == null || toolTipHelper.getShell().isDisposed()) {
 			toolTipHelper = new ToolTipHelper(control);
 		}
 		return toolTipHelper;
