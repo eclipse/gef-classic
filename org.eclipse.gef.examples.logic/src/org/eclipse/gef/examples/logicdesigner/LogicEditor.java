cc2fc78b9dd33e9471541c8b28df8b52569fa568
Alois Zoitl <alois.zoitl@gmx.at>
2024-10-16 23:27:59 +0200

    Moved all streams into a try with resources setup

--- org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/LogicEditor.java
+++ org.eclipse.gef.examples.logic/src/org/eclipse/gef/examples/logicdesigner/LogicEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2022 IBM Corporation and others.
+ * Copyright (c) 2000, 2024 IBM Corporation and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -486,9 +486,9 @@ public class LogicEditor extends GraphicalEditorWithFlyoutPalette {
 	}
 
 	protected void writeToOutputStream(OutputStream os) throws IOException {
-		ObjectOutputStream out = new ObjectOutputStream(os);
-		out.writeObject(getLogicDiagram());
-		out.close();
+		try (ObjectOutputStream out = new ObjectOutputStream(os)) {
+			out.writeObject(getLogicDiagram());
+		}
 	}
 
 	@Override
